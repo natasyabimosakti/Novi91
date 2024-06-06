7f979fba5226047ba8a182e597cf1cf0211237b1
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-06-06 19:02:46 +0700

    Update

---
+++ Blank Out.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Go About Blank
+// @namespace    http://tampermonkey.net/
+// @version      2024-06-02
+// @description  try to take over the world!
+// @author       You
+// @match        http*://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+setInterval(function(){
+    'use strict';
+if(document.getElementsByClassName("native-text")[1]){
+        if(document.getElementsByClassName("native-text")[1].textContent.includes("Postingan")){
+            location.href = "about:blank"
+        }
+    }
+    if(document.getElementsByClassName("native-text")[2]){
+        if(document.getElementsByClassName("native-text")[2].textContent.includes("Postingan")){
+            location.href = "about:blank"
+        }
+    }
+    if(document.getElementsByClassName("native-text")[3]){
+        if(document.getElementsByClassName("native-text")[3].textContent.includes("Postingan")){
+            location.href = "about:blank"
+        }
+    }
+    // Your code here...
+}, 1000)
