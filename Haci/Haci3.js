59ccdb9621bdfb44b2205820b7050d8077151a8e
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-06-06 08:57:11 +0700

    Update Haci3.js

--- Haci/Haci3.js
+++ Haci/Haci3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hachi3
 // @namespace    http://tampermonkey.net/
-// @version      3.32
+// @version      3.33
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci3.js
@@ -89,7 +89,7 @@ var Backlist1 = "pemenang lomba";
 var Backlist2 = "rekap";
 var Backlist3 = "hasil";
 var Backlist4 = "room lomba freebet";
-var Backlist5 = "done";
+var Backlist5 = "mana";
 var Backlist6 = "result";
 var Backlist7 = "result";
 
