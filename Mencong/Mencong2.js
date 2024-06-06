13fd7f4a1e8dc9007f9b340f768f51a9a9ee1f77
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-06-06 10:26:31 +0700

    Update Mencong2.js

--- Mencong/Mencong2.js
+++ Mencong/Mencong2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MENCONG2
 // @namespace    http://tampermonkey.net/
-// @version      3.99
+// @version      3.100
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Mencong/Mencong2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Mencong/Mencong2.js
@@ -380,7 +380,7 @@ function clicksend() {
         clickEvent.initEvent ("mousedown", true, true);
         clicksendcoment.dispatchEvent (clickEvent);
         console.log("Comment Terkirim");
-        location.href = "about:blank"
+        
         /*Tekan TOMBOL SEND*/
     }
 }
