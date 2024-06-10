53dd2e600f6d81ea99dfbb58e4d98a0adacef046
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-06-10 16:23:32 +0700

    Update Yatim4.js

--- Yatim/Yatim4.js
+++ Yatim/Yatim4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Yatim4
 // @namespace    http://tampermonkey.net/
-// @version      3.21
+// @version      3.22
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Yatim/Yatim4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Yatim/Yatim4.js
@@ -387,7 +387,7 @@ function clicksend() {
 }
 
 function closer() {
-    setTimeout(function(){location.href = "about:blank"},800)
+    setTimeout(function(){location.href = "about:blank"},200)
     
     
 }
