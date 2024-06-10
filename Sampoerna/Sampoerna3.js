df4a8acde605eb5862f6017e359e8dc2df527f8b
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-06-10 16:21:23 +0700

    Update Sampoerna3.js

--- Sampoerna/Sampoerna3.js
+++ Sampoerna/Sampoerna3.js
@@ -389,7 +389,7 @@ function clicksend() {
 }
 
 function closer() {
-    setTimeout(function(){location.href = "about:blank"},800)
+    setTimeout(function(){location.href = "about:blank"},200)
     
     
 }
