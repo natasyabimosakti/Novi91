047b9fc57eb86a0b5ff590c93318570594dbdd73
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-06-12 00:32:48 +0700

    Update Zulf1.js

--- Zulf/Zulf1.js
+++ Zulf/Zulf1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW ZULF1
 // @namespace    http://tampermonkey.net/
-// @version      3.98
+// @version      3.99
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Zulf/Zulf1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Zulf/Zulf1.js
@@ -388,7 +388,7 @@ function clicksend() {
 }
 
 function closer() {
-    setTimeout(function(){location.href = "about:blank"},200)
+    setTimeout(function(){location.href = "about:blank"},50)
     
     
 }
