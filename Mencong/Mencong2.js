372004a8a64405916dbc30c364e1c2e0ff1360ba
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-06-12 00:27:17 +0700

    Update Mencong2.js

--- Mencong/Mencong2.js
+++ Mencong/Mencong2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MENCONG2
 // @namespace    http://tampermonkey.net/
-// @version      3.105
+// @version      3.106
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Mencong/Mencong2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Mencong/Mencong2.js
@@ -502,7 +502,7 @@ function clicksend() {
 }
 
 function closer() {
-    setTimeout(function(){location.href = "about:blank"},200)
+    setTimeout(function(){location.href = "about:blank"},50)
 
 
 }
