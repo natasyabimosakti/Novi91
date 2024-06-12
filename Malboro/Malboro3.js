be40dcc32d6877651576ed13d2c7d5325a39f3dc
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-06-12 08:55:38 +0700

    Update Malboro3.js

--- Malboro/Malboro3.js
+++ Malboro/Malboro3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MALBORO 3
 // @namespace    http://tampermonkey.net/
-// @version      3.27
+// @version      3.28
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro3.js
@@ -405,7 +405,7 @@ function clicksend() {
 }
 
 function closer() {
-    setTimeout(function(){location.href = "about:blank"},50)
+    setTimeout(function(){location.href = "about:blank"},5)
 
 
 }
