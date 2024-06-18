118511f89092d2e563fc3b3f915936cd7a4f0934
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-06-18 23:27:29 +0700

    Update Curut1.js

--- Curut/Curut1.js
+++ Curut/Curut1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW CURUT1
 // @namespace    http://tampermonkey.net/
-// @version      3.34
+// @version      3.35
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut1.js
@@ -15,7 +15,7 @@
 // ==/UserScript==
 
 
-var refresh = 60;
+var refresh = 40;
 
 
 
