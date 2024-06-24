6b6e6c6ac09e39c1eecd7a8f1b9978f5647cd269
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-06-24 14:48:29 +0700

    Update Mencong1.js

--- Mencong/Mencong1.js
+++ Mencong/Mencong1.js
@@ -37,7 +37,7 @@ var Comment5 = '#keitogel = (MAKIMUNG31) = 31*90*85';
 var namagroup6 = 'KIOST0T0';
 var Comment6 = 'KIOSTOTO=MATIO50=08*69*36'; 
 
-var namagroup7 = 'IYA';
+var namagroup7 = '453P';
 var Comment7 = 'ASEPTOGEL MAKDUNGDUNG 08*69*36'; 
 
 var namagroup8 = 'ANGKER';
