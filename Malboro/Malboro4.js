3f5c4d40a8ec1efd2fe17a4393cf346085ced650
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-06-24 14:46:45 +0700

    Update Malboro4.js

--- Malboro/Malboro4.js
+++ Malboro/Malboro4.js
@@ -34,7 +34,7 @@ var Comment5 = '#keitogel = (DUKUCABUL23) = 13*02*94';
 var namagroup6 = 'KIOST0T0';
 var Comment6 = 'KIOSTOTO=DANDANG34=54*88*68'; 
 
-var namagroup7 = 'IYA';
+var namagroup7 = '453P';
 var Comment7 = 'ASEPTOGEL DEDEKLESMONO 12*25*41'; 
 
 var namagroup8 = 'ANGKER';
