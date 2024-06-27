d70fd6403c4017690f943fd64a38784d2e447c89
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-06-27 10:05:10 +0700

    Update Mencong1.js

--- Mencong/Mencong1.js
+++ Mencong/Mencong1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MENCONG1
 // @namespace    http://tampermonkey.net/
-// @version      3.115
+// @version      3.116
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Mencong/Mencong1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Mencong/Mencong1.js
@@ -28,7 +28,7 @@ var Comment2 = 'IYATOTO MENTARI17 31*90*85';
 var namagroup3 = 'TIKTAK';
 var Comment3 = '#Tiktaktogel / MERANTAU41 / 01 , 81 , 42'; 
 
-var namagroup4 = 'GAIB';
+var namagroup4 = 'GA1B';
 var Comment4 = 'GAIB4D=MERATIH14=38*22*57'; 
 
 var namagroup5 = 'KEITOGEL';
