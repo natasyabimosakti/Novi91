b882e4009702dee384a68bb94fbabab1539da362
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-13 22:57:13 +0700

    Create mbasic room insider.js

---
+++ mbasic room insider.js
@@ -0,0 +1,117 @@
+// ==UserScript==
+// @name         ROOM Insider
+// @namespace    http://tampermonkey.net/
+// @version      3.05
+// @description  try to take over the world!
+// @updateURL
+// @downloadURL
+// @author       You
+// @match        http*://*/*
+// @run-at       document-end
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        GM.setValue
+// @grant        GM.getValue
+// @grant        window.close
+// ==/UserScript==
+var refresh = 40;
+
+
+
+var admin = ["Siâo","habib","sofia","neng","arxidi","che","aldi","nino","sofia","sonia","serena","alde","puput","mad","hefi","dika","iyatoto","adm","celsia","jne","kotna","yoky","audi","lianda","salsabila","yohana","wok","bastian","hoihai","tink","sinta","kembar","laura","ayesha","tiktak","nella","novi","sandiego","nasution","ratu","priyan","san","ria","sanjaya","siska","Baru","aditia","keitogel","safar","mahendra","multi","mariana","neman","tatang","dewi","primus","roy","dewi","melati","kumbara","dentoto","ananda","cinta","lina","icha","bobby","sanchez","oscar","rendy"];
+
+
+var keyword1 = "ROOM"
+var keyword2 = "𝗥𝗢𝗢𝗠"
+var keyword3 = "LOMBA"
+var keyword4 = "𝗟𝗢𝗠𝗕𝗔"
+var keyword5 = "𝐋𝐎𝐌𝐁𝐀"
+var keyword6 = "LIMBA"
+var keyword7 = "ROM"
+var keyword8 = "R00M"
+var keyword9 = "R0M"
+var keyword10 = "🎱"
+
+var Backlist1 = "pemenang lomba";
+var Backlist2 = "rekap";
+var Backlist3 = "hasil";
+var Backlist4 = "room lomba freebet";
+var Backlist5 = "prediksi";
+var Backlist6 = "result";
+var Backlist7 = "result";
+
+var myInterval = setInterval(function(){
+
+
+    var ceknamagroup
+    var ceknamagroup1
+    var ceknamagroup2
+    var ceknamagroup3
+    var ceknamagroup4
+    'use strict';
+    if( document.querySelectorAll("[data-mcomponent='ServerTextArea']")[4]){
+        ceknamagroup = document.querySelectorAll("[data-mcomponent='ServerTextArea']")[4].textContent;
+    }
+    for (let ntv = 0; ntv < document.querySelectorAll("[role='article']").length; ntv++) {
+        if (document.querySelectorAll("[role='article']")[ntv]){
+            // Nama FB
+            var namafb = document.querySelectorAll("[role='article']")[ntv].querySelectorAll("[role='presentation']")[0];
+            //Jam
+            var jamposting = document.querySelectorAll("[role='article']")[ntv].children[1].getElementsByTagName("abbr")[0];
+            //Postingan
+            var postingan =document.querySelectorAll("[role='article']")[ntv].children[0];
+            //Comment Box
+            var getlink = document.querySelectorAll("[role='article']")[ntv].children[1].children[1].getElementsByTagName("a")[2];
+
+
+            // Cek Jam
+            var ret = jamposting.textContent.replace(/  Admin   |  Moderator   /g, "");
+            if (ret.includes("Baru")||ret == "1 mnt"||ret == "2 mnt"||ret == "3 mnt"||ret == "4 mnt"||ret == "5 mnt"){
+                console.log("Jam Ditemukan " + ret)
+                if(postingan.textContent.toLowerCase().includes(keyword1.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(keyword2.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(keyword3.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(keyword4.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(keyword5.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(keyword6.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(keyword7.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(keyword8.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(keyword9.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(keyword10.toLowerCase())){
+                    console.log("Keyword Ditemukan " + postingan.textContent);
+                    // Cek Backlist
+                    if(postingan.textContent.toLowerCase().includes(Backlist1.toLowerCase())
+                       ||postingan.textContent.toLowerCase().includes(Backlist2.toLowerCase())
+                       ||postingan.textContent.toLowerCase().includes(Backlist3.toLowerCase())
+                       ||postingan.textContent.toLowerCase().includes(Backlist4.toLowerCase())
+                       ||postingan.textContent.toLowerCase().includes(Backlist5.toLowerCase())
+                       ||postingan.textContent.toLowerCase().includes(Backlist6.toLowerCase())
+                       ||postingan.textContent.toLowerCase().includes(Backlist7.toLowerCase())){
+                        console.log("Terdaftar Backlist...!  ");
+                        return;
+                    }
+                    console.log("Proses dilanjutkan tidak ada Backlist");
+                    // Cek Admin
+                    for (var adm in admin){
+                        if(namafb.textContent.toLowerCase().includes(admin[adm].toLowerCase())){
+                            // Tampilkan Siapa Yang Memposting
+                            if(jamposting.textContent.toLowerCase().includes("admin")||jamposting.textContent.toLowerCase().includes("moderator")){
+                                console.log("Admin yang Memosting = Admin/Moderator");
+                            }else{
+                                console.log("Admin yang Memosting = " + admin[adm]);
+                            }
+                            // Click Comment Box
+                             clearInterval(myInterval);
+                            location.href = getlink.href
+
+                      
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+}, 10)
