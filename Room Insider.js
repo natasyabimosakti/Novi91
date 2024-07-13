416fb047d73c7b04118389f798dadf8a8deb7b76
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-13 17:27:20 +0700

    Create Room Insider.js

---
+++ Room Insider.js
@@ -0,0 +1,181 @@
+// ==UserScript==
+// @name         ROOM Insider
+// @namespace    http://tampermonkey.net/
+// @version      3.05
+// @description  try to take over the world!
+// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia1.js
+// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia1.js
+// @author       You
+// @match        http*://*/*
+// @run-at       document-end
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        GM.setValue
+// @grant        GM.getValue
+// @grant        window.close
+// ==/UserScript==
+var refresh = 40;
+
+
+
+var admin = [" ","aldi","artha","adelia","ananda","aditia","andri","ayesha","aurel","alde","adm","audi","andy","ayesha",
+             "brian","boboho","bobby","bonar","bella","bastian",
+             "cinta","calvin","celsia","celine",
+             "denis","dollar","dewa","dewi","dinda","dika","dea",
+             "echa","erika","elly","erwin",
+             "farah","febrian","fiana","farid","fahresa",
+             "gretha",
+             "herfizah","hana","hoihai","hefi","habib",
+             "inisial","intan","icha","irfan",
+             "jhone","jess","jovanka","jenifer","jihan","jesika","je pe",
+             "kumbara","kemon","kayla","katty","kendri","kembar","kotna","kiky",
+             "lehman","lianda","laura","leon","lidya","larissa","leksa","lina",
+             "manu","mesa","mardia","maes","mad","miranda","melati","minion","mariana","mahendra","multi","megaways",
+             "nadila","neng","naura","nathaya","nella","neman","novi",
+             "oun","oscar","otong",
+             "puput","primus","priyan",
+             "ratu","roy","rendy","ria","ruto","riko","rano","robby",
+             "sanchez","siska","safar","sanjaya","san","sandiego","sloter","sinta","slooter","sabrina","salsabila","sintia","sejitu",
+             "tink","thonex","tiara","tania","tag",
+             "ujen",
+             "vonny","viona","virna",
+             "wahid","wahzo","wok","wndt","wiena",
+             "yoky","yasmine","yohana",
+             "zurro"];
+
+var keyword1 = "ROOM"
+var keyword2 = "𝗥𝗢𝗢𝗠"
+var keyword3 = "LOMBA"
+var keyword4 = "𝗟𝗢𝗠𝗕𝗔"
+var keyword5 = "𝐋𝐎𝐌𝐁𝐀"
+var keyword6 = "LIMBA"
+var keyword7 = "ROM"
+var keyword8 = "R00M"
+var keyword9 = "R0M"
+var keyword10 = "🎱"
+
+var Backlist1 = "pemenang lomba";
+var Backlist2 = "rekap";
+var Backlist3 = "hasil";
+var Backlist4 = "room lomba freebet";
+var Backlist5 = "prediksi";
+var Backlist6 = "result";
+var Backlist7 = "result";
+
+var myrefresh = setInterval(function(){
+    var urutkan = document.querySelectorAll("[data-mcomponent='ServerTextArea']");
+    var urutkan2 = document.querySelectorAll("[data-mcomponent='TextArea']");
+    var waktupost = document.getElementsByClassName("native-text");
+    window.scrollTo(0, 2000);
+    if (document.readyState === "complete") {
+        for (var coke = 0; coke < urutkan2.length; coke++) {
+            if (urutkan2[coke].textContent.includes("URUTKAN")) {
+
+                urutkan2[coke].click()
+
+
+            }
+        }
+
+    }
+    if (document.readyState === "complete") {
+        for (var cok = 0; cok < urutkan.length; cok++) {
+            if(urutkan[cok].textContent.includes("URUTKAN")) {
+                urutkan[cok].click()
+
+            }
+        }
+
+    }
+
+
+    if (document.readyState === "complete") {
+        for (var coki = 0; coki < waktupost.length; coki++) {
+            if(waktupost[coki].textContent.includes("Aktivitas")) {
+                waktupost[coki].click()
+
+            }
+        }
+
+    }
+}, refresh * 10)
+
+var myInterval = setInterval(function(){
+
+
+    var ceknamagroup
+    var ceknamagroup1
+    var ceknamagroup2
+    var ceknamagroup3
+    var ceknamagroup4
+    'use strict';
+    if( document.querySelectorAll("[data-mcomponent='ServerTextArea']")[4]){
+        ceknamagroup = document.querySelectorAll("[data-mcomponent='ServerTextArea']")[4].textContent;
+    }
+    console.log(" ");
+    for (let ntv = 0; ntv < document.querySelectorAll('[data-tracking-duration-id').length; ntv++) {
+        if (document.querySelectorAll('[data-tracking-duration-id')[ntv]){
+            // Nama FB
+            var namafb = document.querySelectorAll('[data-tracking-duration-id]')[ntv].getElementsByClassName('native-text')[0];
+            //Jam
+            var jamposting = document.querySelectorAll('[data-tracking-duration-id]')[ntv].getElementsByClassName('native-text')[1];
+            //Postingan
+            var postingan =document.querySelectorAll('[data-tracking-duration-id')[ntv].getElementsByClassName('native-text')[3];
+            //Comment Box
+            var datacommentbox = document.querySelectorAll('[data-tracking-duration-id')[ntv].getElementsByClassName('native-text').length -1
+            var commentbox = document.querySelectorAll('[data-tracking-duration-id')[ntv].getElementsByClassName('native-text')[datacommentbox];
+
+            // Cek Jam
+            var ret = jamposting.textContent.replace(/  Admin   |  Moderator   /g, "");
+            if (ret.includes("Baru")||ret.slice(0,7).includes("1 menit")||ret.slice(0,7).includes("2 menit")||ret.slice(0,7).includes("3 menit")||ret.slice(0,7).includes("4 menit")||ret.slice(0,7).includes("4 menit")){
+                console.log("Jam Ditemukan " + ret)
+                if(postingan.textContent.toLowerCase().includes(keyword1.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(keyword2.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(keyword3.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(keyword4.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(keyword5.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(keyword6.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(keyword7.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(keyword8.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(keyword9.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(keyword10.toLowerCase())){
+                    console.log("Keyword Ditemukan " + postingan.textContent);
+                    // Cek Backlist
+                    if(postingan.textContent.toLowerCase().includes(Backlist1.toLowerCase())
+                       ||postingan.textContent.toLowerCase().includes(Backlist2.toLowerCase())
+                       ||postingan.textContent.toLowerCase().includes(Backlist3.toLowerCase())
+                       ||postingan.textContent.toLowerCase().includes(Backlist4.toLowerCase())
+                       ||postingan.textContent.toLowerCase().includes(Backlist5.toLowerCase())
+                       ||postingan.textContent.toLowerCase().includes(Backlist6.toLowerCase())
+                       ||postingan.textContent.toLowerCase().includes(Backlist7.toLowerCase())){
+                        console.log("Terdaftar Backlist...!  ");
+                        return;
+                    }
+                    console.log("Proses dilanjutkan tidak ada Backlist");
+                    // Cek Admin
+                    for (var adm in admin){
+                        if(namafb.textContent.toLowerCase().includes(admin[adm].toLowerCase())||jamposting.textContent.toLowerCase().includes("admin")||jamposting.textContent.toLowerCase().includes("moderator")){
+                            // Tampilkan Siapa Yang Memposting
+                            if(jamposting.textContent.toLowerCase().includes("admin")||jamposting.textContent.toLowerCase().includes("moderator")){
+                                console.log("Admin yang Memosting = Admin/Moderator");
+                            }else{
+                                console.log("Admin yang Memosting = " + admin[adm]);
+                            }
+                            // Click Comment Box
+                            postingan.click()
+                            clearInterval(myInterval);
+                            clearInterval(myrefresh);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+}, 10)
+
+
+
+
