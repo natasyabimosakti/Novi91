3c186d90c343a3420f56b70a3d22db5d79bdfe6d
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-15 21:26:14 +0700

    Update AutoJoin.js

--- AutoJoin.js
+++ AutoJoin.js
@@ -15,38 +15,11 @@
 // ==/UserScript==
 
 var refresh = 400;
-var cekjoin1 = document.getElementsByClassName('native-text')[10]
-var cekjoin2 = document.getElementsByClassName('native-text')[11]
-var cekjoin3 = document.getElementsByClassName('native-text')[12]
-var cekjoin4 = document.getElementsByClassName('native-text')[13]
-var cekjoin5 = document.getElementsByClassName('native-text')[14]
+ 
 setInterval(function(){
     document.querySelectorAll("[aria-label='Bergabung grup']")[0].click()
 
-    if(cekjoin1||cekjoin2||cekjoin3||cekjoin4||cekjoin5){
-        if(cekjoin1.textContent.includes("Bergabung grup")==true) {
-            document.getElementsByClassName('native-text')[10].click();
-            window.location.reload()
-        }
-        if(cekjoin2.textContent.includes("Bergabung grup")==true) {
-            document.getElementsByClassName('native-text')[11].click();
-            window.location.reload()
-        }
-        if(cekjoin3.textContent.includes("Bergabung grup")==true) {
-            document.getElementsByClassName('native-text')[12].click();
-            window.location.reload()
-        }
-        if(cekjoin4.textContent.includes("Bergabung grup")==true) {
-            document.getElementsByClassName('native-text')[13].click();
-            window.location.reload()
-        }
-        if(cekjoin5.textContent.includes("Bergabung grup")==true) {
-            document.getElementsByClassName('native-text')[14].click();
-            window.location.reload()
-        }
-
-
-    }
+    
     if(document.URL.includes('search')){
         if(document.getElementsByClassName('native-text')[2]){
             if(document.getElementsByClassName('native-text')[2].textContent.includes("grup")){
