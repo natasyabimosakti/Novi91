e2212dbece2625ac9222046f8b5c6a95861a81e1
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-16 22:17:33 +0700

    Update Perkutut4.js

--- Perkutut/Perkutut4.js
+++ Perkutut/Perkutut4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Perkutut4
 // @namespace    http://tampermonkey.net/
-// @version      3.47
+// @version      3.48
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Perkutut/Perkutut4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Perkutut/Perkutut4.js
@@ -113,7 +113,7 @@ var admin = ["aldi","artha","adelia","ananda","aditia","andri","ayesha","aurel",
              "oun","oscar","otong",
              "puput","primus","priyan",
              "ratu","roy","rendy","ria","ruto","riko","rano","robby",
-             "sanchez","sutanti","siska","safar","sanjaya","san","sandiego","sloter","sinta","slooter","sabrina","salsabila","sintia","sejitu",
+             "sanchez","sutanti","siska","safar","sanjaya","san","sandiego","sloter","sinta","selly","slooter","sabrina","salsabila","sintia","sejitu",
              "tink","thonex","tiara","tania","tag",
              "ujen",
              "vonny","viona","virna",
