e8a47e20af79ba7e24fd80406de3b97ceb1d6dc2
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-19 03:04:34 +0700

    Update Sampoerna1.js

--- Sampoerna/Sampoerna1.js
+++ Sampoerna/Sampoerna1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sampoerna1
 // @namespace    http://tampermonkey.net/
-// @version      3.43
+// @version      3.44
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna1.js
@@ -19,8 +19,8 @@
 /*======================================================================Paste Script Tampermonkey di sini===============================================================*/
 
 
-var namagroup1 = 'RITOGEL';
-var Comment1 = '#RITOGEL(XERBIA13)43*31*29'; 
+var namagroup1 = 'YAO';
+var Comment1 = '#YAOTOGEL XERBIA13 43*31*29'; 
 
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( XENON88 ) : 89*76*27'; 
