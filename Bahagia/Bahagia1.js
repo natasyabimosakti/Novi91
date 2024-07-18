98611245a19645f72f26bb1621c0bce1799fa210
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-19 02:54:25 +0700

    Update Bahagia1.js

--- Bahagia/Bahagia1.js
+++ Bahagia/Bahagia1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bahagia1
 // @namespace    http://tampermonkey.net/
-// @version      3.07
+// @version      3.08
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia1.js
@@ -15,8 +15,8 @@
 // ==/UserScript==
 
 
-var namagroup1 = 'RITOGEL';
-var Comment1 = '#RITOGEL(ASTRI29)56*38*31'; 
+var namagroup1 = 'YAO';
+var Comment1 = '#YAOTOGEL ASTRI29 56*38*31'; 
 
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( ASRIN32 ) : 59*01*11'; 
