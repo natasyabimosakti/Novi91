1a9ca33ced1a1ac488386b846c4f55a79557f9d5
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-25 17:13:49 +0700

    Update Haci3.js

--- Haci/Haci3.js
+++ Haci/Haci3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hachi3
 // @namespace    http://tampermonkey.net/
-// @version      3.53
+// @version      3.54
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci3.js
@@ -50,10 +50,10 @@ var namagroup10 = 'SLOTO';
 var Comment10 = '#SLOTOGEL (SIKLUWER) : 56*43*31'; 
 
 var namagroup11 = 'KEBAYA';
-var Comment11 = '(KEBAYA4D) = (SINTONG19) 50*91*21'; 
+var Comment11 = '(KEBAYA4D) = (SINTONG19) 15*30*69'; 
 
 var namagroup12 = 'BESTOTO';
-var Comment12 = 'SIMBAHMU : 15*30*69 #BESTOTO88'; 
+var Comment12 = 'SIMBAHMU : 50*91*21 #BESTOTO88'; 
 
 var namagroup13 = 'GOHT0G3L';
 var Comment13 = 'GOHTOGEL=SIMPAL19=35*12*78'; 
