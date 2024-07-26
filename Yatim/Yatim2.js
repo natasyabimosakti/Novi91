cbdf2f8d78d7289ed20971e77dc599715af3b5ce
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-27 06:33:15 +0700

    Update Yatim2.js

--- Yatim/Yatim2.js
+++ Yatim/Yatim2.js
@@ -16,7 +16,56 @@
 
 
 
+var namagroup1 = 'SHIOKELINCI';
+var Comment1 = '#shiokelinci4d*FERTULE*11*20'; 
 
+var namagroup2 = 'BUKU';
+var Comment2 = 'IYATOTO FERCUKZ 33*92*66'; 
+
+var namagroup3 = 'TIKTAK';
+var Comment3 = '#Tiktaktogel / FERPEKZ / 12 , 86 , 68'; 
+
+var namagroup4 = 'GAIB';
+var Comment4 = 'GAIB4D=FERIBS=71*96*75'; 
+
+var namagroup5 = 'KEITOGEL';
+var Comment5 = '#keitogel = (FERKEII) = 78*46*38'; 
+
+var namagroup6 = 'KIOST0T0';
+var Comment6 = 'KIOSTOTO=FERYOS=67*98*38'; 
+
+var namagroup7 = '453P VIP';
+var Comment7 = 'ASEPTOGEL FERCAU 02*09*60'; 
+
+var namagroup8 = 'ANGKER';
+var Comment8 = 'ANGKER4D=FERKERR=58*88*41'; 
+
+var namagroup9 = 'GILA';
+var Comment9 = 'GILA4D=FERGILL=02*09*60'; 
+
+var namagroup10 = 'JNE';
+var Comment10 = '#JNETOTO(FERJENS)*67*98*05'; 
+
+var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
+var Comment11 = '#TA*FERRAT*78*46*77*'; 
+
+var namagroup12 = 'MASTER KUY';
+var Comment12 = 'TOGELKUY FERKOYY 71*96*75'; 
+
+var namagroup13 = 'TOYIBSLOT';
+var Comment13 = '#TOYIBSLOT ( FERYEBS ) : 12*86*68'; 
+
+var namagroup14 = 'DENTOTO';
+var Comment14 = '#DENTOTO 33*92*66 ( FERSIP )'; 
+
+var namagroup15 = 'HATRICK';
+var Comment15 = 'VESPATOGEL (FERPERS)  11*20*83'; 
+
+var namagroup16 = 'LOMBA TEBAK';
+var Comment16 = 'TOYA (FERTULE) 58*88*41'; 
+
+var namagroup17 = 'CIVIC';
+var Comment17 = '#CIVICTOTO(FERJENS) = 77*05';
 
 var namagroup18 = 'Jawatengah';
 var Comment18 = 'Tester';
