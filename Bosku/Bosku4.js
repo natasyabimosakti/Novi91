4b90738836aad8f5c1d4fe44ff418e5b7052cad6
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-27 06:04:08 +0700

    Update Bosku4.js

--- Bosku/Bosku4.js
+++ Bosku/Bosku4.js
@@ -17,7 +17,56 @@
 
 
 
+var namagroup1 = 'SHIOKELINCI';
+var Comment1 = '#shiokelinci4d*ZONEST2*06*28'; 
 
+var namagroup2 = 'BUKU';
+var Comment2 = 'IYATOTO ZOROK73 16*81*71'; 
+
+var namagroup3 = 'TIKTAK';
+var Comment3 = '#Tiktaktogel / ZOMBIE27 / 90 , 79 , 09'; 
+
+var namagroup4 = 'GAIB';
+var Comment4 = 'GAIB4D=ZORAN36=15*98*86'; 
+
+var namagroup5 = 'KEITOGEL';
+var Comment5 = '#keitogel = (ZONIAK63) = 37*13*02'; 
+
+var namagroup6 = 'KIOST0T0';
+var Comment6 = 'KIOSTOTO=ZOMING82=93*62*42'; 
+
+var namagroup7 = '453P VIP';
+var Comment7 = 'ASEPTOGEL ZONATIK73 21*73*87'; 
+
+var namagroup8 = 'ANGKER';
+var Comment8 = 'ANGKER4D=ZONATING=34*76*24'; 
+
+var namagroup9 = 'GILA';
+var Comment9 = 'GILA4D=ZOWOK26=34*76*24'; 
+
+var namagroup10 = 'JNE';
+var Comment10 = '#JNETOTO(ZOKET72)*21*73*63'; 
+
+var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
+var Comment11 = '#TA*ZOKUTU72*93*62*63*'; 
+
+var namagroup12 = 'MASTER KUY';
+var Comment12 = 'TOGELKUY ZOWEKNI82 37*13*02'; 
+
+var namagroup13 = 'TOYIBSLOT';
+var Comment13 = '#TOYIBSLOT ( ZOWINDO ) : 15*98*86'; 
+
+var namagroup14 = 'DENTOTO';
+var Comment14 = '#DENTOTO 90*79*09 ( ZORLETA )'; 
+
+var namagroup15 = 'HATRICK';
+var Comment15 = 'VESPATOGEL (ZONIAK26)  16*81*71'; 
+
+var namagroup16 = 'LOMBA TEBAK';
+var Comment16 = 'TOYA (ZONIAK63) 06*28*27'; 
+
+var namagroup17 = 'CIVIC';
+var Comment17 = '#CIVICTOTO(ZOMING82) = 87*24';
 
 var namagroup18 = 'Jawatengah';
 var Comment18 = 'Tester';
