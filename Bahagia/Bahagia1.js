e3f5a2f55dc737169b7f441812087d532ff25c41
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-29 00:37:28 +0700

    Update Bahagia1.js

--- Bahagia/Bahagia1.js
+++ Bahagia/Bahagia1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bahagia1
 // @namespace    http://tampermonkey.net/
-// @version      3.10
+// @version      3.11
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia1.js
@@ -107,11 +107,11 @@ var refresh = 70;
 
 
 
-var admin = ["Siâo","aldi","alenta","artha","adelia","ananda","aditia","andri","ayesha","aurel","alde","adm","audi","asmani","andy","ayesha",
+var admin = ["Siâo","aldi","aprilia","alenta","artha","adelia","ananda","aditia","andri","ayesha","aurel","alde","adm","audi","asmani","andy","ayesha",
              "brian","boboho","bobby","bonar","bella","bastian","bungaps",
-             "cinta","calvin","celsia","celine",
+             "cinta","calvin","celsia","cila","cindy","chintya","carole","celine",
              "denis","dollar","dewa","dewi","dinda","dika","dea",
-             "echa","erika","elly","erwin",
+             "echa","erika","elly","eugene","erwin",
              "farah","febrian","fiana","farid","fahresa",
              "gretha",
              "herfizah","hana","hoihai","hefi","habib",
@@ -122,7 +122,7 @@ var admin = ["Siâo","aldi","alenta","artha","adelia","ananda","aditia","andri",
              "manu","mesa","mardia","mila","maes","mad","miranda","melati","minion","mariana","mahendra","multi","megaways",
              "nadila","neng","naura","nathaya","nick","nella","neman","novi",
              "oun","oscar","otong",
-             "puput","primus","pura","priyan",
+             "puput","primus","payton","pura","priyan",
              "ratu","roy","rendy","ria","ruto","riko","rano","robby",
              "sanchez","selly","sutanti","siska","safar","sanjaya","san","sandiego","sloter","sinta","slooter","sabrina","salsabila","sintia","sejitu",
              "tink","thonex","tiara","tania","tag",
