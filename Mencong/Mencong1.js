ba39cc986915fefef27cc42abf22ed1b82b6d3f3
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-28 23:54:07 +0700

    Update Mencong1.js

--- Mencong/Mencong1.js
+++ Mencong/Mencong1.js
@@ -44,8 +44,8 @@ var Comment8 = 'ANGKER4D=MACRONI52=51*35*01';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=MEROJAYA51=51*35*01'; 
 
-var namagroup10 = 'L0MBA T0T0';
-var Comment10 = '#TOTO4D (MELAMUN44) 55*90*06'; 
+var namagroup10 = 'PEJUANG';
+var Comment10 = '#JNETOTO(MELAMUN44)*55*90*06'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
 var Comment11 = '#TA*MAUEWEK5X*17*95*61*'; 
