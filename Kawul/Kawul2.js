5b2a00403977043f9c6e89f873c9df25953be538
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-29 00:43:29 +0700

    Update Kawul2.js

--- Kawul/Kawul2.js
+++ Kawul/Kawul2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kawul2
 // @namespace    http://tampermonkey.net/
-// @version      3.41
+// @version      3.42
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul2.js
@@ -104,15 +104,11 @@ var id17 = await GM.getValue(17);
 
 
 
-var refresh = 70;
-
-
-
-var admin = ["Siâo","aldi","alenta","artha","adelia","ananda","aditia","andri","ayesha","aurel","alde","adm","audi","asmani","andy","ayesha",
+var admin = ["Siâo","aldi","aprilia","alenta","artha","adelia","ananda","aditia","andri","ayesha","aurel","alde","adm","audi","asmani","andy","ayesha",
              "brian","boboho","bobby","bonar","bella","bastian","bungaps",
-             "cinta","calvin","celsia","celine",
+             "cinta","calvin","celsia","cila","cindy","chintya","carole","celine",
              "denis","dollar","dewa","dewi","dinda","dika","dea",
-             "echa","erika","elly","erwin",
+             "echa","erika","elly","eugene","erwin",
              "farah","febrian","fiana","farid","fahresa",
              "gretha",
              "herfizah","hana","hoihai","hefi","habib",
@@ -123,7 +119,7 @@ var admin = ["Siâo","aldi","alenta","artha","adelia","ananda","aditia","andri",
              "manu","mesa","mardia","mila","maes","mad","miranda","melati","minion","mariana","mahendra","multi","megaways",
              "nadila","neng","naura","nathaya","nick","nella","neman","novi",
              "oun","oscar","otong",
-             "puput","primus","pura","priyan",
+             "puput","primus","payton","pura","priyan",
              "ratu","roy","rendy","ria","ruto","riko","rano","robby",
              "sanchez","selly","sutanti","siska","safar","sanjaya","san","sandiego","sloter","sinta","slooter","sabrina","salsabila","sintia","sejitu",
              "tink","thonex","tiara","tania","tag",
@@ -132,7 +128,6 @@ var admin = ["Siâo","aldi","alenta","artha","adelia","ananda","aditia","andri",
              "wahid","wahzo","wok","wndt","wiena",
              "yoky","yasmine","yao","yohana",
              "zurro"];
-
 
 
 var keyword1 = "ROOM"
