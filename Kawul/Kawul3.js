03a150ddde45a59fdcb8d2af38f917ce83933e77
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-28 22:40:32 +0700

    Update Kawul3.js

--- Kawul/Kawul3.js
+++ Kawul/Kawul3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kawul3
 // @namespace    http://tampermonkey.net/
-// @version      3.10
+// @version      3.41
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul3.js
@@ -24,8 +24,8 @@ var Comment2 = 'K86TOTO ( JOMPLANG ) : 90*23*65';
 var namagroup3 = 'PUSAT LOMBA';
 var Comment3 = 'Shiotogel4d (JONTOR6) 64*48*86'; 
 
-var namagroup4 = 'SEJ1TU';
-var Comment4 = '#SEJITU ( JOKOBODO ) : 43*53*16'; 
+var namagroup4 = 'TEMPO';
+var Comment4 = 'JOKOBODO : 43*53*16 #TEMPOTOTO'; 
 
 var namagroup5 = 'RNR';
 var Comment5 = '‌#RNR303(JOVINDUL) : 20*27*60'; 
