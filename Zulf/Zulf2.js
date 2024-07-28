e5ce5ab8e6a711635c9210c71cd97787f339bd9a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-29 00:00:24 +0700

    Update Zulf2.js

--- Zulf/Zulf2.js
+++ Zulf/Zulf2.js
@@ -43,8 +43,8 @@ var Comment8 = 'ANGKER4D=KORBANSLOT11=12*86*68';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=SUKASUKA12=12*86*68'; 
 
-var namagroup10 = 'L0MBA T0T0';
-var Comment10 = '#TOTO4D (RAJANYAJP12) 33*92*66'; 
+var namagroup10 = 'PEJUANG';
+var Comment10 = '#JNETOTO(RAJANYAJP12)*33*92*66'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
 var Comment11 = '#TA*ALBAKU13*11*20*38*'; 
