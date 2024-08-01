1394b08f75e4695e140e70fa7220453b5d3a9c2b
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-02 02:42:21 +0700

    Update Yatim3.js

--- Yatim/Yatim3.js
+++ Yatim/Yatim3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Yatim3
 // @namespace    http://tampermonkey.net/
-// @version      3.42
+// @version      3.43
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Yatim/Yatim3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Yatim/Yatim3.js
