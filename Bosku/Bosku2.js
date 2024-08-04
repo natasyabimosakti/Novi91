d17eca1d7e8ac41e3ab82907b88bc506b9056798
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-04 21:49:54 +0700

    Update Bosku2.js

--- Bosku/Bosku2.js
+++ Bosku/Bosku2.js
@@ -129,16 +129,16 @@ var myrefresh = setInterval(function(){
     var urutkan = document.querySelectorAll("[data-mcomponent='ServerTextArea']");
     var waktupost = document.getElementsByClassName("native-text");
     window.scrollTo(0, 2000);
-  if(!document.querySelectorAll("[role='presentation']")[0]){
-    if (document.readyState === "complete") {
-        for (var cok = 0; cok < urutkan.length; cok++) {
-            if(urutkan[cok].textContent.includes("URUTKAN")) {
-                urutkan[cok].click()
+    if(!document.querySelectorAll("[role='presentation']")[0]){
+        if (document.readyState === "complete") {
+            for (var cok = 0; cok < urutkan.length; cok++) {
+                if(urutkan[cok].textContent.includes("URUTKAN")) {
+                    urutkan[cok].click()
+                }
             }
         }
-    }
 
-       }
+    }
     if(document.querySelectorAll("[role='presentation']")[0]){
         if (document.readyState === "complete") {
             for (var coki = 0; coki < waktupost.length; coki++) {
@@ -207,7 +207,6 @@ var myInterval = setInterval(function(){
                             if(document.querySelectorAll("[data-tracking-duration-id]")[ntv].children[0].children[0]){
                                 console.log("ada")
                                 document.querySelectorAll("[data-tracking-duration-id]")[ntv].children[0].children[0].click()
-                                clearInterval(myInterval);
                                 clearInterval(myrefresh);
                                 return
                             }
@@ -220,11 +219,7 @@ var myInterval = setInterval(function(){
             }
         }
     }
-}, 10)
 
-
-
-var commentanku = setInterval(function(){
     try {
         var ceknamagroup
         'use strict';
@@ -238,6 +233,7 @@ var commentanku = setInterval(function(){
                         GM.setValue( 1,1);
                         document.getElementsByClassName("internal-input")[0].value = Comment1;
                         console.log("Sudah Comment")
+                        clearInterval(myInterval);
                         clicksend();
                     }
                 }
@@ -248,6 +244,7 @@ var commentanku = setInterval(function(){
                         /*cek nama group dan tulis commntar*/
                         document.getElementsByClassName("internal-input")[0].value = Comment2;
                         console.log("Sudah Comment")
+                        clearInterval(myInterval);
                         clicksend();
                     }
                 }
@@ -258,6 +255,7 @@ var commentanku = setInterval(function(){
                         /*cek nama group dan tulis commntar*/
                         document.getElementsByClassName("internal-input")[0].value = Comment3;
                         console.log("Sudah Comment")
+                        clearInterval(myInterval);
                         clicksend();
                     }
                 }
@@ -268,6 +266,7 @@ var commentanku = setInterval(function(){
                         GM.setValue( 4,1);
                         document.getElementsByClassName("internal-input")[0].value = Comment4;
                         console.log("Sudah Comment")
+                        clearInterval(myInterval);
                         clicksend();
                     }
                 }
@@ -278,8 +277,9 @@ var commentanku = setInterval(function(){
                         GM.setValue( 5,1);
                         document.getElementsByClassName("internal-input")[0].value = Comment5;
                         console.log("Sudah Comment")
+                        clearInterval(myInterval);
                         clicksend();
-                        return;
+
                     }
                 }
                 if (ceknamagroup[namag].textContent.includes(namagroup6) ) {
@@ -289,6 +289,7 @@ var commentanku = setInterval(function(){
                         GM.setValue( 6,1);
                         document.getElementsByClassName("internal-input")[0].value = Comment6;
                         console.log("Sudah Comment")
+                        clearInterval(myInterval);
                         clicksend();
                     }
                 }
@@ -299,6 +300,7 @@ var commentanku = setInterval(function(){
                         GM.setValue( 7,1);
                         document.getElementsByClassName("internal-input")[0].value = Comment7;
                         console.log("Sudah Comment")
+                        clearInterval(myInterval);
                         clicksend();
                     }
                 }
@@ -309,6 +311,7 @@ var commentanku = setInterval(function(){
                         GM.setValue( 8,1);
                         document.getElementsByClassName("internal-input")[0].value = Comment8;
                         console.log("Sudah Comment")
+                        clearInterval(myInterval);
                         clicksend();
                     }
                 }
@@ -319,6 +322,7 @@ var commentanku = setInterval(function(){
                         GM.setValue( 9,1);
                         document.getElementsByClassName("internal-input")[0].value = Comment9;
                         console.log("Sudah Comment")
+                        clearInterval(myInterval);
                         clicksend();
                     }
                 }
@@ -332,6 +336,7 @@ var commentanku = setInterval(function(){
                         GM.setValue( 10,1);
                         document.getElementsByClassName("internal-input")[0].value = Comment10;
                         console.log("Sudah Comment")
+                        clearInterval(myInterval);
                         clicksend();
                     }
                 }
@@ -342,6 +347,7 @@ var commentanku = setInterval(function(){
                         GM.setValue( 11,1);
                         document.getElementsByClassName("internal-input")[0].value = Comment11;
                         console.log("Sudah Comment")
+                        clearInterval(myInterval);
                         clicksend();
                     }
                 }
@@ -352,6 +358,7 @@ var commentanku = setInterval(function(){
                         GM.setValue( 12,1);
                         document.getElementsByClassName("internal-input")[0].value = Comment12;
                         console.log("Sudah Comment")
+                        clearInterval(myInterval);
                         clicksend();
                     }
                 }
@@ -362,6 +369,7 @@ var commentanku = setInterval(function(){
                         GM.setValue( 13,1);
                         document.getElementsByClassName("internal-input")[0].value = Comment13;
                         console.log("Sudah Comment")
+                        clearInterval(myInterval);
                         clicksend();
                     }
                 }
@@ -372,6 +380,7 @@ var commentanku = setInterval(function(){
                         GM.setValue( 14,1);
                         document.getElementsByClassName("internal-input")[0].value = Comment14;
                         console.log("Sudah Comment")
+                        clearInterval(myInterval);
                         clicksend();
                     }
                 }
@@ -382,6 +391,7 @@ var commentanku = setInterval(function(){
                         GM.setValue( 15,1);
                         document.getElementsByClassName("internal-input")[0].value = Comment15;
                         console.log("Sudah Comment")
+                        clearInterval(myInterval);
                         clicksend();
                     }
                 }
@@ -392,6 +402,7 @@ var commentanku = setInterval(function(){
                         GM.setValue( 16,1);
                         document.getElementsByClassName("internal-input")[0].value = Comment16;
                         console.log("Sudah Comment")
+                        clearInterval(myInterval);
                         clicksend();
                     }
                 }
@@ -402,6 +413,7 @@ var commentanku = setInterval(function(){
                         GM.setValue( 17,1);
                         document.getElementsByClassName("internal-input")[0].value = Comment17;
                         console.log("Sudah Comment")
+                        clearInterval(myInterval);
                         clicksend();
                     }
                 }
@@ -412,6 +424,7 @@ var commentanku = setInterval(function(){
                         GM.setValue( 18,1);
                         document.getElementsByClassName("internal-input")[0].value = Comment18;
                         console.log("Sudah Comment")
+                        clearInterval(myInterval);
                         clicksend();
                     }
                 }
@@ -431,7 +444,6 @@ function clicksend() {
     if( document.getElementsByClassName("internal-input")[0].value.length > 1){
         document.querySelectorAll("[aria-label='Posting komentar']")[0].click()
         console.log("Comment Terkirim");
-        clearInterval(commentanku);
         closer()
     }
     /*Tekan TOMBOL SEND*/
@@ -442,5 +454,5 @@ function clicksend() {
 
 
 function closer() {
-    setTimeout(function(){location.href = "about:blank"},5)
+    setTimeout(function(){location.href = "about:blank"},9)
 }
