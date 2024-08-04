31e7ca76de51ca70f20f103cbd790bd0871bd47d
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-04 22:37:43 +0700

    Update Manuk3.js

--- Manuk/Manuk3.js
+++ Manuk/Manuk3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MANUK 3
 // @namespace    http://tampermonkey.net/
-// @version      3.49
+// @version      3.50
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk3.js
@@ -436,7 +436,7 @@ var myInterval = setInterval(function(){
         console.log("erorr " + err)
     }
 
-},240)
+},40)
 
 
 
@@ -456,5 +456,5 @@ function clicksend() {
 
 
 function closer() {
-    setTimeout(function(){location.href = "about:blank"},20)
+    setTimeout(function(){location.href = "about:blank"},40)
 }
