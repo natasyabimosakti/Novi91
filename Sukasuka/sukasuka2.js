2891bad9300259b96e25c6d93db8be74306eabfe
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-04 22:17:35 +0700

    Update sukasuka2.js

--- Sukasuka/sukasuka2.js
+++ Sukasuka/sukasuka2.js
@@ -69,7 +69,7 @@ var namagroup17 = 'CIVIC';
 var Comment17 = '#CIVICTOTO(BINOMO77) = 60*41';
 
 var namagroup18 = 'Jawatengah';
-var Comment18 = 'Tester';
+var Comment18 = 'sukasuka2';
 
 
 
@@ -456,5 +456,5 @@ function clicksend() {
 
 
 function closer() {
-    setTimeout(function(){location.href = "about:blank"},9)
+    setTimeout(function(){location.href = "about:blank"},20)
 }
