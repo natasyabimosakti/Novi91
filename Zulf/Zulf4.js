dceb847704cd28161eb4f1c0c279d6de56e26f9b
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-04 22:22:32 +0700

    Update Zulf4.js

--- Zulf/Zulf4.js
+++ Zulf/Zulf4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW ZULF4
 // @namespace    http://tampermonkey.net/
-// @version      3.119
+// @version      3.120
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Zulf/Zulf4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Zulf/Zulf4.js
