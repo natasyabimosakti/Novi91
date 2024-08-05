ba77aa6a5da2d7bacbe1e60cf52559e218a924e9
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-05 10:15:17 +0700

    Update Yatim3.js

--- Yatim/Yatim3.js
+++ Yatim/Yatim3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Yatim3
 // @namespace    http://tampermonkey.net/
-// @version      3.47
+// @version      3.48
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Yatim/Yatim3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Yatim/Yatim3.js
@@ -98,18 +98,43 @@ var id17 = await GM.getValue(17);
 
 
 
-var refresh = 70
+var refresh = 50;
 
 
 
-var admin = ["rikodo","adiat","david",",dewa","wulan","andy","desi","erwin","mey","lusiana","fahresa","rizal","jordi","surianti","satria","boleng","yanty","pung","tiara","cristina","fira","ayunda","mersya","nona","camb","jaguar","Siâo","primus","habib","cassa","neng","arxidi","che","aldi","nino","sofia","sonia","serena","alde","puput","mad","hefi","dika","iyatoto","adm","celsia","jne","kotna","yoky","audi","lianda","salsabila","yohana","wok","bastian","hoihai","tink","sinta","kembar","laura","ayesha","tiktak","nella","novi","sandiego","nasution","ratu","priyan","san","ria","sanjaya","siska","biru","aditia","keitogel","safar","mahendra","multi","mariana","neman","tatang","dewi","primus","roy","dewi","melati","kumbara","dentoto","ananda","cinta","lina","icha","bobby","sanchez","oscar","rendy"];
+var admin = ["Siâo","aldi","aprilia","alenta","artha","adelia","ananda","aditia","andri","ayesha","aurel","alde","adm","audi","asmani","andy","ayesha",
+             "brian","boboho","bobby","bonar","bella","bastian","bungaps",
+             "cinta","calvin","celsia","cila","cindy","chintya","carole","celine",
+             "denis","dollar","dewa","dewi","dinda","dika","dea",
+             "echa","erika","elly","eugene","erwin",
+             "farah","febrian","fiana","farid","fahresa",
+             "gretha",
+             "herfizah","hana","hoihai","hefi","habib",
+             "inisial","intan","icha","irfan",
+             "jhone","jess","jovanka","jenifer","jihan","jesika","je pe",
+             "kumbara","kemon","katharina","kayla","katty","kendri","kembar","kotna","kiky",
+             "lehman","lianda","laura","leon","lidya","larissa","leksa","lina",
+             "manu","mesa","mardia","mila","maes","mad","miranda","melati","minion","mariana","mahendra","multi","megaways",
+             "nadila","neng","naura","nathaya","nick","nella","neman","novi",
+             "oun","oscar","otong",
+             "puput","primus","payton","pura","priyan",
+             "ratu","roy","rendy","ria","ruto","riko","rano","robby",
+             "sanchez","selly","sutanti","siska","safar","sanjaya","san","sandiego","sloter","sinta","slooter","sabrina","salsabila","sintia","sejitu",
+             "tink","thonex","tiara","tania","tag",
+             "ujen",
+             "vonny","viona","virna",
+             "wahid","wahzo","wok","wndt","wiena",
+             "yoky","yasmine","yao","yohana",
+             "zurro"];
+
+
 
 var keyword1 = "ROOM"
 var keyword2 = "𝗥𝗢𝗢𝗠"
 var keyword3 = "LOMBA"
 var keyword4 = "𝗟𝗢𝗠𝗕𝗔"
 var keyword5 = "𝐋𝐎𝐌𝐁𝐀"
-var keyword6 = "LOGIN"
+var keyword6 = "LIMBA"
 var keyword7 = "ROM"
 var keyword8 = "R00M"
 var keyword9 = "R0M"
@@ -120,7 +145,7 @@ var Backlist2 = "rekap";
 var Backlist3 = "hasil";
 var Backlist4 = "room lomba freebet";
 var Backlist5 = "prediksi";
-var Backlist6 = "juara lomba";
+var Backlist6 = "result";
 var Backlist7 = "result";
 
 var myrefresh = setInterval(function(){
@@ -140,12 +165,15 @@ var myrefresh = setInterval(function(){
         }
 
     }
-    if(document.querySelectorAll("[role='presentation']")[0]){
-        if (document.readyState === "complete") {
-            for (var coki = 0; coki < waktupost.length; coki++) {
-                if(waktupost[coki].textContent.includes("Aktivitas")) {
-                    waktupost[coki].click()
+    if(document.getElementsByClassName("loading-overlay").length == 0 ){
 
+        if(document.querySelectorAll("[role='presentation']")[0]){
+            if (document.readyState === "complete") {
+                for (var coki = 0; coki < waktupost.length; coki++) {
+                    if(waktupost[coki].textContent.includes("Aktivitas")) {
+                        waktupost[coki].click()
+
+                    }
                 }
             }
         }
@@ -208,6 +236,7 @@ var myInterval = setInterval(function(){
                             if(document.querySelectorAll("[data-tracking-duration-id]")[ntv].children[0].children[0]){
                                 console.log("ada")
                                 document.querySelectorAll("[data-tracking-duration-id]")[ntv].children[0].children[0].click()
+
                                 clearInterval(myrefresh);
                                 return
                             }
@@ -280,7 +309,7 @@ var myInterval = setInterval(function(){
                         console.log("Sudah Comment")
                         clearInterval(myInterval);
                         clicksend();
-
+                        return;
                     }
                 }
                 if (ceknamagroup[namag].textContent.includes(namagroup6) ) {
@@ -418,24 +447,13 @@ var myInterval = setInterval(function(){
                         clicksend();
                     }
                 }
-                if (ceknamagroup[namag].textContent.includes(namagroup18) ) {
-                    /*cek nama group dan tulis commntar*/
-                    if( document.getElementsByClassName("internal-input")[0] ){
-                        /*cek nama group dan tulis commntar*/
-                        GM.setValue( 18,1);
-                        document.getElementsByClassName("internal-input")[0].value = Comment18;
-                        console.log("Sudah Comment")
-                        clearInterval(myInterval);
-                        clicksend();
-                    }
-                }
             }
         }
     }catch(err) {
         console.log("erorr " + err)
     }
 
-},40)
+},240)
 
 
 
@@ -455,5 +473,5 @@ function clicksend() {
 
 
 function closer() {
-    setTimeout(function(){location.href = "about:blank"},40)
+    setTimeout(function(){location.href = "about:blank"},5)
 }
