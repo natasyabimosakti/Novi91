5667b6de2f618e860a378f020063a4f984d9d586
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-09 11:42:54 +0700

    Update Sampoerna4.js

--- Sampoerna/Sampoerna4.js
+++ Sampoerna/Sampoerna4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sampoerna4
 // @namespace    http://tampermonkey.net/
-// @version      3.61
+// @version      3.62
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna4.js
@@ -81,6 +81,7 @@ var Comment18 = 'asek';
 
 
 
+var refresh = 50
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
