506c3a24ce5cc83f1393c6f287ec87913e9bd66c
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-09 11:42:33 +0700

    Update Peyang4.js

--- Peyang/Peyang4.js
+++ Peyang/Peyang4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Peyang4
 // @namespace    http://tampermonkey.net/
-// @version      3.54
+// @version      3.55
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang4.js
@@ -80,6 +80,7 @@ var Comment18 = 'asek';
 
 
 
+var refresh = 50
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
