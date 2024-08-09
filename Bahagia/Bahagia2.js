f75cfdf1209626cd531b9ccb3893bbe0fc6a14a6
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-09 11:41:03 +0700

    Update Bahagia2.js

--- Bahagia/Bahagia2.js
+++ Bahagia/Bahagia2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bahagia2
 // @namespace    http://tampermonkey.net/
-// @version      3.24
+// @version      3.25
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia2.js
@@ -82,6 +82,7 @@ var Comment18 = 'asek';
 
 
 
+var refresh = 50
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
