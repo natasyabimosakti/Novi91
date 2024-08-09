c8b6a31ad8d36028371971b20aa37621a23c2d75
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-09 11:41:44 +0700

    Update Kawul2.js

--- Kawul/Kawul2.js
+++ Kawul/Kawul2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kawul2
 // @namespace    http://tampermonkey.net/
-// @version      3.56
+// @version      3.57
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul2.js
@@ -84,6 +84,7 @@ var Comment18 = 'asek';
 
 
 
+var refresh = 50
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
