73359abd85376718b1df82d73fa2abbf04da3edc
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-09 11:41:28 +0700

    Update Haci3.js

--- Haci/Haci3.js
+++ Haci/Haci3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hachi3
 // @namespace    http://tampermonkey.net/
-// @version      3.69
+// @version      3.70
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci3.js
@@ -80,6 +80,7 @@ var Comment18 = 'asek';
 
 
 
+var refresh = 50
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
