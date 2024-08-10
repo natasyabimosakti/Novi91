6982f000fce35822d3ed8bca70ae7d9188a38af1
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-10 23:54:43 +0700

    Update Perkutut2.js

--- Perkutut/Perkutut2.js
+++ Perkutut/Perkutut2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Perkutut2
 // @namespace    http://tampermonkey.net/
-// @version      3.72
+// @version      3.73
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Perkutut/Perkutut2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Perkutut/Perkutut2.js
@@ -49,7 +49,7 @@ var Comment10 = 'LING77 (ELANG72) 34*70';
 var namagroup11 = 'KEBAYA';
 var Comment11 = '(KEBAYA4D) = (ELANGY) 14*31*28'; 
 
-var namagroup12 = 'BESTOTO';
+var namagroup12 = 'ROOM LOMBA TEBAK';
 var Comment12 = 'ELANGX3 : 40*60*76 #BESTOTO88'; 
 
 var namagroup13 = 'GOHT0G3L';
