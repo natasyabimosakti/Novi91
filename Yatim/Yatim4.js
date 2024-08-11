091d7436a4f9d3a398c833f0c88072f2e6239d3b
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-11 19:54:29 +0700

    Update Yatim4.js

--- Yatim/Yatim4.js
+++ Yatim/Yatim4.js
@@ -42,7 +42,7 @@ var Comment8 = 'ANGKER4D=YADIANI77=31*44*59';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=YASALAM64=94*89*29'; 
 
-var namagroup10 = 'JNE';
+var namagroup10 = 'PEJUANG';
 var Comment10 = '#JNETOTO(YAHWEH00)*08*87*53'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
