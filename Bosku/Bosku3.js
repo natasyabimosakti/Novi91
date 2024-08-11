25583e7c7f31bd241e14dcad937781d31dbf1e85
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-11 19:39:12 +0700

    Update Bosku3.js

--- Bosku/Bosku3.js
+++ Bosku/Bosku3.js
@@ -43,7 +43,7 @@ var Comment8 = 'ANGKER4D=YOBENWES3=61*89*97';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=YOWESBEN6=61*89*97'; 
 
-var namagroup10 = 'JNE';
+var namagroup10 = 'PEJUANG';
 var Comment10 = '#JNETOTO(YONEKWIK37)*77*46*64'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
