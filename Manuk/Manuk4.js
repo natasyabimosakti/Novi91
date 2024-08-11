a026954436ef49eb44260bd533050a26f5a9d7f4
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-11 13:34:27 +0700

    Update Manuk4.js

--- Manuk/Manuk4.js
+++ Manuk/Manuk4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MANUK 4
 // @namespace    http://tampermonkey.net/
-// @version      3.61
+// @version      3.62
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk4.js
@@ -45,7 +45,7 @@ var Comment8 = 'ANGKER4D=PLECIKLUK=15*98*86';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=PLECUAN=16*81*71'; 
 
-var namagroup10 = 'PEJUANG';
+var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(PLECIKAN)*06*28*27'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
