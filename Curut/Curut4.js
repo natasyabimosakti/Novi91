0bc7d867b6278de60703ee957b1c8bc400a60425
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-11 19:37:48 +0700

    Update Curut4.js

--- Curut/Curut4.js
+++ Curut/Curut4.js
@@ -43,7 +43,7 @@ var Comment8 = 'ANGKER4D=TUKUSAPU12=19*03*43';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=TUKUEMPEK10=19*03*18'; 
 
-var namagroup10 = 'JNE';
+var namagroup10 = 'PEJUANG';
 var Comment10 = '#JNETOTO(TUKANGBATU)*31*44*59'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
