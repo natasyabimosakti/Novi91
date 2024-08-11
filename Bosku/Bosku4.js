d14a1d75526008bed1f4726692c9bf187c2cf921
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-11 13:35:06 +0700

    Update Bosku4.js

--- Bosku/Bosku4.js
+++ Bosku/Bosku4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BOSQUE4
 // @namespace    http://tampermonkey.net/
-// @version      3.67
+// @version      3.68
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku4.js
@@ -44,7 +44,7 @@ var Comment8 = 'ANGKER4D=ZONATING=34*76*24';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=ZOWOK26=34*76*24'; 
 
-var namagroup10 = 'PEJUANG';
+var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(ZOKET72)*21*73*63'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
