0a85c6fc66971837b4cfcdad4b826821418c5651
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-11 19:38:46 +0700

    Update Manuk4.js

--- Manuk/Manuk4.js
+++ Manuk/Manuk4.js
@@ -45,7 +45,7 @@ var Comment8 = 'ANGKER4D=PLECIKLUK=15*98*86';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=PLECUAN=16*81*71'; 
 
-var namagroup10 = 'JNE';
+var namagroup10 = 'PEJUANG';
 var Comment10 = '#JNETOTO(PLECIKAN)*06*28*27'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
