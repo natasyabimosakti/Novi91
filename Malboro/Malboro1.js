1b8d7b0893ebb6faab7b9e01000469892fd29826
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-11 19:38:07 +0700

    Update Malboro1.js

--- Malboro/Malboro1.js
+++ Malboro/Malboro1.js
@@ -43,7 +43,7 @@ var Comment8 = 'ANGKER4D=ANUNTU284=37*15*50';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=ANALOH49=14*57*42'; 
 
-var namagroup10 = 'JNE';
+var namagroup10 = 'PEJUANG';
 var Comment10 = '#JNETOTO(ALORJI3)*37*15*50'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
