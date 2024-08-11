9f79e2c181c11b19c8d29b6fdfaf1a5d5c7a32db
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-11 19:54:32 +0700

    Update Zulf1.js

--- Zulf/Zulf1.js
+++ Zulf/Zulf1.js
@@ -43,7 +43,7 @@ var Comment8 = 'ANGKER4D=ZULAFRNA002=17*95*61';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=ZUHULU00=17*95*61'; 
 
-var namagroup10 = 'JNE';
+var namagroup10 = 'PEJUANG';
 var Comment10 = '#JNETOTO(ZULFCANTIK)*47*54*49'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
