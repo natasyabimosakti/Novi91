aeb801815dae03442abf05194ae7252b88cae261
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-14 18:56:33 +0700

    Update sukasuka3.js

--- Sukasuka/sukasuka3.js
+++ Sukasuka/sukasuka3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BENT3
 // @namespace    http://tampermonkey.net/
-// @version      3.59
+// @version      3.60
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sukasuka/sukasuka3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sukasuka/sukasuka3.js
@@ -43,7 +43,7 @@ var Comment8 = 'ANGKER4D=CUILANSENG12=34*13*79';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=CUALINSENG41=34*13*79'; 
 
-var namagroup10 = 'PEJUANG';
+var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(CACINGAN54)*22*24*21'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
@@ -137,8 +137,7 @@ var myrefresh = setInterval(function(){
     var urutkan = document.querySelectorAll("[data-mcomponent='ServerTextArea']");
     var urutkan2 = document.querySelectorAll("[data-mcomponent='TextArea']");
     var waktupost = document.getElementsByClassName("native-text");
-    window.scrollTo(0, 2000);
-
+     
     for (var coke = 0; coke < urutkan2.length; coke++) {
         if (urutkan2[coke].textContent.includes("URUTKAN")) {
 
