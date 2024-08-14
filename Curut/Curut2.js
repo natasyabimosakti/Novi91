5e9366bac65fd1bc9de916bd72871bf497c08b3a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-14 18:50:09 +0700

    Update Curut2.js

--- Curut/Curut2.js
+++ Curut/Curut2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW CURUT2
 // @namespace    http://tampermonkey.net/
-// @version      3.67
+// @version      3.68
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut2.js
@@ -43,7 +43,7 @@ var Comment8 = 'ANGKER4D=MOMER14=11*20*38';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=MOLET25=11*20*83'; 
 
-var namagroup10 = 'PEJUANG';
+var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(MOSIOM19)*58*88*41'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
@@ -136,7 +136,7 @@ var myrefresh = setInterval(function(){
     var urutkan = document.querySelectorAll("[data-mcomponent='ServerTextArea']");
     var urutkan2 = document.querySelectorAll("[data-mcomponent='TextArea']");
     var waktupost = document.getElementsByClassName("native-text");
-    window.scrollTo(0, 2000);
+     
 
     for (var coke = 0; coke < urutkan2.length; coke++) {
         if (urutkan2[coke].textContent.includes("URUTKAN")) {
