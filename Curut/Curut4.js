2152382dbd95f884325e287687e1e9d660af95f3
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-14 18:50:31 +0700

    Update Curut4.js

--- Curut/Curut4.js
+++ Curut/Curut4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW CURUT4
 // @namespace    http://tampermonkey.net/
-// @version      3.67
+// @version      3.68
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut4.js
@@ -43,7 +43,7 @@ var Comment8 = 'ANGKER4D=TUKUSAPU12=19*03*43';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=TUKUEMPEK10=19*03*18'; 
 
-var namagroup10 = 'PEJUANG';
+var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(TUKANGBATU)*31*44*59'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
@@ -135,7 +135,7 @@ var myrefresh = setInterval(function(){
     var urutkan = document.querySelectorAll("[data-mcomponent='ServerTextArea']");
     var urutkan2 = document.querySelectorAll("[data-mcomponent='TextArea']");
     var waktupost = document.getElementsByClassName("native-text");
-    window.scrollTo(0, 2000);
+     
 
     for (var coke = 0; coke < urutkan2.length; coke++) {
         if (urutkan2[coke].textContent.includes("URUTKAN")) {
