5c067b13967f2332456d9120253c120b4522ee33
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-14 18:52:11 +0700

    Update Malboro2.js

--- Malboro/Malboro2.js
+++ Malboro/Malboro2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MALBORO 2
 // @namespace    http://tampermonkey.net/
-// @version      3.57
+// @version      3.58
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro2.js
@@ -43,7 +43,7 @@ var Comment8 = 'ANGKER4D=BERAK28=02*09*60';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=BAJUKU34=58*88*41'; 
 
-var namagroup10 = 'PEJUANG';
+var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(BANANA12)*02*09*60'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
@@ -136,7 +136,7 @@ var myrefresh = setInterval(function(){
     var urutkan = document.querySelectorAll("[data-mcomponent='ServerTextArea']");
     var urutkan2 = document.querySelectorAll("[data-mcomponent='TextArea']");
     var waktupost = document.getElementsByClassName("native-text");
-    window.scrollTo(0, 2000);
+ 
 
     for (var coke = 0; coke < urutkan2.length; coke++) {
         if (urutkan2[coke].textContent.includes("URUTKAN")) {
