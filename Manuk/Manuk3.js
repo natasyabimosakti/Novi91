9279c7512586f8468a7ff24f00084f28569e4da2
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-14 19:10:34 +0700

    Update Manuk3.js

--- Manuk/Manuk3.js
+++ Manuk/Manuk3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MANUK 3
 // @namespace    http://tampermonkey.net/
-// @version      3.62
+// @version      3.63
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk3.js
@@ -44,7 +44,7 @@ var Comment8 = 'ANGKER4D=DOROKERTO3=60*32*38';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=DOROMIBER2=26*53*82'; 
 
-var namagroup10 = 'PEJUANG';
+var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(DORONYOK31)*47*23*10'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
@@ -138,7 +138,7 @@ var myrefresh = setInterval(function(){
     var urutkan = document.querySelectorAll("[data-mcomponent='ServerTextArea']");
     var urutkan2 = document.querySelectorAll("[data-mcomponent='TextArea']");
     var waktupost = document.getElementsByClassName("native-text");
-    window.scrollTo(0, 2000);
+     
 
     for (var coke = 0; coke < urutkan2.length; coke++) {
         if (urutkan2[coke].textContent.includes("URUTKAN")) {
