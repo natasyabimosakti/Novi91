7cf370cd65ddc1888002c0f65409729749979dcf
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-14 18:57:23 +0700

    Update Yatim4.js

--- Yatim/Yatim4.js
+++ Yatim/Yatim4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Yatim4
 // @namespace    http://tampermonkey.net/
-// @version      3.58
+// @version      3.59
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Yatim/Yatim4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Yatim/Yatim4.js
@@ -42,7 +42,7 @@ var Comment8 = 'ANGKER4D=YADIANI77=31*44*59';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=YASALAM64=94*89*29'; 
 
-var namagroup10 = 'PEJUANG';
+var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(YAHWEH00)*08*87*53'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
@@ -136,7 +136,7 @@ var myrefresh = setInterval(function(){
     var urutkan = document.querySelectorAll("[data-mcomponent='ServerTextArea']");
     var urutkan2 = document.querySelectorAll("[data-mcomponent='TextArea']");
     var waktupost = document.getElementsByClassName("native-text");
-    window.scrollTo(0, 2000);
+     
 
     for (var coke = 0; coke < urutkan2.length; coke++) {
         if (urutkan2[coke].textContent.includes("URUTKAN")) {
