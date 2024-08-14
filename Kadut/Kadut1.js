fdd1e5f12ebda6d8886eccb147ed287b99bff684
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-14 18:50:42 +0700

    Update Kadut1.js

--- Kadut/Kadut1.js
+++ Kadut/Kadut1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT1
 // @namespace    http://tampermonkey.net/
-// @version      3.80
+// @version      3.81
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kadut/Kadut1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kadut/Kadut1.js
@@ -45,7 +45,7 @@ var Comment8 = 'ANGKER4D=BUDELTUNG=01*44*31';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=BUANGKALI=88*75*65'; 
 
-var namagroup10 = 'PEJUANG';
+var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(BUWOSOK12)*17*84*50'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
@@ -139,8 +139,7 @@ var myrefresh = setInterval(function(){
     var urutkan = document.querySelectorAll("[data-mcomponent='ServerTextArea']");
     var urutkan2 = document.querySelectorAll("[data-mcomponent='TextArea']");
     var waktupost = document.getElementsByClassName("native-text");
-    window.scrollTo(0, 2000);
-
+ 
     for (var coke = 0; coke < urutkan2.length; coke++) {
         if (urutkan2[coke].textContent.includes("URUTKAN")) {
 
