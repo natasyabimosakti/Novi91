b70989a52889f6fa2c618abc8b699f9669706630
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-14 18:58:01 +0700

    Update Zulf4.js

--- Zulf/Zulf4.js
+++ Zulf/Zulf4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW ZULF4
 // @namespace    http://tampermonkey.net/
-// @version      3.129
+// @version      3.130
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Zulf/Zulf4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Zulf/Zulf4.js
@@ -44,7 +44,7 @@ var Comment8 = 'ANGKER4D=FAISMU=39*27*32';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=FASTON11=39*27*32'; 
 
-var namagroup10 = 'PEJUANG';
+var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(FAZAMIR00)*25*16*85'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
@@ -138,7 +138,7 @@ var myrefresh = setInterval(function(){
     var urutkan = document.querySelectorAll("[data-mcomponent='ServerTextArea']");
     var urutkan2 = document.querySelectorAll("[data-mcomponent='TextArea']");
     var waktupost = document.getElementsByClassName("native-text");
-    window.scrollTo(0, 2000);
+     
 
     for (var coke = 0; coke < urutkan2.length; coke++) {
         if (urutkan2[coke].textContent.includes("URUTKAN")) {
