289e6e55b72891191ff03a94fcbe056cb0634918
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-14 18:52:49 +0700

    Update Manyut1.js

--- Manyut/Manyut1.js
+++ Manyut/Manyut1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MANYUT1
 // @namespace    http://tampermonkey.net/
-// @version      3.149
+// @version      3.150
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut1.js
@@ -44,7 +44,7 @@ var Comment8 = 'ANGKER4D=HOPRET303=47*54*49';
 var namagroup9 = 'GILA';
 var Comment9 = 'GILA4D=HONOS112=47*54*99'; 
 
-var namagroup10 = 'PEJUANG';
+var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(HOMOGEN12)*65*56*99'; 
 
 var namagroup11 = 'T_O_T_O_A_K_U_R_A_T';
@@ -138,7 +138,7 @@ var myrefresh = setInterval(function(){
     var urutkan = document.querySelectorAll("[data-mcomponent='ServerTextArea']");
     var urutkan2 = document.querySelectorAll("[data-mcomponent='TextArea']");
     var waktupost = document.getElementsByClassName("native-text");
-    window.scrollTo(0, 2000);
+     
 
     for (var coke = 0; coke < urutkan2.length; coke++) {
         if (urutkan2[coke].textContent.includes("URUTKAN")) {
