63485be8491047057e86d54095b4f498c90d90a0
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-18 04:47:54 +0700

    Update Manuk2.js

--- Manuk/Manuk2.js
+++ Manuk/Manuk2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MANUK 2
 // @namespace    http://tampermonkey.net/
-// @version      3.69
+// @version      3.70
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk2.js
@@ -122,6 +122,7 @@ var Backlist7 = "result";
 
 var jitter = 0
 var myrefresh = setInterval(function(){
+ window.scroll(0,200)
     if (tm == "" || tm == undefined || tm == null) {
         GM.setValue("time", hour);
     }
