974f52782d0a5636a8f1c427b3bd400fe6b7b484
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-21 19:45:52 +0700

    Update sukasuka1.js

--- Sukasuka/sukasuka1.js
+++ Sukasuka/sukasuka1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BENT1
 // @namespace    http://tampermonkey.net/
-// @version      3.71
+// @version      3.72
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sukasuka/sukasuka1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sukasuka/sukasuka1.js
@@ -72,7 +72,7 @@ var Comment18 = 'sukasuka1';
 
 
 
-var refresh = 23;
+var refresh = 30;
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
