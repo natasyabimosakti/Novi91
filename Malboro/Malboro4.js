d53280a20b29078dc0f9c993350c529a780a4b53
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-21 19:33:36 +0700

    Update Malboro4.js

--- Malboro/Malboro4.js
+++ Malboro/Malboro4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MALBORO 4
 // @namespace    http://tampermonkey.net/
-// @version      3.68
+// @version      3.69
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro4.js
@@ -73,7 +73,7 @@ var Comment18 = 'Malboro4';
 
 
 
-var refresh = 30;
+var refresh = 23;
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
@@ -119,7 +119,7 @@ var Backlist7 = "result";
 
 var jitter = 0
 var myrefresh = setInterval(function(){
- window.scroll(0,200)
+ window.scroll(0,100)
     if (tm == "" || tm == undefined || tm == null) {
         GM.setValue("time", hour);
     }
@@ -179,10 +179,11 @@ var myrefresh = setInterval(function(){
                             for (var clk = 0; clk < commentbox.length; clk++) {
                                 if(commentbox[clk]){
                                     if(commentbox[clk].textContent.toLowerCase().includes("jawab")|| commentbox[clk].textContent.toLowerCase().includes("tulis")){
-                                        commentbox[clk].click()
+                                        
                                         clearInterval(myrefresh);
                                         console.log("Click Posting box")
                                         jitter = 0
+                                        commentbox[clk].click()
                                         game.start()
                                         return;
                                     }
