85ee3ed97bf9a30eaea9fd879ef713b210c9a5eb
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-21 18:57:04 +0700

    Update Manyut3.js

--- Manyut/Manyut3.js
+++ Manyut/Manyut3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MANYUT3
 // @namespace    http://tampermonkey.net/
-// @version      3.158
+// @version      3.159
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut3.js
@@ -41,7 +41,7 @@ var Comment7 = 'ASEPTOGEL EROSI52 63*30*72';
 var namagroup8 = 'ANGKER';
 var Comment8 = 'ANGKER4D=ERFANGUN15=64*40*69'; 
 
-var namagroup9 = 'GILA';
+var namagroup9 = 'GIL4';
 var Comment9 = 'GILA4D=ERGUN51=64*40*97'; 
 
 var namagroup10 = 'JNE';
@@ -72,7 +72,7 @@ var namagroup18 = 'Jawatengah';
 var Comment18 = 'Manyut3';
 
 
-var refresh = 40;
+var refresh = 20;
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
@@ -512,7 +512,7 @@ function gameClosure() {
     var currentGame;
     return {
         start() {
-            currentGame = setInterval(game, 5)
+            currentGame = setInterval(game, 0)
         },
         stop() {
             clearInterval(currentGame)
