001418edc00dde9780871a39b74c5627aa442353
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-21 19:33:40 +0700

    Update Manuk1.js

--- Manuk/Manuk1.js
+++ Manuk/Manuk1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MANUK 1
 // @namespace    http://tampermonkey.net/
-// @version      3.73
+// @version      3.74
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk1.js
@@ -74,7 +74,7 @@ var Comment18 = 'Manuk1';
 
 
 
-var refresh = 30;
+var refresh = 23;
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
@@ -120,7 +120,7 @@ var Backlist7 = "result";
 
 var jitter = 0
 var myrefresh = setInterval(function(){
- window.scroll(0,200)
+ window.scroll(0,100)
     if (tm == "" || tm == undefined || tm == null) {
         GM.setValue("time", hour);
     }
@@ -180,10 +180,11 @@ var myrefresh = setInterval(function(){
                             for (var clk = 0; clk < commentbox.length; clk++) {
                                 if(commentbox[clk]){
                                     if(commentbox[clk].textContent.toLowerCase().includes("jawab")|| commentbox[clk].textContent.toLowerCase().includes("tulis")){
-                                        commentbox[clk].click()
+                                        
                                         clearInterval(myrefresh);
                                         console.log("Click Posting box")
                                         jitter = 0
+                                        commentbox[clk].click()
                                         game.start()
                                         return;
                                     }
