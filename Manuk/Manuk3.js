01526a7ff1d7a5b1f518d9625214397906740fe7
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-21 19:10:04 +0700

    Update Manuk3.js

--- Manuk/Manuk3.js
+++ Manuk/Manuk3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MANUK 3
 // @namespace    http://tampermonkey.net/
-// @version      3.72
+// @version      3.73
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk3.js
@@ -72,7 +72,7 @@ var namagroup18 = 'Jawatengah';
 var Comment18 = 'Manuk3';
 
 
-var refresh = 20;
+var refresh = 30;
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
