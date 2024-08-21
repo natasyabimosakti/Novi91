b8c2015d9ab0ca1a75b3e39822489d217c3facdd
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-21 19:46:05 +0700

    Update sukasuka4.js

--- Sukasuka/sukasuka4.js
+++ Sukasuka/sukasuka4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BENT4
 // @namespace    http://tampermonkey.net/
-// @version      3.71
+// @version      3.72
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sukasuka/sukasuka4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sukasuka/sukasuka4.js
@@ -71,7 +71,7 @@ var namagroup18 = 'Jawatengah';
 var Comment18 = 'sukasuka4';
 
 
-var refresh = 23;
+var refresh = 30;
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
