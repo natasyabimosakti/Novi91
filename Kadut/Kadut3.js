6008e911d52110c8674752730ed591b1b1a1747a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-21 19:33:12 +0700

    Update Kadut3.js

--- Kadut/Kadut3.js
+++ Kadut/Kadut3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT3
 // @namespace    http://tampermonkey.net/
-// @version      3.91
+// @version      3.92
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kadut/Kadut3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kadut/Kadut3.js
@@ -75,7 +75,7 @@ var Comment18 = 'Kadut3';
 
 
 
-var refresh = 30;
+var refresh = 23;
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
@@ -121,7 +121,7 @@ var Backlist7 = "result";
 
 var jitter = 0
 var myrefresh = setInterval(function(){
- window.scroll(0,200)
+ window.scroll(0,100)
     if (tm == "" || tm == undefined || tm == null) {
         GM.setValue("time", hour);
     }
@@ -181,10 +181,11 @@ var myrefresh = setInterval(function(){
                             for (var clk = 0; clk < commentbox.length; clk++) {
                                 if(commentbox[clk]){
                                     if(commentbox[clk].textContent.toLowerCase().includes("jawab")|| commentbox[clk].textContent.toLowerCase().includes("tulis")){
-                                        commentbox[clk].click()
+                                        
                                         clearInterval(myrefresh);
                                         console.log("Click Posting box")
                                         jitter = 0
+                                        commentbox[clk].click()
                                         game.start()
                                         return;
                                     }
