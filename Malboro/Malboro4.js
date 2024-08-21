c169804d4565dc6aaa657130dd1bc86cebc243ce
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-21 18:56:26 +0700

    Update Malboro4.js

--- Malboro/Malboro4.js
+++ Malboro/Malboro4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MALBORO 4
 // @namespace    http://tampermonkey.net/
-// @version      3.66
+// @version      3.67
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro4.js
@@ -41,7 +41,7 @@ var Comment7 = 'ASEPTOGEL DEDEKLESMONO 08*87*53';
 var namagroup8 = 'ANGKER';
 var Comment8 = 'ANGKER4D=DORAEMON34=94*89*29'; 
 
-var namagroup9 = 'GILA';
+var namagroup9 = 'GIL4';
 var Comment9 = 'GILA4D=DAMIAN12=31*44*59'; 
 
 var namagroup10 = 'JNE';
@@ -73,7 +73,7 @@ var Comment18 = 'Malboro4';
 
 
 
-var refresh = 40;
+var refresh = 20;
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
@@ -513,7 +513,7 @@ function gameClosure() {
     var currentGame;
     return {
         start() {
-            currentGame = setInterval(game, 5)
+            currentGame = setInterval(game, 0)
         },
         stop() {
             clearInterval(currentGame)
