008ce3324407a0b360ee4933e51e9c991420bf8a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-21 19:01:00 +0700

    Update Yatim2.js

--- Yatim/Yatim2.js
+++ Yatim/Yatim2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Yatim2
 // @namespace    http://tampermonkey.net/
-// @version      3.67
+// @version      3.68
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Yatim/Yatim2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Yatim/Yatim2.js
@@ -40,7 +40,7 @@ var Comment7 = 'ASEPTOGEL FERCAU 02*09*60';
 var namagroup8 = 'ANGKER';
 var Comment8 = 'ANGKER4D=FERKERR=58*88*41'; 
 
-var namagroup9 = 'GILA';
+var namagroup9 = 'GIL4';
 var Comment9 = 'GILA4D=FERGILL=02*09*60'; 
 
 var namagroup10 = 'JNE';
@@ -72,7 +72,7 @@ var Comment18 = 'Yatim2';
 
 
 
-var refresh = 40;
+var refresh = 20;
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
@@ -512,7 +512,7 @@ function gameClosure() {
     var currentGame;
     return {
         start() {
-            currentGame = setInterval(game, 5)
+            currentGame = setInterval(game, 0)
         },
         stop() {
             clearInterval(currentGame)
