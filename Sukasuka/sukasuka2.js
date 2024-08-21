b340bfdc87e39d66a5d4f6e735ddd79491637835
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-21 19:00:27 +0700

    Update sukasuka2.js

--- Sukasuka/sukasuka2.js
+++ Sukasuka/sukasuka2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BENT2
 // @namespace    http://5.net/
-// @version      3.68
+// @version      3.69
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sukasuka/sukasuka2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sukasuka/sukasuka2.js
@@ -41,7 +41,7 @@ var Comment7 = 'ASEPTOGEL BAMIN12 12*86*68';
 var namagroup8 = 'ANGKER';
 var Comment8 = 'ANGKER4D=BINOMO77=71*96*75'; 
 
-var namagroup9 = 'GILA';
+var namagroup9 = 'GIL4';
 var Comment9 = 'GILA4D=BEKOL12=71*96*75'; 
 
 var namagroup10 = 'JNE';
@@ -73,7 +73,7 @@ var Comment18 = 'sukasuka2';
 
 
 
-var refresh = 40;
+var refresh = 20;
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
@@ -513,7 +513,7 @@ function gameClosure() {
     var currentGame;
     return {
         start() {
-            currentGame = setInterval(game, 5)
+            currentGame = setInterval(game, 0)
         },
         stop() {
             clearInterval(currentGame)
