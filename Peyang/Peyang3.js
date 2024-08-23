98039dce5db4e2486de78e48f6b3e2b37a66395e
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-24 03:04:22 +0700

    Update Peyang3.js

--- Peyang/Peyang3.js
+++ Peyang/Peyang3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Peyang3
 // @namespace    http://tampermonkey.net/
-// @version      3.64
+// @version      3.65
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang3.js
@@ -261,6 +261,7 @@ var myInterval = setInterval(function(){
                                 if(commentbox[clk]){
                                     if(commentbox[clk].textContent.toLowerCase().includes("jawab")|| commentbox[clk].textContent.toLowerCase().includes("tulis")){
                                         commentbox[clk].click()
+                                        clearInterval(myrefresh);
                                     }
                                 }
                             }
@@ -624,7 +625,7 @@ function clicksend() {
 }
 
 function closer() {
-    setTimeout(function(){location.href = "about:blank"},200)
+    setTimeout(function(){location.href = "about:blank"},500)
 
 
 }
