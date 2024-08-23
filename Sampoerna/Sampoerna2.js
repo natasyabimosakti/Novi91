baee54ad01ea5e8cb8ceb9b0fd78c04a6b60dca8
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-24 03:04:37 +0700

    Update Sampoerna2.js

--- Sampoerna/Sampoerna2.js
+++ Sampoerna/Sampoerna2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sampoerna2
 // @namespace    http://tampermonkey.net/
-// @version      3.72
+// @version      3.73
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna2.js
@@ -265,6 +265,7 @@ var myInterval = setInterval(function(){
                                 if(commentbox[clk]){
                                     if(commentbox[clk].textContent.toLowerCase().includes("jawab")|| commentbox[clk].textContent.toLowerCase().includes("tulis")){
                                         commentbox[clk].click()
+                                        clearInterval(myrefresh);
                                     }
                                 }
                             }
@@ -628,7 +629,7 @@ function clicksend() {
 }
 
 function closer() {
-    setTimeout(function(){location.href = "about:blank"},200)
+    setTimeout(function(){location.href = "about:blank"},500)
 
 
 }
