854e83284b36bff3154e800a45f989628a588e99
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-28 08:16:44 +0700

    Update Peyang2.js

--- Peyang/Peyang2.js
+++ Peyang/Peyang2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Peyang2
 // @namespace    http://tampermonkey.net/
-// @version      3.72
+// @version      3.73
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang2.js
@@ -637,7 +637,7 @@ function clicksend() {
 }
 
 function closer() {
-    setTimeout(function(){location.href = "about:blank"},5)
+    setTimeout(function(){location.href = "about:blank"},500)
 
 
 }
