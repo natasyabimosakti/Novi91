955b56056cd54d5340e936a2186ee3d4b802ce6e
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-28 08:14:33 +0700

    Update Bahagia2.js

--- Bahagia/Bahagia2.js
+++ Bahagia/Bahagia2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bahagia2
 // @namespace    http://tampermonkey.net/
-// @version      3.43
+// @version      3.44
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia2.js
@@ -642,7 +642,7 @@ function clicksend() {
 }
 
 function closer() {
-    setTimeout(function(){location.href = "about:blank"},5)
+    setTimeout(function(){location.href = "about:blank"},500)
 
 
 }
