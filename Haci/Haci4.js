4f19eb3e75dd2f0cbc93731c050ad25d669b218b
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-28 08:15:39 +0700

    Update Haci4.js

--- Haci/Haci4.js
+++ Haci/Haci4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hachi4
 // @namespace    http://tampermonkey.net/
-// @version      3.87
+// @version      3.88
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci4.js
@@ -640,7 +640,7 @@ function clicksend() {
 }
 
 function closer() {
-    setTimeout(function(){location.href = "about:blank"},5)
+    setTimeout(function(){location.href = "about:blank"},500)
 
 
 }
