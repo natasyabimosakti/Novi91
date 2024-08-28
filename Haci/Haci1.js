632884f2ffb6a3145f49b70bb09134e0da0a0d47
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-28 07:49:39 +0700

    Update Haci1.js

--- Haci/Haci1.js
+++ Haci/Haci1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hachi1
 // @namespace    http://tampermonkey.net/
-// @version      3.85
+// @version      3.86
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci1.js
@@ -76,6 +76,7 @@ var Comment17 = 'Haci1';
 
 var namagroup18 = 'lajw';
 var Comment18 = 'asek';
+
 
 
 
@@ -230,8 +231,14 @@ var myInterval = setInterval(function(){
             //Postingan
             var postingan =document.querySelectorAll('[data-tracking-duration-id')[ntv].getElementsByClassName('native-text')[3];
             //Comment Box
-            var datacommentbox = document.querySelectorAll('[data-tracking-duration-id')[ntv].getElementsByClassName('native-text').length -2
-            var commentbox = document.querySelectorAll('[data-tracking-duration-id')[ntv].getElementsByClassName('native-text')[datacommentbox];
+            var datacommentbox1 = document.querySelectorAll('[data-tracking-duration-id')[ntv].getElementsByClassName('native-text').length -1
+            var datacommentbox2 = document.querySelectorAll('[data-tracking-duration-id')[ntv].getElementsByClassName('native-text').length -2
+            var commentbox 
+          if(document.querySelectorAll('[data-tracking-duration-id')[ntv].getElementsByClassName('native-text')[datacommentbox1].textContent.toLowerCase().includes("tulis")){
+            commentbox = document.querySelectorAll('[data-tracking-duration-id')[ntv].getElementsByClassName('native-text')[datacommentbox1]
+          }else{
+            commentbox = document.querySelectorAll('[data-tracking-duration-id')[ntv].getElementsByClassName('native-text')[datacommentbox2]
+          }
 
             // Cek Jam
             var ret = jamposting.textContent.replace(/  Admin   |  Moderator   /g, "");
