fa6634e2622b78ad860811fa21e5e4b3819a2f10
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-03 05:43:10 +0700

    Update Haci3.js

--- Haci/Haci3.js
+++ Haci/Haci3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hachi3
 // @namespace    http://tampermonkey.net/
-// @version      3.97
+// @version      3.98
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci3.js
@@ -76,7 +76,6 @@ var Comment17 = 'Haci3';
 
 var namagroup18 = 'lajw';
 var Comment18 = 'asek';
-
 
 
 var refresh = 30;
@@ -228,7 +227,7 @@ var myrefresh = setInterval(function(){
                         console.log("Sudah Comment")
                         clearInterval(myrefresh);
                         clicksend();
-
+                        throw new Error();
                         return;
                     } else {
                         location.href = "about:blank"
@@ -243,7 +242,7 @@ var myrefresh = setInterval(function(){
                         console.log("Sudah Comment")
                         clearInterval(myrefresh);
                         clicksend();
-
+                        throw new Error();
                         return;
                     } else {
                         location.href = "about:blank"
@@ -258,7 +257,7 @@ var myrefresh = setInterval(function(){
                         console.log("Sudah Comment")
                         clearInterval(myrefresh);
                         clicksend();
-
+                        throw new Error();
                         return;
                     } else {
                         location.href = "about:blank"
@@ -273,7 +272,7 @@ var myrefresh = setInterval(function(){
                         console.log("Sudah Comment")
                         clearInterval(myrefresh);
                         clicksend();
-
+                        throw new Error();
                         return;
                     } else {
                         location.href = "about:blank"
@@ -288,7 +287,7 @@ var myrefresh = setInterval(function(){
                         console.log("Sudah Comment")
                         clearInterval(myrefresh);
                         clicksend();
-
+                        throw new Error();
                         return;
                     } else {
                         location.href = "about:blank"
@@ -303,7 +302,7 @@ var myrefresh = setInterval(function(){
                         console.log("Sudah Comment")
                         clearInterval(myrefresh);
                         clicksend();
-
+                        throw new Error();
                         return;
                     } else {
                         location.href = "about:blank"
@@ -318,7 +317,7 @@ var myrefresh = setInterval(function(){
                         console.log("Sudah Comment")
                         clearInterval(myrefresh);
                         clicksend();
-
+                        throw new Error();
                         return;
                     } else {
                         location.href = "about:blank"
@@ -333,7 +332,7 @@ var myrefresh = setInterval(function(){
                         console.log("Sudah Comment")
                         clearInterval(myrefresh);
                         clicksend();
-
+                        throw new Error();
                         return;
                     } else {
                         location.href = "about:blank"
@@ -348,7 +347,7 @@ var myrefresh = setInterval(function(){
                         console.log("Sudah Comment")
                         clearInterval(myrefresh);
                         clicksend();
-
+                        throw new Error();
                         return;
                     } else {
                         location.href = "about:blank"
@@ -363,7 +362,7 @@ var myrefresh = setInterval(function(){
                         console.log("Sudah Comment")
                         clearInterval(myrefresh);
                         clicksend();
-
+                        throw new Error();
                         return;
                     } else {
                         location.href = "about:blank"
@@ -378,7 +377,7 @@ var myrefresh = setInterval(function(){
                         console.log("Sudah Comment")
                         clearInterval(myrefresh);
                         clicksend();
-
+                        throw new Error();
                         return;
                     } else {
                         location.href = "about:blank"
@@ -393,7 +392,7 @@ var myrefresh = setInterval(function(){
                         console.log("Sudah Comment")
                         clearInterval(myrefresh);
                         clicksend();
-
+                        throw new Error();
                         return;
                     } else {
                         location.href = "about:blank"
@@ -408,7 +407,7 @@ var myrefresh = setInterval(function(){
                         console.log("Sudah Comment")
                         clearInterval(myrefresh);
                         clicksend();
-
+                        throw new Error();
                         return;
                     } else {
                         location.href = "about:blank"
@@ -423,7 +422,7 @@ var myrefresh = setInterval(function(){
                         console.log("Sudah Comment")
                         clearInterval(myrefresh);
                         clicksend();
-
+                        throw new Error();
                         return;
                     } else {
                         location.href = "about:blank"
@@ -439,7 +438,7 @@ var myrefresh = setInterval(function(){
                         console.log("Sudah Comment")
                         clearInterval(myrefresh);
                         clicksend();
-
+                        throw new Error();
                         return;
                     } else {
                         location.href = "about:blank"
@@ -455,7 +454,7 @@ var myrefresh = setInterval(function(){
                         console.log("Sudah Comment")
                         clearInterval(myrefresh);
                         clicksend();
-
+                        throw new Error();
                         return;
                     } else {
                         location.href = "about:blank"
@@ -502,32 +501,32 @@ var myrefresh = setInterval(function(){
 
     var urutkan = document.querySelectorAll("[data-mcomponent='ServerTextArea']");
     var waktupost = document.getElementsByClassName("native-text");
-    if (document.readyState === "complete") {
-        for (var cok = 0; cok < urutkan.length; cok++) {
-            if(urutkan[cok].textContent.includes("URUTKAN")) {
-                urutkan[cok].click()
-
+    if(!document.querySelectorAll("[role='presentation']")[0]){
+        if (document.readyState === "complete") {
+            for (var cok = 0; cok < urutkan.length; cok++) {
+                if(urutkan[cok].textContent.includes("URUTKAN")) {
+                    urutkan[cok].click()
+                }
             }
         }
-
     }
+    if(document.getElementsByClassName("loading-overlay").length == 0 ){
 
-
-    if (document.readyState === "complete") {
-        for (var coki = 0; coki < waktupost.length; coki++) {
-            if(waktupost[coki].textContent.includes("Aktivitas")) {
-                waktupost[coki].click()
-
+        if(document.querySelectorAll("[role='presentation']")[0]){
+            if (document.readyState === "complete") {
+                for (var coki = 0; coki < waktupost.length; coki++) {
+                    if(waktupost[coki].textContent.includes("Aktivitas")) {
+                        waktupost[coki].click()
+                    }
+                }
             }
         }
-
     }
 
 }, refresh * 10)
 
 
 function clicksend() {
-
     /*Tampilkan TOMBOL SEND*/
     if(document.getElementsByClassName("textbox-submit-button")[0]){
         document.getElementsByClassName("textbox-submit-button")[0].style.display=""
@@ -543,6 +542,8 @@ function clicksend() {
         /*Tekan TOMBOL SEND*/
     }
 }
+
+
 
 function closer() {
     setTimeout(function(){location.href = "about:blank"},100)
