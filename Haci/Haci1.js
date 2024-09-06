742d011ea37cfc5176d31a875c01004831083145
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-06 14:16:25 +0700

    Update Haci1.js

--- Haci/Haci1.js
+++ Haci/Haci1.js
@@ -108,7 +108,7 @@ var admin = ["Siâo","aldi","aprilia","alenta","artha","adelia","ananda","aditia
              "brian","boboho","bobby","bonar","bella","bastian","bungaps","budi",
              "cinta","calvin","celsia","cila","cindy","chintya","carole","celine","cheline","corene",
              "denis","dollar","dewa","dewi","dinda","dika","dea","debby",
-             "echa","erika","elly","eugene","erwin","erna","elmira",
+             "echa","erika","elly","eleano","eugene","erwin","erna","elmira",
              "farah","febrian","fiana","farid","fahresa",
              "gretha",
              "herfizah","hana","hoihai","hefi","habib","harry",
@@ -125,9 +125,10 @@ var admin = ["Siâo","aldi","aprilia","alenta","artha","adelia","ananda","aditia
              "tink","thonex","tiara","tania","tag",
              "ujen",
              "vonny","viona","virna",
-             "wahid","wahzo","wok","wndt","wiena",
+             "wahid","wahzo","wok","wndt","wiena","wulan",
              "yoky","yasmine","yao","yohana",
              "zurro"];
+
 
 
 
