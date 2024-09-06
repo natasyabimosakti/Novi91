85d698e9691cd9c4aac9222e2e8b2f2f0552c9d4
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-06 14:19:47 +0700

    Update Perkutut4.js

--- Perkutut/Perkutut4.js
+++ Perkutut/Perkutut4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Perkutut4
 // @namespace    http://tampermonkey.net/
-// @version      3.98
+// @version      3.99
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Perkutut/Perkutut4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Perkutut/Perkutut4.js
@@ -104,7 +104,7 @@ var admin = ["Siâo","aldi","aprilia","alenta","artha","adelia","ananda","aditia
              "brian","boboho","bobby","bonar","bella","bastian","bungaps","budi",
              "cinta","calvin","celsia","cila","cindy","chintya","carole","celine","cheline","corene",
              "denis","dollar","dewa","dewi","dinda","dika","dea","debby",
-             "echa","erika","elly","eugene","erwin","erna","elmira",
+             "echa","erika","elly","eleano","eugene","erwin","erna","elmira",
              "farah","febrian","fiana","farid","fahresa",
              "gretha",
              "herfizah","hana","hoihai","hefi","habib","harry",
@@ -121,9 +121,10 @@ var admin = ["Siâo","aldi","aprilia","alenta","artha","adelia","ananda","aditia
              "tink","thonex","tiara","tania","tag",
              "ujen",
              "vonny","viona","virna",
-             "wahid","wahzo","wok","wndt","wiena",
+             "wahid","wahzo","wok","wndt","wiena","wulan",
              "yoky","yasmine","yao","yohana",
              "zurro"];
+
 
 
 
