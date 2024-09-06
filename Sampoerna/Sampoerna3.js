7eaf5ba722d8eff3ca6f016e2be44060a503ef82
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-06 14:21:49 +0700

    Update Sampoerna3.js

--- Sampoerna/Sampoerna3.js
+++ Sampoerna/Sampoerna3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sampoerna3
 // @namespace    http://tampermonkey.net/
-// @version      3.92
+// @version      3.93
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna3.js
@@ -109,7 +109,7 @@ var admin = ["Siâo","aldi","aprilia","alenta","artha","adelia","ananda","aditia
              "brian","boboho","bobby","bonar","bella","bastian","bungaps","budi",
              "cinta","calvin","celsia","cila","cindy","chintya","carole","celine","cheline","corene",
              "denis","dollar","dewa","dewi","dinda","dika","dea","debby",
-             "echa","erika","elly","eugene","erwin","erna","elmira",
+             "echa","erika","elly","eleano","eugene","erwin","erna","elmira",
              "farah","febrian","fiana","farid","fahresa",
              "gretha",
              "herfizah","hana","hoihai","hefi","habib","harry",
@@ -126,7 +126,7 @@ var admin = ["Siâo","aldi","aprilia","alenta","artha","adelia","ananda","aditia
              "tink","thonex","tiara","tania","tag",
              "ujen",
              "vonny","viona","virna",
-             "wahid","wahzo","wok","wndt","wiena",
+             "wahid","wahzo","wok","wndt","wiena","wulan",
              "yoky","yasmine","yao","yohana",
              "zurro"];
 
