153717f0b993d17c365892a3803c7c84b966f9a4
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-10 22:58:26 +0700

    Update sukasuka4.js

--- Sukasuka/sukasuka4.js
+++ Sukasuka/sukasuka4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BENT4
 // @namespace    http://tampermonkey.net/
-// @version      3.75
+// @version      3.76
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sukasuka/sukasuka4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sukasuka/sukasuka4.js
@@ -23,7 +23,7 @@ var namagroup2 = 'BUKU';
 var Comment2 = 'IYATOTO DUINIATOTO23 08*87*53'; 
 
 var namagroup3 = 'TIKTAK';
-var Comment3 = '#Tiktaktogel / DICIAKKUCING / 94 , 89 , 29'; 
+var Comment3 = 'Tiktaktogel / DICIAKKUCING / 94 , 89 , 29'; 
 
 var namagroup4 = 'GAIB';
 var Comment4 = 'GAIB4D=DINDAMANIS12=31*44*59'; 
