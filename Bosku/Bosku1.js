8d0ff168d40e6265f3a7c165f2e2c1dbbfa5db94
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-12 03:00:08 +0700

    Update Bosku1.js

--- Bosku/Bosku1.js
+++ Bosku/Bosku1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BOSQUE1
 // @namespace    http://tampermonkey.net/
-// @version      3.89
+// @version      3.90
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku1.js
@@ -138,14 +138,7 @@ var Backlist7 = "result";
 
 
 var myrefresh = setInterval(function(){
-    var lasturl
-    if(document.URL.length > 40) {
-        lasturl = document.URL
-    }else{
-        document.location.href = lasturl
-    }
-
-
+    
 
     if (tm == "" || tm == undefined || tm == null) {
         GM.setValue("time", hour);
@@ -553,15 +546,4 @@ function closer() {
 
 
 }
-var lasturlku
-var jar = setInterval(function(){
 
-    if(location.href.includes("group")){
-        lasturlku = location.href;
-
-    }
-    if(location.href.length <= 30 ){
-        location.href = lasturlku
-    }
-
-},1000)
