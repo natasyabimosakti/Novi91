ad86c8da97fecf9274f1f5bd31a7605b77060c87
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-11 12:34:38 +0700

    Update Yatim1.js

--- Yatim/Yatim1.js
+++ Yatim/Yatim1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Yatim1
 // @namespace    http://tampermonkey.net/
-// @version      3.75
+// @version      3.76
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Yatim/Yatim1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Yatim/Yatim1.js
@@ -39,7 +39,7 @@ var Comment7 = 'ASEPTOGEL GUNGKLIKK 37*15*50';
 var namagroup8 = 'ANGKER';
 var Comment8 = 'ANGKER4D=GUNGYUDZ=14*57*42'; 
 
-var namagroup9 = 'GIL4';
+var namagroup9 = 'G1LA';
 var Comment9 = 'GILA4D=GUNGCUAKZ=37*15*50'; 
 
 var namagroup10 = 'JNE';
