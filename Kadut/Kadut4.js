eeb0c951c4d6ea1be2fb6c66b7312426bc480dc3
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-12 03:03:55 +0700

    Update Kadut4.js

--- Kadut/Kadut4.js
+++ Kadut/Kadut4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT4
 // @namespace    http://tampermonkey.net/
-// @version      3.100
+// @version      3.101
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kadut/Kadut4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kadut/Kadut4.js
@@ -73,6 +73,7 @@ var Comment18 = 'Kadut4';
 
 
 
+
 var refresh = 30;
 var d = new Date();
 var hour = d.getHours();
@@ -141,14 +142,7 @@ var Backlist7 = "result";
 
 
 var myrefresh = setInterval(function(){
-    var lasturl
-    if(document.URL.length > 40) {
-        lasturl = document.URL
-    }else{
-        document.location.href = lasturl
-    }
-
-
+    
 
     if (tm == "" || tm == undefined || tm == null) {
         GM.setValue("time", hour);
@@ -556,15 +550,4 @@ function closer() {
 
 
 }
-var lasturlku
-var jar = setInterval(function(){
 
-    if(location.href.includes("group")){
-        lasturlku = location.href;
-
-    }
-    if(location.href.length <= 30 ){
-        location.href = lasturlku
-    }
-
-},1000)
