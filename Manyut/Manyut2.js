ce8dde6ccbe626ee1a00d2fc2c7969ee79dfe002
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-11 12:32:06 +0700

    Update Manyut2.js

--- Manyut/Manyut2.js
+++ Manyut/Manyut2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MANYUT2
 // @namespace    http://tampermonkey.net/
-// @version      3.166
+// @version      3.167
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut2.js
@@ -41,7 +41,7 @@ var Comment7 = 'ASEPTOGEL EKOCOK75 11*20*83';
 var namagroup8 = 'ANGKER';
 var Comment8 = 'ANGKER4D=EKORKU42=33*92*66'; 
 
-var namagroup9 = 'GIL4';
+var namagroup9 = 'G1LA';
 var Comment9 = 'GILA4D=EKORENG51=33*92*38'; 
 
 var namagroup10 = 'JNE';
