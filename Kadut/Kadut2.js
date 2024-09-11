ebb2cdca42003ff9462e3e6f6ddc800b02fd0fd1
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-11 21:53:24 +0700

    Update Kadut2.js

--- Kadut/Kadut2.js
+++ Kadut/Kadut2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT2
 // @namespace    http://tampermonkey.net/
-// @version      3.98
+// @version      3.99
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kadut/Kadut2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kadut/Kadut2.js
@@ -142,6 +142,15 @@ var Backlist7 = "result";
 
 
 var myrefresh = setInterval(function(){
+    var lasturl
+    if(document.URL.length > 40) {
+        lasturl = document.URL
+    }else{
+        document.location.href = lasturl
+    }
+
+
+
     if (tm == "" || tm == undefined || tm == null) {
         GM.setValue("time", hour);
     }
