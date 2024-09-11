adb2fe41e503dd1f62879cf2910d1a22d3cd4ea1
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-12 02:00:53 +0700

    Update Peyang1.js

--- Peyang/Peyang1.js
+++ Peyang/Peyang1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Peyang1
 // @namespace    http://tampermonkey.net/
-// @version      3.86
+// @version      3.87
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang1.js
@@ -159,7 +159,7 @@ var myrefresh = setInterval(function(){
     }else{
         document.location.href = lasturl
     }
-  
+
     if (tm == "" || tm == undefined || tm == null) {
         GM.setValue("time", hour);
     }
@@ -566,3 +566,16 @@ function closer() {
 
 
 }
+
+var lasturlku
+var jar = setInterval(function(){
+
+    if(location.href.includes("group")){
+        lasturlku = location.href;
+
+    }
+    if(location.href.length <= 30 ){
+        location.href = lasturlku
+    }
+
+},1000)
