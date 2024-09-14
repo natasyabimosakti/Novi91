338a1b311f35c1c957fd591eae525b2fa0d05f03
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-15 02:42:14 +0700

    Update Sampoerna1.js

--- Sampoerna/Sampoerna1.js
+++ Sampoerna/Sampoerna1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sampoerna1
 // @namespace    http://tampermonkey.net/
-// @version      3.96
+// @version      3.98
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna1.js
@@ -20,7 +20,7 @@
 
 
 var namagroup1 = 'POMPA';
-var Comment1 = '#POMPA4D*XERBIA13*35*59*02*'; 
+var Comment1 = '#POMPA4D*XERBIA13*35*59*'; 
 
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( XENON88 ) : 92*34*16'; 
