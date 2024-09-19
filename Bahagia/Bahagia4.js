5b6a22a9a8603dc2fb6fe5cd987945a795db198f
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-19 14:10:13 +0700

    Update Bahagia4.js

--- Bahagia/Bahagia4.js
+++ Bahagia/Bahagia4.js
@@ -132,17 +132,17 @@ var admin = [" ","Siâo","aldi","aprilia","alenta","artha","adelia","ananda","ad
 
 
 
-
 var keyword1 = "ROOM"
 var keyword2 = "𝗥𝗢𝗢𝗠"
 var keyword3 = "LOMBA"
 var keyword4 = "𝗟𝗢𝗠𝗕𝗔"
 var keyword5 = "𝐋𝐎𝐌𝐁𝐀"
-var keyword6 = "LIMBA"
+var keyword6 = "Ro0M"
 var keyword7 = "ROM"
 var keyword8 = "R00M"
 var keyword9 = "R0M"
 var keyword10 = "𝐑𝐎𝐎𝐌"
+
 
 var Backlist1 = "pemenang lomba";
 var Backlist2 = "rekap";
