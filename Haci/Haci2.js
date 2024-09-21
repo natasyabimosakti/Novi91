12d9e044068d615c8143fbef617525182b11d57b
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-21 22:28:50 +0700

    Update Haci2.js

--- Haci/Haci2.js
+++ Haci/Haci2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hachi2
 // @namespace    http://tampermonkey.net/
-// @version      3.106
+// @version      3.107
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci2.js
@@ -20,8 +20,8 @@
 /*======================================================================Paste Script Tampermonkey di sini===============================================================*/
 
 
-var namagroup1 = 'POMPA';
-var Comment1 = '#POMPA4D*FERTULE*73*04*'; 
+var namagroup1 = 'Cancer';
+var Comment1 = 'CANCERTOTO ( FERTULE ) : 73*04*81'; 
 
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( FERCUKZ ) : 85*08*60'; 
