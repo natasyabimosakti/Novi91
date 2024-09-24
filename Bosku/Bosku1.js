66704eb8b4c4f8c58e816c26e3d832f69dd9d787
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-25 03:37:47 +0700

    Update Bosku1.js

--- Bosku/Bosku1.js
+++ Bosku/Bosku1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BOSQUE1
 // @namespace    http://tampermonkey.net/
-// @version      3.92
+// @version      3.93
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku1.js
@@ -133,7 +133,7 @@ var Backlist3 = "hasil";
 var Backlist4 = "room lomba freebet";
 var Backlist5 = "prediksi";
 var Backlist6 = "result";
-var Backlist7 = "result";
+var Backlist7 = "commen";
 
 
 
