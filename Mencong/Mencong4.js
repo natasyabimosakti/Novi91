f377a5775ab2de56b0e884cde47608299d9f8699
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-27 18:53:55 +0700

    Update Mencong4.js

--- Mencong/Mencong4.js
+++ Mencong/Mencong4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MENCONG4
 // @namespace    http://tampermonkey.net/
-// @version      3.177
+// @version      3.178
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Mencong/Mencong4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Mencong/Mencong4.js
@@ -47,7 +47,7 @@ var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(TOMSENG21)*74*82*84'; 
 
 var namagroup11 = 'CUYTOTO';
-var Comment11 = '#CUYTOTO*TOMLAK12*39*27*32'; 
+var Comment11 = 'CUYTOTO*TOMLAK12*39*27*32'; 
 
 var namagroup12 = 'MASTER KUY';
 var Comment12 = 'TOGELKUY TOMBIS34 25*16*85'; 
