f689d6fc4c52cee9add6711c9c645a413203d181
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-27 18:50:35 +0700

    Update Malboro3.js

--- Malboro/Malboro3.js
+++ Malboro/Malboro3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MALBORO 3
 // @namespace    http://tampermonkey.net/
-// @version      3.81
+// @version      3.82
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro3.js
@@ -47,7 +47,7 @@ var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(CAPCAI38)*52*26*70'; 
 
 var namagroup11 = 'CUYTOTO';
-var Comment11 = '#CUYTOTO*YOYOKWIK3*04*76*81'; 
+var Comment11 = 'CUYTOTO*YOYOKWIK3*04*76*81'; 
 
 var namagroup12 = 'MASTER KUY';
 var Comment12 = 'TOGELKUY CEBOKX2 00*28*07'; 
