96c25712db900f868e05831574ccbeca98b6750a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-27 18:49:15 +0700

    Update Kadut2.js

--- Kadut/Kadut2.js
+++ Kadut/Kadut2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT2
 // @namespace    http://tampermonkey.net/
-// @version      3.104
+// @version      3.105
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kadut/Kadut2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kadut/Kadut2.js
@@ -49,7 +49,7 @@ var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(SRIWING67)*18*52*36'; 
 
 var namagroup11 = 'CUYTOTO';
-var Comment11 = '#CUYTOTO*SRIANU36*00*20*30'; 
+var Comment11 = 'CUYTOTO*SRIANU36*00*20*30'; 
 
 var namagroup12 = 'MASTER KUY';
 var Comment12 = 'TOGELKUY SRISUWING 94*43*12'; 
