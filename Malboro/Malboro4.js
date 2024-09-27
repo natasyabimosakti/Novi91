7307645ad41bc1781d6e305d291e8490e0163f31
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-27 18:50:51 +0700

    Update Malboro4.js

--- Malboro/Malboro4.js
+++ Malboro/Malboro4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MALBORO 4
 // @namespace    http://tampermonkey.net/
-// @version      3.81
+// @version      3.82
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro4.js
@@ -48,7 +48,7 @@ var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(DUGALTOK)*94*89*29'; 
 
 var namagroup11 = 'CUYTOTO';
-var Comment11 = '#CUYTOTO*ZOKUTU72*08*87*53'; 
+var Comment11 = 'CUYTOTO*ZOKUTU72*08*87*53'; 
 
 var namagroup12 = 'MASTER KUY';
 var Comment12 = 'TOGELKUY DEBORA21 91*93*23'; 
