45644815fda68d7954f384bb6dc827dc6ef2d171
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-02 17:55:27 +0700

    Update Bosku3.js

--- Bosku/Bosku3.js
+++ Bosku/Bosku3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BOSQUE3
 // @namespace    http://tampermonkey.net/
-// @version      3.95
+// @version      3.96
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku3.js
@@ -46,8 +46,8 @@ var Comment9 = 'GILA4D=YOWESBEN6=61*89*97';
 var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(YONEKWIK37)*77*46*64'; 
 
-var namagroup11 = 'CUYTOTO';
-var Comment11 = 'CUYTOTO*YOYOKWIK30*72*39*64'; 
+var namagroup11 = 'TOYIB';
+var Comment11 = '#TOYIBSLOT ( YOKWIK47 ) : 72*39*64'; 
 
 var namagroup12 = 'MASTER KUY';
 var Comment12 = 'TOGELKUY YOKOJO3 80*51*95'; 
