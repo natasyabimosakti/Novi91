9d024df841a600438896c330c8fb05c8fc1b3102
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-02 18:05:13 +0700

    Update Yatim2.js

--- Yatim/Yatim2.js
+++ Yatim/Yatim2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Yatim2
 // @namespace    http://tampermonkey.net/
-// @version      3.83
+// @version      3.84
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Yatim/Yatim2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Yatim/Yatim2.js
@@ -46,8 +46,8 @@ var Comment9 = 'GILA4D=FERGILL=02*09*60';
 var namagroup10 = 'JNE';
 var Comment10 = '#JNETOTO(FERJENS)*67*98*05'; 
 
-var namagroup11 = 'CUYTOTO';
-var Comment11 = 'CUYTOTO*FERRAT*78*46*77'; 
+var namagroup11 = 'TOYIB';
+var Comment11 = '#TOYIBSLOT ( FERYEBS ) : 78*46*77'; 
 
 var namagroup12 = 'MASTER KUY';
 var Comment12 = 'TOGELKUY FERKOYY 71*96*75'; 
