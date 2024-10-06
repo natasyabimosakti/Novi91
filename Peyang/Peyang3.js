4fdf9052ec9bd9fc6c2ede4783df92d570ea371c
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-07 05:58:08 +0700

    Update Peyang3.js

--- Peyang/Peyang3.js
+++ Peyang/Peyang3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Peyang3
 // @namespace    http://tampermonkey.net/
-// @version      3.93
+// @version      3.94
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang3.js
