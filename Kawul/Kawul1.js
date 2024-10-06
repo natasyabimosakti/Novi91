e039d0a829bfebde6b25633bf8a9d4617212eed6
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-07 05:59:03 +0700

    Update Kawul1.js

--- Kawul/Kawul1.js
+++ Kawul/Kawul1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kawul1
 // @namespace    http://tampermonkey.net/
-// @version      3.95
+// @version      3.96
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul1.js
