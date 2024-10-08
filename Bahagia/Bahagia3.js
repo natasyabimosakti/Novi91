13684ac9e245e923e62bdec8b1737ed9e3bf6daa
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-08 09:03:33 +0700

    Update Bahagia3.js

--- Bahagia/Bahagia3.js
+++ Bahagia/Bahagia3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bahagia3
 // @namespace    http://tampermonkey.net/
-// @version      3.70
+// @version      3.72
 // @description  dunia
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia3.js
@@ -116,7 +116,7 @@ var keyword2 = "𝗥𝗢𝗢𝗠"
 var keyword3 = "LOMBA"
 var keyword4 = "𝗟𝗢𝗠𝗕𝗔"
 var keyword5 = "𝐋𝐎𝐌𝐁𝐀"
-var keyword6 = "LIMBA"
+var keyword6 = "❤️"
 var keyword7 = "ROM"
 var keyword8 = "R00M"
 var keyword9 = "login"
