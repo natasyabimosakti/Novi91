40aa6a939efe24da864e5c648c90102d09bf1005
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-08 23:03:01 +0700

    Update Peyang3.js

--- Peyang/Peyang3.js
+++ Peyang/Peyang3.js
@@ -34,8 +34,8 @@ var Comment5 = '#INSTANTOTO (NOVISAN31) = 82*25';
 var namagroup6 = 'DIVA4D';
 var Comment6 = '#DIVA4D (NOVIRAMBU3) = 13*26*51'; 
 
-var namagroup7 = 'ORMAS';
-var Comment7 = 'ORMASTOTO(NOVITARU34) : 36*17*31'; 
+var namagroup7 = 'MAIN';
+var Comment7 = '#(MAINTOGEL365) (NOVITARU34) : 36*17*31'; 
 
 var namagroup8 = 'TOK99T0T0';
 var Comment8 = 'Tok99Toto ( NOVITAS53 ) : 16*18*12'; 
