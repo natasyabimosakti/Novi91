4d8b321335ea01191269ec528028853cdce45b82
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-08 22:58:47 +0700

    Update Kawul2.js

--- Kawul/Kawul2.js
+++ Kawul/Kawul2.js
@@ -34,8 +34,8 @@ var Comment5 = '#INSTANTOTO (SRITILKON) = 61*30';
 var namagroup6 = 'DIVA4D';
 var Comment6 = '#DIVA4D (SRIWIDURTI) = 95*42*36'; 
 
-var namagroup7 = 'ORMAS';
-var Comment7 = 'ORMASTOTO(SRINDIM63) : 06*76*46'; 
+var namagroup7 = 'MAIN';
+var Comment7 = '#(MAINTOGEL365) (SRINDIM63) : 06*76*46'; 
 
 var namagroup8 = 'TOK99T0T0';
 var Comment8 = 'Tok99Toto ( SRINDIM63 ) : 26*56*97'; 
