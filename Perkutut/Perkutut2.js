e26dc4407a988c31a4a951ff3a1e405a3ceda33e
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-09 14:11:06 +0700

    Update Perkutut2.js

--- Perkutut/Perkutut2.js
+++ Perkutut/Perkutut2.js
@@ -34,7 +34,7 @@ var namagroup6 = 'DIVA4D';
 var Comment6 = '#DIVA4D (ELANG38) = 50*03*30'; 
 
 var namagroup7 = 'MAIN';
-var Comment7 = '#(MAINTOGEL365) (ELANG94) : 95*83*23'; 
+var Comment7 = '#(MAINTOGEL365) (ELANG72) : 95*83*23'; 
 
 var namagroup8 = 'TOK99T0T0';
 var Comment8 = 'Tok99Toto ( ELANG51 ) : 93*43*58'; 
