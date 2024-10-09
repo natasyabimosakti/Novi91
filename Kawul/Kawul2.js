ffd05053880d82beb7418cc2ae21e220c4a1dfc4
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-09 14:16:15 +0700

    Update Kawul2.js

--- Kawul/Kawul2.js
+++ Kawul/Kawul2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kawul2
 // @namespace    http://tampermonkey.net/
-// @version      3.98
+// @version      3.99
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul2.js
@@ -35,7 +35,7 @@ var namagroup6 = 'DIVA4D';
 var Comment6 = '#DIVA4D (SRIWIDURTI) = 95*42*36'; 
 
 var namagroup7 = 'MAIN';
-var Comment7 = '#(MAINTOGEL365) (SRINDIM63) : 06*76*46'; 
+var Comment7 = '#(MAINTOGEL365) (SRILAKCU) : 06*76*46'; 
 
 var namagroup8 = 'TOK99T0T0';
 var Comment8 = 'Tok99Toto ( SRINDIM63 ) : 26*56*97'; 
