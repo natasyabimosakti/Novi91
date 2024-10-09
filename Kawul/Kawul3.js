8b7fb1415a98a951a06aa866fe5efff2435a4a13
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-09 14:28:40 +0700

    Update Kawul3.js

--- Kawul/Kawul3.js
+++ Kawul/Kawul3.js
@@ -34,7 +34,7 @@ var namagroup6 = 'DIVA4D';
 var Comment6 = '#DIVA4D (RISRAIMU) = 72*14*35'; 
 
 var namagroup7 = 'MAIN';
-var Comment7 = '#(MAINTOGEL365) (JOROKLUH) : 87*22*02'; 
+var Comment7 = '#(MAINTOGEL365) (RIROKKWE) : 87*22*02'; 
 
 var namagroup8 = 'TOK99T0T0';
 var Comment8 = 'Tok99Toto ( RIROKKWE ) : 74*15*89'; 
