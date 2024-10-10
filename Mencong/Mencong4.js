432e2bc837a4e29f49ba996fc8b3c8a95be43689
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-10 15:48:27 +0700

    Update Mencong4.js

--- Mencong/Mencong4.js
+++ Mencong/Mencong4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MENCONG4
 // @namespace    http://tampermonkey.net/
-// @version      3.180
+// @version      3.181
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Mencong/Mencong4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Mencong/Mencong4.js
@@ -94,28 +94,27 @@ var id16 = await GM.getValue(16);
 var id17 = await GM.getValue(17);
 var id18 = await GM.getValue(18);
 
-var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","adi","aldi","ananda","alde","adm","ayesha","aqisya","anjani","apri","amore","arifin","ayunda",
-             "boleng","biru","bobby","bastian","bambang",
-             "cristina","camb","cassa","che","cinta","celsia","calista","cahyo","cipto","claura",
-             "david",",dewa","desi","debby","dewi","dentoto","dika","delon",
-             "erwin","elvina",
+var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","aditia","aldi","ananda","alde","adm","ayesha","aqisya","arga","arifin",
+             "boleng","biru","bobby","bastian","boboho","bola",
+             "cristina","camb","cassa","che","cinta","celsia","cila","calon","chika",
+             "david",",dewa","desi","debby","dewi","dentoto","dika","dealova",
+             "erwin","emilia",
              "fira","fahresa",
-             "gita","genzo",
-             "habib","hefi","hoihai","herfizah",
-             "icha","iyatoto",
-             "jordi","jaguar","jne","je pe","jess","junior",
-             "kei","kumbara","kembar","kotna","karina",
-             "lianda","lusiana","lina","laura","lia",
-             "mahendra","monica","mey","mersya","mad","multi","mariana","melati","meleqq","megaways","minion","melly",
-             "nasution","nyocol","naura","neng","nino","nona","neman","novi","nella","nayla","naomi",
-             "oscar",
-             "pung","puput","priyan","primus","primus",
-             "ratu","rio","ria","rikodo","rizal","roy","rendy","rahma",
-             "sandiego","san","sanjaya","siska","safar","sinta","surianti","satria","sapto","salsabila","sanchez","sofia","sonia","serena","specialis",
-             "tink","tiktak","tiara","tatang",
-             "yanty","yoky","yohana","yura","yaya",
-             "wulan","wok","wak",
-             "zuko"];
+             "gita",
+             "habib","hefi","hoihai","hana",
+             "icha","iyatoto","invest","ivanna","inisial",
+             "jordi","jaguar","jne","jovanka","jessica","je pe","jess","jenifer",
+             "keitogel","kumbara","kembar","kotna","karina","katharina","kemon",
+             "lianda","lusiana","lina","laura","lehman","leader","leon","lidya",
+             "mahendra","monica","mey","mersya","mad","multi","mariana","melati","male","megaways","manu",
+             "nasution","nyocol","naura","neng","nino","nona","neman","novi","nella","nahdya","nur","namira","nindy",
+             "oscar","ozawa",
+             "pung","puput","priyan","primus","primus","pencari","pricilia",
+             "ratu","rio","ria","rikodo","rizal","roy","rendy","rana",
+             "sandiego","san","sanjaya","siska","safar","sinta","surianti","satria","sapto","salsabila","sanchez","sofia","sonia","serena","sahara","specialis","sam","sasha",
+             "tink","tiktak","tiara","tatang","tania",
+             "yanty","yoky","yohana",
+             "wulan","wok"];
 
 
 var keyword1 = "ROOM"
