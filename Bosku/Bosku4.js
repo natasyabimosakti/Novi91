e914dd0894dc72fbd47d885c1e098030abdb4253
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-11 18:10:57 +0700

    Update Bosku4.js

--- Bosku/Bosku4.js
+++ Bosku/Bosku4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BOSQUE4
 // @namespace    http://tampermonkey.net/
-// @version      3.97
+// @version      3.98
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku4.js
@@ -73,7 +73,7 @@ var Comment18 = 'Bosku4';
 
 
 
-var refresh = 30;
+var refresh = 40;
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
