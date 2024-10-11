3545ffde10ec5f741eb09c458eb822de5d7e5602
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-11 18:21:16 +0700

    Update Mencong2.js

--- Mencong/Mencong2.js
+++ Mencong/Mencong2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MENCONG2
 // @namespace    http://tampermonkey.net/
-// @version      3.181
+// @version      3.182
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Mencong/Mencong2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Mencong/Mencong2.js
@@ -72,7 +72,7 @@ var Comment18 = 'Mencong2';
 
 
 
-var refresh = 30;
+var refresh = 40;
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
