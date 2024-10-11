2bacc9f56dcdab68e3f22046623d256c16ba07ee
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-11 18:11:54 +0700

    Update Curut3.js

--- Curut/Curut3.js
+++ Curut/Curut3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW CURUT3
 // @namespace    http://tampermonkey.net/
-// @version      3.94
+// @version      3.95
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut3.js
@@ -74,7 +74,7 @@ var Comment18 = 'Curut3';
 
 
 
-var refresh = 30;
+var refresh = 40;
 var d = new Date();
 var hour = d.getHours();
 var tm = await GM.getValue("time");
