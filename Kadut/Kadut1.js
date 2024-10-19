c9d915bd659b304c755a03e6de2a91242fbb55c9
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-19 11:43:27 +0700

    Update Kadut1.js

--- Kadut/Kadut1.js
+++ Kadut/Kadut1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT1
 // @namespace    http://tampermonkey.net/
-// @version      3.110
+// @version      3.111
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kadut/Kadut1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kadut/Kadut1.js
@@ -42,7 +42,7 @@ var Comment7 = 'ASEPTOGEL BOLINA61 25*85*67';
 var namagroup8 = 'MENARA';
 var Comment8 = '#MENARA4D=BUDELTUNG= 01*44'; 
 
-var namagroup9 = 'G1LA';
+var namagroup9 = 'GIL4';
 var Comment9 = 'GILA4D=BUANGKALI=88*75*65'; 
 
 var namagroup10 = 'JNE';
