407f873ea6c04c76c4cf2c67781a52d3625e62f0
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-19 12:17:03 +0700

    Update Kadut3.js

--- Kadut/Kadut3.js
+++ Kadut/Kadut3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT3
 // @namespace    http://tampermonkey.net/
-// @version      3.112
+// @version      3.113
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kadut/Kadut3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kadut/Kadut3.js
