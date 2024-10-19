0af20ccc8afa35e397ae935de4392f601cff3f49
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-19 11:46:01 +0700

    Update sukasuka2.js

--- Sukasuka/sukasuka2.js
+++ Sukasuka/sukasuka2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BENT2
 // @namespace    http://5.net/
-// @version      3.90
+// @version      3.91
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sukasuka/sukasuka2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sukasuka/sukasuka2.js
@@ -41,7 +41,7 @@ var Comment7 = 'ASEPTOGEL BAMIN12 12*86*68';
 var namagroup8 = 'MENARA';
 var Comment8 = '#MENARA4D=BINOMO77= 71*96'; 
 
-var namagroup9 = 'G1LA';
+var namagroup9 = 'GIL4';
 var Comment9 = 'GILA4D=BEKOL12=71*96*75'; 
 
 var namagroup10 = 'JNE';
