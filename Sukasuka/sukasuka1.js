16b6bbebc7c4d3c289b8092ef4ddc1143141d069
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-19 12:22:36 +0700

    Update sukasuka1.js

--- Sukasuka/sukasuka1.js
+++ Sukasuka/sukasuka1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BENT1
 // @namespace    http://tampermonkey.net/
-// @version      3.90
+// @version      3.91
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sukasuka/sukasuka1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sukasuka/sukasuka1.js
