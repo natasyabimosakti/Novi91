a02ceb3ce001f24ceb4b51b684cbd7606f86d48f
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-03 00:46:00 +0700

    Update Kawul4.js

--- Kawul/Kawul4.js
+++ Kawul/Kawul4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kawul4
 // @namespace    http://tampermonkey.net/
-// @version      3.103
+// @version      3.104
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul4.js
@@ -73,7 +73,7 @@ var namagroup18 = 'lajw';
 var Comment18 = 'asek';
 
 
-
+var refresh = 30;
 
 var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","aditia","aldi","ananda","alde","adm","ayesha","aqisya","arga","arifin","aru","agung","alenta",
              "boleng","biru","bobby","bastian","boboho","bola","bunga",
