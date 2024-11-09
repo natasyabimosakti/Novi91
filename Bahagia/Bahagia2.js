28940d0d603b0b59b78715ea78b6b480344a068c
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-10 05:57:57 +0700

    Update Bahagia2.js

--- Bahagia/Bahagia2.js
+++ Bahagia/Bahagia2.js
@@ -74,7 +74,7 @@ var Comment16 = 'CITA4D*BOMBAA54*15*92*39';
 
 
 var namagroup17 = 'Jawatengah';
-var Comment17 = 'Bahagia1';
+var Comment17 = 'Bahagia2';
 
 var namagroup18 = 'lajw';
 var Comment18 = 'asek';
