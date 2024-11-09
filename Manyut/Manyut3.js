0a1da5b24ea69c5d0f548d034ce13f342895a88a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-10 06:51:17 +0700

    Update Manyut3.js

--- Manyut/Manyut3.js
+++ Manyut/Manyut3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MANYUT3
 // @namespace    http://tampermonkey.net/
-// @version      3.188
+// @version      3.189
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut3.js
@@ -56,8 +56,8 @@ var Comment12 = 'TOGELKUY ERAGUN68 52*26*70';
 var namagroup13 = 'TASIK';
 var Comment13 = '#TASIKTOTO(ERFGUN35) = 04*76'; 
 
-var namagroup14 = 'DENTOTO';
-var Comment14 = '#DENTOTO 00*28*07 ( ERFGUN12 )'; 
+var namagroup14 = 'TGL123';
+var Comment14 = '#(TOGEL123) = (ERFGUN12) 00*28*07'; 
 
 var namagroup15 = 'VESPA';
 var Comment15 = 'VESPATOGEL (ERKON23) 34*13*79'; 
