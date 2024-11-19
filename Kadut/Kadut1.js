b1fa09cf50a8c6e080fa7674762a44d6f8c544eb
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-20 06:47:21 +0700

    Update Kadut1.js

--- Kadut/Kadut1.js
+++ Kadut/Kadut1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT1
 // @namespace    http://tampermonkey.net/
-// @version      3.123
+// @version      3.124
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kadut/Kadut1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kadut/Kadut1.js
@@ -57,8 +57,8 @@ var Comment12 = 'TOGELKUY BUSUNGMU62 29*74*04';
 var namagroup13 = 'TASIK';
 var Comment13 = '#TASIKTOTO(BUANGKABEH) = 96*83'; 
 
-var namagroup14 = 'TGL123';
-var Comment14 = '#(TOGEL123) = (BOTAP27) 19*59*03'; 
+var namagroup14 = 'SILA';
+var Comment14 = '‌#SILATOGEL*BOTAP27*19*59*03*BETTING'; 
 
 var namagroup15 = 'VESPA';
 var Comment15 = 'VESPATOGEL (BOTAP27) 25*85*41'; 
@@ -86,15 +86,16 @@ var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","adi","aldi
              "habib","hefi","hoihai","herfizah",
              "icha","iyatoto","intan",
              "jordi","jaguar","jne","je pe","jess","junior","jovanka","jasmine",
-             "kei","kumbara","kembar","kotna","karina",
+             "kei","kumbara","kembar","kotna","karina","kopi",
              "lianda","lusiana","lina","laura","lia",
              "mahendra","monica","mey","mersya","mad","multi","mariana","melati","meleqq","megaways","minion","melly","monicha","manu","maryam","mode",
-             "nasution","nyocol","naura","neng","nino","nona","neman","novi","nella","nayla","naomi",
+             "nasution","nyocol","naura","neng","nino","nona","neman","novi","nella","nayla","naomi","nica",
              "oscar","ozawa",
              "pung","puput","priyan","primus","primus","prediction",
              "ratu","rio","ria","rikodo","rizal","roy","rendy","rahma","ratsa",
              "sandiego","san","sanjaya","siska","safar","sinta","surianti","satria","sapto","salsabila","sanchez","sofia","sonia","serena","specialis","seojun","saskia","sifa",
              "tink","tiktak","tiara","tatang","tomi",
+             "xian",
              "yanty","yoky","yohana","yura","yaya",
              "wulan","wok","wak",
              "zuko"];
