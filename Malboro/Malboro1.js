774af34dfed5eacbbbbed052dbf5de29353b4f99
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-28 17:58:35 +0700

    Update Malboro1.js

--- Malboro/Malboro1.js
+++ Malboro/Malboro1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MALBORO 1
 // @namespace    http://tampermonkey.net/
-// @version      3.103
+// @version      3.104
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro1.js
