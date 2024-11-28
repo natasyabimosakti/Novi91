0141f868a404ebeade13ae612e9d87ab9495ec8f
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-28 17:58:50 +0700

    Update Manyut1.js

--- Manyut/Manyut1.js
+++ Manyut/Manyut1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MANYUT1
 // @namespace    http://tampermonkey.net/
-// @version      3.194
+// @version      3.195
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut1.js
