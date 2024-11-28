5ee0fb11099396c5af1f9e65f2e6ed98294338c4
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-28 17:58:48 +0700

    Update Manuk4.js

--- Manuk/Manuk4.js
+++ Manuk/Manuk4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MANUK 4
 // @namespace    http://tampermonkey.net/
-// @version      3.107
+// @version      3.108
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk4.js
