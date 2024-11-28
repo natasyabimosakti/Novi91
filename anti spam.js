c867cfe3450f62a512d5e4a3457216d2fac33d87
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-28 19:13:12 +0700

    Update anti spam.js

--- anti spam.js
+++ anti spam.js
@@ -1,20 +1,22 @@
 // ==UserScript==
 // @name         Anti Spam
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/refs/heads/main/anti%20spam.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/refs/heads/main/anti%20spam.js
 // @author       You
 // @match        http*://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
+// @grant        GM.setValue
+// @grant        GM.getValue
+// @grant        window.close
 // ==/UserScript==
 
 var lasturlku= ""
 var jar = setInterval(function(){
 
-
+var url = await GM.getValue(1);
     
 
 
@@ -33,13 +35,13 @@ var ujar = setInterval(function(){
     
 
     if(location.href.includes("group")){
-        lasturlku = location.href;  
+        GM.setValue(1,location.href);
         return;
     }
     if(lasturlku.length > 10){
 
     if(location.href.length <= 40 ){
-        location.href = lasturlku
+        location.href = url
         clearInterval(ujar)
         return;
     }
