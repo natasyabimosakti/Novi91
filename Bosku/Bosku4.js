e40d35b38255ee4eec6a4c056432f5a4469417df
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-30 18:46:21 +0700

    Update Bosku4.js

--- Bosku/Bosku4.js
+++ Bosku/Bosku4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BOSQUE4
 // @namespace    http://tampermonkey.net/
-// @version      3.117
+// @version      3.118
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku4.js
@@ -471,12 +471,14 @@ function clicksend() {
         clickEvent.initEvent ("mousedown", true, true);
         clicksendcoment.dispatchEvent (clickEvent);
         console.log("Comment Terkirim");
-        setInterval(closer,20)
+          closer()
+        location.href = "about:blank"
         /*Tekan TOMBOL SEND*/
-
     }
 }
 
 function closer() {
-    location.href = "about:blank"
+    setTimeout(function(){location.href = "about:blank"},100)
+
+
 }
