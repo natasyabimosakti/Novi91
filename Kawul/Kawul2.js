0fb12e74bc166bf7f8db321dc3e0891034560c76
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-30 15:01:02 +0700

    Update Kawul2.js

--- Kawul/Kawul2.js
+++ Kawul/Kawul2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kawul2
 // @namespace    http://tampermonkey.net/
-// @version      3.110
+// @version      3.111
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul2.js
@@ -470,13 +470,14 @@ function clicksend() {
         clickEvent.initEvent ("mousedown", true, true);
         clicksendcoment.dispatchEvent (clickEvent);
         console.log("Comment Terkirim");
+        closer()
         location.href = "about:blank"
-        setInterval(closer,20)
         /*Tekan TOMBOL SEND*/
-
     }
 }
 
 function closer() {
-    location.href = "about:blank"
+    setTimeout(function(){location.href = "about:blank"},100)
+
+
 }
