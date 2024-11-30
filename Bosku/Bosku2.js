93fd6288a8d0400cc68e7dbbce7a10bb547afd67
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-30 18:46:05 +0700

    Update Bosku2.js

--- Bosku/Bosku2.js
+++ Bosku/Bosku2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BOSQUE2
 // @namespace    http://tampermonkey.net/
-// @version      3.116
+// @version      3.117
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku2.js
@@ -469,12 +469,14 @@ function clicksend() {
         clickEvent.initEvent ("mousedown", true, true);
         clicksendcoment.dispatchEvent (clickEvent);
         console.log("Comment Terkirim");
-        setInterval(closer,20)
+          closer()
+        location.href = "about:blank"
         /*Tekan TOMBOL SEND*/
-
     }
 }
 
 function closer() {
-    location.href = "about:blank"
+    setTimeout(function(){location.href = "about:blank"},100)
+
+
 }
