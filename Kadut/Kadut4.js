859b37c6d38ff8da0d5af0766cebf7b476cbbd9f
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-30 20:09:28 +0700

    Update Kadut4.js

--- Kadut/Kadut4.js
+++ Kadut/Kadut4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW KADUT4
 // @namespace    http://tampermonkey.net/
-// @version      3.129
+// @version      3.130
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kadut/Kadut4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kadut/Kadut4.js
@@ -73,7 +73,7 @@ var Comment18 = 'Kadut4';
 
 
 
-var refresh = 30;
+var refresh = 50;
 
 var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","adi","aldi","ananda","alde","adm","ayesha","aqisya","anjani","apri","amore","arifin","ayunda","agung","arem","arifa",
              "boleng","biru","bobby","bastian","bambang","bogard",
@@ -98,7 +98,6 @@ var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","adi","aldi
              "yanty","yoky","yohana","yura","yaya",
              "wulan","wok","wak",
              "zuko"];
-
 
 
 var keyword1 = "ROOM"
@@ -446,7 +445,7 @@ function gameClosure() {
     var currentGame;
     return {
         start() {
-            currentGame = setInterval(game, 0)
+            currentGame = setInterval(game, 100)
         },
         stop() {
             clearInterval(currentGame)
@@ -454,7 +453,6 @@ function gameClosure() {
     }
 }
 var game = gameClosure()
-
 function clicksend() {
 
     jitter = 1
