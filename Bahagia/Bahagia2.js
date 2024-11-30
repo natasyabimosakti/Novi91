2b37fa922e55a540eac45895cea2748e138ab455
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-30 14:45:25 +0700

    Update Bahagia2.js

--- Bahagia/Bahagia2.js
+++ Bahagia/Bahagia2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bahagia2
 // @namespace    http://tampermonkey.net/
-// @version      3.83
+// @version      3.84
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia2.js
@@ -58,7 +58,7 @@ var namagroup13 = 'GOHT0G3L';
 var Comment13 = 'GOHTOGEL=BERANAK76=75*50*73'; 
 
 var namagroup14 = 'BLITAR';
-var Comment14 = '#BLITAR4D ( BIKANG12 ) : 52*35*54'; 
+var Comment14 = '#BLITAR4D SRILAKCU = 26*56'; 
 
 var namagroup15 = 'Hoho';
 var Comment15 = 'BAMIN12 : 28*42*13 #HOHOTOGEL'; 
