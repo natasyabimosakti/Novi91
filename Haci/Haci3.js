ec39a6841e5d8fedcad4e9c55162c3ff756c35c7
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-11-30 15:00:35 +0700

    Update Haci3.js

--- Haci/Haci3.js
+++ Haci/Haci3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hachi3
 // @namespace    http://tampermonkey.net/
-// @version      3.122
+// @version      3.123
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci3.js
@@ -473,13 +473,14 @@ function clicksend() {
         clickEvent.initEvent ("mousedown", true, true);
         clicksendcoment.dispatchEvent (clickEvent);
         console.log("Comment Terkirim");
+         closer()
         location.href = "about:blank"
-        setInterval(closer,20)
         /*Tekan TOMBOL SEND*/
-
     }
 }
 
 function closer() {
-    location.href = "about:blank"
+    setTimeout(function(){location.href = "about:blank"},100)
+
+
 }
