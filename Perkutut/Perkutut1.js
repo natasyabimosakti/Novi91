afb1642193b0ae6035ae52d9de614bc6889b1009
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-02 10:37:21 +0700

    Update Perkutut1.js

--- Perkutut/Perkutut1.js
+++ Perkutut/Perkutut1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Perkutut1
 // @namespace    http://tampermonkey.net/
-// @version      3.122
+// @version      3.123
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Perkutut/Perkutut1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Perkutut/Perkutut1.js
@@ -26,8 +26,8 @@ var Comment3 = 'Shiotogel4d (MANUKAN11) 22*62*71';
 var namagroup4 = 'TEMPO';
 var Comment4 = 'MANUK007 : 29*35*21 #TEMPOTOTO'; 
 
-var namagroup5 = 'WINTER';
-var Comment5 = '#WINTER4D(MANANUK213) = 80*25'; 
+var namagroup5 = 'ISTANA';
+var Comment5 = 'JANGKAUTOTO(MANANUK213) : 80*25*72'; 
 
 var namagroup6 = 'DIVA4D';
 var Comment6 = '#DIVA4D (MANUKGENI12) = 68*87*96'; 
@@ -53,8 +53,8 @@ var Comment12 = 'MANUKGENI12 : 22*62*71 #BESTOTO88';
 var namagroup13 = 'GOHT0G3L';
 var Comment13 = 'GOHTOGEL=MANUKX12=39*90*66'; 
 
-var namagroup14 = 'BLITAR';
-var Comment14 = '#BLITAR4D MANUKGENI12 = 80*25'; 
+var namagroup14 = 'TARUNG';
+var Comment14 = '#TARUNGTOTO (MANUKGENI12)=80_25'; 
 
 var namagroup15 = 'Hoho';
 var Comment15 = 'MANUKX12 : 68*87*96 #HOHOTOGEL'; 
@@ -75,15 +75,89 @@ var namagroup18 = 'lajw';
 var Comment18 = 'asek';
 
 
+// ==UserScript==
+// @name         Kawul4
+// @namespace    http://tampermonkey.net/
+// @version      3.112
+// @description  try to take over the world!
+// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul4.js
+// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul4.js
+// @author       You
+// @match        http*://*/*
+// @run-at       document-end
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        GM.setValue
+// @grant        GM.getValue
+// @grant        window.close
+// ==/UserScript==
 
-var refresh = 30;
+
+
+var namagroup1 = 'LICIN';
+var Comment1 = '#LICIN4D (SALAMANAE) 85*67*18'; 
+
+var namagroup2 = 'K86';
+var Comment2 = 'K86TOTO ( SAKUWIL ) : 47*12*94'; 
+
+var namagroup3 = 'PUSAT LOMBA';
+var Comment3 = 'Shiotogel4d (SANGEKABEH) 37*00*70'; 
+
+var namagroup4 = 'TEMPO';
+var Comment4 = 'SAKAREPMU : 73*54*51 #TEMPOTOTO'; 
+
+var namagroup5 = 'ISTANA';
+var Comment5 = 'JANGKAUTOTO(SAKAUBEN) : 25*03*91'; 
+
+var namagroup6 = 'DIVA4D';
+var Comment6 = '#DIVA4D (SAKEMBER) = 04*84*96'; 
+
+var namagroup7 = 'RNR';
+var Comment7 = '#RNR303(SAKAUBEN) : 24*79*08'; 
+
+var namagroup8 = 'TOK99T0T0';
+var Comment8 = 'Tok99Toto ( SALAMANAE ) : 69*41*62'; 
+
+var namagroup9 = 'Wellz';
+var Comment9 = 'SIJITOGEL SALAMANSEK 37*00*70'; 
+
+var namagroup10 = 'LING';
+var Comment10 = 'LING77 (SAIKIPIYE) 73*54'; 
+
+var namagroup11 = 'MAYAPADA';
+var Comment11 = 'BETT*Mayapada4D(BO)*SANTAYBOSKU*24*79*08'; 
+
+var namagroup12 = 'ROOM LOMBA TEBAK';
+var Comment12 = 'SAWERBOSE0 : 04*84*96 #BESTOTO88'; 
+
+var namagroup13 = 'GOHT0G3L';
+var Comment13 = 'GOHTOGEL=SAWERSEK=85*67*18'; 
+
+var namagroup14 = 'TARUNG';
+var Comment14 = '#TARUNGTOTO (SALAMANAE)=01_41'; 
+
+var namagroup15 = 'Hoho';
+var Comment15 = 'SAMBATAE : 25*03*91 #HOHOTOGEL'; 
+
+var namagroup16 = 'KECERDASAN';
+var Comment16 = 'CITA4D*SANGEKABEH*47*12*94';
+
+
+
+var namagroup17 = 'Jawatengah';
+var Comment17 = 'Kawul4';
+
+var namagroup18 = 'lajw';
+var Comment18 = 'asek';
+
+
+var refresh = 50;
 
 var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","aditia","aldi","ananda","alde","adm","ayesha","aqisya","arga","arifin","aru","agung","alenta","andi","arsyah","mrdepo","acha",
              "boleng","biru","bobby","bastian","boboho","bola","bunga",
              "cristina","camb","cassa","che","cinta","celsia","cila","calon","chika","calvin","chikaku",
              "david",",dewa","desi","debby","dewi","dentoto","dika","dealova","diva",
              "erwin","emilia",
-             "fira","fahresa","fiana",
+             "fira","fahresa","fiana","fahmi",
              "gita",
              "habib","hefi","hoihai","hana",",har",
              "icha","iyatoto","invest","ivanna","inisial",",ishaura",
@@ -446,7 +520,7 @@ function gameClosure() {
     var currentGame;
     return {
         start() {
-            currentGame = setInterval(game, 0)
+            currentGame = setInterval(game, 10)
         },
         stop() {
             clearInterval(currentGame)
@@ -472,7 +546,7 @@ function clicksend() {
         clickEvent.initEvent ("mousedown", true, true);
         clicksendcoment.dispatchEvent (clickEvent);
         console.log("Comment Terkirim");
-         closer()
+          closer()
         location.href = "about:blank"
         /*Tekan TOMBOL SEND*/
     }
