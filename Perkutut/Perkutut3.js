2d0ecff745e3a1b3ae1bd072d0eeb8af7426366d
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-09 18:13:47 +0700

    Update Perkutut3.js

--- Perkutut/Perkutut3.js
+++ Perkutut/Perkutut3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Perkutut3
 // @namespace    http://tampermonkey.net/
-// @version      3.126
+// @version      3.127
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Perkutut/Perkutut3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Perkutut/Perkutut3.js
@@ -25,8 +25,8 @@ var Comment2 = 'K86TOTO ( DOROMIBER ) : 36*59*02';
 var namagroup3 = 'PUSAT LOMBA';
 var Comment3 = 'Shiotogel4d (DOROMIBER2) 63*92*65'; 
 
-var namagroup4 = 'TEMPO';
-var Comment4 = 'DOROPOL12 : 19*99*17 #TEMPOTOTO'; 
+var namagroup4 = 'ZODIAK';
+var Comment4 = '#ZODIAKTOTO (DOROPOL12) : 19*99*17'; 
 
 var namagroup5 = 'ISTANA';
 var Comment5 = 'JANGKAUTOTO(DORODOR12) : 33*98*16'; 
@@ -80,8 +80,8 @@ var Comment18 = 'asek';
 
 var refresh = 50;
 
-var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","aditia","aldi","ananda","alde","adm","ayesha","aqisya","arga","arifin","aru","agung","alenta","andi","arsyah","mrdepo","acha",
-             "boleng","biru","bobby","bastian","boboho","bola","bunga",
+var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","aditia","aldi","ananda","alde","adm","ayesha","aqisya","arga","arifin","aru","agung","alenta","andi","arsyah","mrdepo","acha","annisa",
+             "boleng","biru","bobby","bastian","boboho","bola","bunga","bonbin",
              "cristina","camb","cassa","che","cinta","celsia","cila","calon","chika","calvin","chika",
              "david",",dewa","desi","debby","dewi","dentoto","dika","dealova","diva",
              "erwin","emilia",
@@ -99,7 +99,7 @@ var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","aditia","a
              "ratu","rio","ria","rikodo","rizal","roy","rendy","rana","rindi",
              "sandiego","san","sanjaya","siska","safar","sinta","surianti","satria","sapto","salsabila","sanchez","sofia","sonia","serena","sahara","specialis","sam","sasha","sintia","sifa","satria",
              "tink","tiktak","tiara","tatang","tania",
-             "yanty","yoky","yohana","yii","vero",
+             "yanty","yoky","yohana","yii","vero","vaulian",
              "wulan","wok","widya"];
 
 var keyword1 = "ROOM"
