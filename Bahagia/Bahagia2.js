62a1f2dd608fcb2f9a1d216bdd451f010f2d91b3
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-09 18:08:27 +0700

    Update Bahagia2.js

--- Bahagia/Bahagia2.js
+++ Bahagia/Bahagia2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bahagia2
 // @namespace    http://tampermonkey.net/
-// @version      3.87
+// @version      3.88
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia2.js
@@ -27,8 +27,8 @@ var Comment2 = 'K86TOTO ( BIMBO91 ) : 52*35*54';
 var namagroup3 = 'PUSAT LOMBA';
 var Comment3 = 'Shiotogel4d (BINAKARMA54) 28*42*13'; 
 
-var namagroup4 = 'TEMPO';
-var Comment4 = 'BISMON76 : 15*92*39 #TEMPOTOTO'; 
+var namagroup4 = 'ZODIAK';
+var Comment4 = '#ZODIAKTOTO (BISMON76) : 15*92*39'; 
 
 var namagroup5 = 'ISTANA';
 var Comment5 = 'JANGKAUTOTO(BINAKARMA54) : 46*91*61'; 
@@ -83,8 +83,8 @@ var Comment18 = 'asek';
 
 var refresh = 50;
 
-var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","aditia","aldi","ananda","alde","adm","ayesha","aqisya","arga","arifin","aru","agung","alenta","andi","arsyah","mrdepo","acha",
-             "boleng","biru","bobby","bastian","boboho","bola","bunga",
+var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","aditia","aldi","ananda","alde","adm","ayesha","aqisya","arga","arifin","aru","agung","alenta","andi","arsyah","mrdepo","acha","annisa",
+             "boleng","biru","bobby","bastian","boboho","bola","bunga","bonbin",
              "cristina","camb","cassa","che","cinta","celsia","cila","calon","chika","calvin","chika",
              "david",",dewa","desi","debby","dewi","dentoto","dika","dealova","diva",
              "erwin","emilia",
@@ -102,8 +102,9 @@ var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","aditia","a
              "ratu","rio","ria","rikodo","rizal","roy","rendy","rana","rindi",
              "sandiego","san","sanjaya","siska","safar","sinta","surianti","satria","sapto","salsabila","sanchez","sofia","sonia","serena","sahara","specialis","sam","sasha","sintia","sifa","satria",
              "tink","tiktak","tiara","tatang","tania",
-             "yanty","yoky","yohana","yii","vero",
+             "yanty","yoky","yohana","yii","vero","vaulian",
              "wulan","wok","widya"];
+
 var keyword1 = "ROOM"
 var keyword2 = "𝗥𝗢𝗢𝗠"
 var keyword3 = "LOMBA"
