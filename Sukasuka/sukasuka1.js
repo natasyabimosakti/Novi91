8f4ed9902d0b6b6fd14f505b4fd2cd745335b34e
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-09 21:01:56 +0700

    Update sukasuka1.js

--- Sukasuka/sukasuka1.js
+++ Sukasuka/sukasuka1.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      3.112
 // @description  try to take over the world!
-// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sukasuka/sukasuka1.js
+// @version      3.NaN
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sukasuka/sukasuka1.js
 // @author       You
 // @match        http*://*/*
@@ -31,8 +31,8 @@ var Comment4 = 'GAIB4D=ASKIPLI99=08*14*33';
 var namagroup5 = 'KEITOGEL';
 var Comment5 = '#keitogel = (ASABKUBUR12) = 29*74*04'; 
 
-var namagroup6 = 'OMASLOT';
-var Comment6 = '#OMASLOT ( ASIZAN33 ) : 96*83'; 
+var namagroup6 = 'POSO';
+var Comment6 = '#POSOTOTO=ASIZAN33= 96*83'; 
 
 var namagroup7 = '453P VIP';
 var Comment7 = 'ASEPTOGEL ASIZAN33 19*59*03'; 
@@ -80,7 +80,7 @@ var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","adi","aldi
              "cristina","camb","cassa","che","cinta","celsia","calista","cahyo","cipto","claura","chelsea","calista",
              "david",",dewa","desi","debby","dewi","dentoto","dika","delon","dewy",
              "erwin","elvina",
-             "fira","fahresa","findlay",
+             "fira","fahresa","findlay","fatimah",
              "gita","genzo",
              "habib","hefi","hoihai","herfizah","hanny","hanabi","hokage",
              "icha","iyatoto","intan",
@@ -90,9 +90,9 @@ var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","adi","aldi
              "mahendra","monica","mey","mersya","mad rm","multi","mariana","melati","meleqq","megaways","minion","melly","monicha","manu","maryam","mode",
              "nasution","nyocol","naura","neng","nino","nona","neman","novi","nella","nayla","naomi","nica",
              "oscar","ozawa",
-             "pung","puput","priyan","primus","primus","prediction",
-             "ratu","rio","ria","rikodo","rizal","roy","rendy","rahma","ratsa","sinta",
-             "sandiego","sanjaya","siska","safar","sinta","surianti","satria","sapto","salsabila","sanchez","sofia","sonia","serena","specialis","seojun","saskia","sifa",
+             "pung","puput","priyan","primus","primus","prediction","pebri",
+             "ratu","rio","ria","rikodo","rizal","roy","rendy","rahma","ratsa","sinta","rara",
+             "sandiego","sanjaya","siska","safar","sinta","surianti","satria","sapto","salsabila","sanchez","sofia","sonia","serena","specialis","seojun","saskia","sifa","seojun",
              "tink","tiktak","tiara","tatang","tomi",
              "xian",
              "yanty","yoky","yohana","yura","yaya",
