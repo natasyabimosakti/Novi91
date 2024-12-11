1d704d9903c8f8d1ca0d49efa856b22209f43727
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-11 17:58:56 +0700

    Update Kawul4.js

--- Kawul/Kawul4.js
+++ Kawul/Kawul4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kawul4
 // @namespace    http://tampermonkey.net/
-// @version      3.116
+// @version      3.117
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul4.js
@@ -28,8 +28,8 @@ var Comment3 = 'Shiotogel4d (SANGEKABEH) 37*00*70';
 var namagroup4 = 'ZODIAK';
 var Comment4 = '#ZODIAKTOTO (SAKAREPMU) : 73*54*51'; 
 
-var namagroup5 = 'ISTANA';
-var Comment5 = 'JANGKAUTOTO(SAKAUBEN) : 25*03*91'; 
+var namagroup5 = 'PEDRO';
+var Comment5 = '#PEDRO4D (SAKAUBEN*25*03*91)'; 
 
 var namagroup6 = 'DIVA4D';
 var Comment6 = '#DIVA4D (SAKEMBER) = 04*84*96'; 
