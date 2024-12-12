31b5486c729c93f6aef37947b606a53e7167ec6c
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-12 20:10:53 +0700

    Update Manuk1.js

--- Manuk/Manuk1.js
+++ Manuk/Manuk1.js
@@ -33,7 +33,7 @@ var Comment4 = 'GAIB4D=MANUK123=14*57*42';
 var namagroup5 = 'KEITOGEL';
 var Comment5 = '#keitogel = (MANANUK213) = 65*56*48'; 
 
-var namagroup6 = 'POSO';
+var namagroup6 = 'JOGJA';
 var Comment6 = '#POSOTOTO=MANUKGENI12= 47*54'; 
 
 var namagroup7 = '453P VIP';
