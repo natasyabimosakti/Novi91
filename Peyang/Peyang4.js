d8964c827ff3e1db5ca1502a64d421abd6d0dea9
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-16 18:08:50 +0700

    Update Peyang4.js

--- Peyang/Peyang4.js
+++ Peyang/Peyang4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Peyang4
 // @namespace    http://tampermonkey.net/
-// @version      3.114
+// @version      3.115
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang4.js
@@ -25,8 +25,8 @@ var Comment2 = 'K86TOTO ( TOMRAIDER13 ) : 67*40*01';
 var namagroup3 = 'PUSAT LOMBA';
 var Comment3 = 'Shiotogel4d (TOMBOLPENTIL) 32*37*23'; 
 
-var namagroup4 = 'ZODIAK';
-var Comment4 = '#ZODIAKTOTO (TOMBOLNUKLIR) : 79*21*03'; 
+var namagroup4 = 'RIATOTO';
+var Comment4 = '#RIATOTO TOMBOLNUKLIR = 79*21*03'; 
 
 var namagroup5 = 'PEDRO';
 var Comment5 = '#PEDRO4D (TOMBRENG34*52*93*35)'; 
