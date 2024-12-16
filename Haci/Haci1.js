de93afd5028becb74d4f3d59d8d596779bdc9748
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-16 18:04:28 +0700

    Update Haci1.js

--- Haci/Haci1.js
+++ Haci/Haci1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hachi1
 // @namespace    http://tampermonkey.net/
-// @version      3.129
+// @version      3.130
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci1.js
@@ -26,8 +26,8 @@ var Comment2 = 'K86TOTO ( GUNGHOK4 ) : 18*28*38';
 var namagroup3 = 'PUSAT LOMBA';
 var Comment3 = 'Shiotogel4d (GUNGCUAKZ) 13*36*58'; 
 
-var namagroup4 = 'ZODIAK';
-var Comment4 = '#ZODIAKTOTO (GUNGKALIGUNG) : 42*76*16'; 
+var namagroup4 = 'RIATOTO';
+var Comment4 = '#RIATOTO GUNGKALIGUNG = 42*76*16'; 
 
 var namagroup5 = 'PEDRO';
 var Comment5 = '#PEDRO4D (GUNGBUTZ*71*48*25)'; 
