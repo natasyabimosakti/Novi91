6e3bce9460c5fd37be2c6a65616bfcd0d7e31aeb
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-19 00:05:00 +0700

    Update Bahagia4.js

--- Bahagia/Bahagia4.js
+++ Bahagia/Bahagia4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bahagia4
 // @namespace    http://tampermonkey.net/
-// @version      3.94
+// @version      3.95
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia4.js
@@ -73,7 +73,7 @@ var Comment18 = 'asek';
 
 
 
-var refresh = 50;
+var refresh = 30;
 
 var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","aditia","aldi","ananda","alde","adm","ayesha","aqisya","arga","arifin","aru","agung","alenta","andi","arsyah","mrdepo","acha","annisa","amelia","anisa","fania","ban nee","putri","anisa",
              "boleng","biru","bobby","bastian","boboho","bola","bunga","bonbin",
@@ -444,7 +444,7 @@ function gameClosure() {
     var currentGame;
     return {
         start() {
-            currentGame = setInterval(game, 10)
+            currentGame = setInterval(game, 1)
         },
         stop() {
             clearInterval(currentGame)
