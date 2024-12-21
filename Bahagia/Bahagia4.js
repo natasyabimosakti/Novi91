f79f6a9dfd5cbedb0de32edbdf83833175357d55
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-22 01:20:59 +0700

    Update Bahagia4.js

--- Bahagia/Bahagia4.js
+++ Bahagia/Bahagia4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bahagia4
 // @namespace    http://tampermonkey.net/
-// @version      3.102
+// @version      3.103
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bahagia/Bahagia4.js
@@ -484,7 +484,7 @@ function clicksend() {
         clickEvent.initEvent ("mousedown", true, true);
         clicksendcoment.dispatchEvent (clickEvent);
         console.log("Comment Terkirim");
-        Cutter==1
+        Cutter=1
         location.href = "about:blank"
 
         setTimeout(function(){location.href = "about:blank"},500)
