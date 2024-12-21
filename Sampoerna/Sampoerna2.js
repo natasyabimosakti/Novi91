be7befe22a4f51b2da048cc8f41266590cc7e354
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-22 01:00:05 +0700

    Update Sampoerna2.js

--- Sampoerna/Sampoerna2.js
+++ Sampoerna/Sampoerna2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sampoerna2
 // @namespace    http://tampermonkey.net/
-// @version      3.130
+// @version      3.131
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna2.js
@@ -132,6 +132,9 @@ var Backlist7 = "totomacau";
 var jitter = 0
 var myrefresh = setInterval(function(){
     window.scroll(0,100)
+    if(jitter == 1){
+            return;
+    }
 
     for (let ntv = 0; ntv < document.querySelectorAll('[data-tracking-duration-id').length; ntv++) {
         if (document.querySelectorAll('[data-tracking-duration-id')[ntv]){
@@ -500,4 +503,3 @@ function closer() {
 
 
 }
-
