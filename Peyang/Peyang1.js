41773d5deecf1a1fbfefb188e082e1b6b63394ad
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-22 01:18:20 +0700

    Update Peyang1.js

--- Peyang/Peyang1.js
+++ Peyang/Peyang1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Peyang1
 // @namespace    http://tampermonkey.net/
-// @version      3.123
+// @version      3.124
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang1.js
@@ -129,10 +129,10 @@ var Cutter = 0
 var myrefresh = setInterval(function(){
     window.scroll(0,100)
     if(Cutter == 1){
-            return;
-        }
+        location.href = "about:blank"
+    }
     if(jitter == 1){
-            return;
+        return;
     }
 
     for (let ntv = 0; ntv < document.querySelectorAll('[data-tracking-duration-id').length; ntv++) {
@@ -244,9 +244,6 @@ function gameClosure() {
     function game() {
         console.log('The game is Start')
         if(jitter == 1){
-            return;
-        }
-        if(document.getElementsByClassName("prevent-scrolling")[0]){
             return;
         }
         console.log('The game is running')
@@ -472,8 +469,11 @@ function gameClosure() {
 var game = gameClosure()
 
 function clicksend() {
-game.stop()
+    game.stop()
     jitter = 1
+    if(document.getElementsByClassName("prevent-scrolling")[0]){
+        return;
+    }
     /*Tampilkan TOMBOL SEND*/
     if(document.getElementsByClassName("textbox-submit-button")[0] && document.getElementsByClassName("multi-line-floating-textbox")[0].value.length >= 1){
         document.getElementsByClassName("textbox multi-line-floating-textbox")[0].dispatchEvent(
@@ -488,18 +488,20 @@ game.stop()
         clickEvent.initEvent ("mousedown", true, true);
         clicksendcoment.dispatchEvent (clickEvent);
         console.log("Comment Terkirim");
-
-            setTimeout(function(){location.href = "about:blank"},500)
-        closer()
+        Cutter=1
         location.href = "about:blank"
 
-        Cutter=1
+        setTimeout(function(){location.href = "about:blank"},500)
+        closer()
+
+
+
 
         /*Tekan TOMBOL SEND*/
     }
 }
 function closer() {
-   location.href = "about:blank"
+    location.href = "about:blank"
 
 
 }
