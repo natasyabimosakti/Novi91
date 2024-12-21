e67900cf800211f0beef21b8084bada590ac02d9
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-22 01:07:22 +0700

    Update Kawul4.js

--- Kawul/Kawul4.js
+++ Kawul/Kawul4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kawul4
 // @namespace    http://tampermonkey.net/
-// @version      3.127
+// @version      3.128
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul4.js
@@ -123,8 +123,12 @@ var Backlist7 = "totomacau";
 
 
 var jitter = 0
+var Cutter = 0
 var myrefresh = setInterval(function(){
     window.scroll(0,100)
+    if(Cutter == 1){
+            return;
+        }
     if(jitter == 1){
             return;
     }
@@ -485,14 +489,15 @@ game.stop()
 
             setTimeout(function(){location.href = "about:blank"},500)
         closer()
+        location.href = "about:blank"
 
-
+        Cutter=1
 
         /*Tekan TOMBOL SEND*/
     }
 }
 function closer() {
-    setTimeout(function(){location.href = "about:blank"},1000)
+   location.href = "about:blank"
 
 
 }
