1196caa3e825478612cbdec20066c658ee0a53d6
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-22 00:48:35 +0700

    Update Haci3.js

--- Haci/Haci3.js
+++ Haci/Haci3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hachi3
 // @namespace    http://tampermonkey.net/
-// @version      3.137
+// @version      3.138
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Haci/Haci3.js
@@ -467,7 +467,7 @@ function gameClosure() {
 var game = gameClosure()
 
 function clicksend() {
-
+game.stop()
     jitter = 1
     /*Tampilkan TOMBOL SEND*/
     if(document.getElementsByClassName("textbox-submit-button")[0] && document.getElementsByClassName("multi-line-floating-textbox")[0].value.length >= 1){
@@ -483,14 +483,18 @@ function clicksend() {
         clickEvent.initEvent ("mousedown", true, true);
         clicksendcoment.dispatchEvent (clickEvent);
         console.log("Comment Terkirim");
-          closer()
+
+            setTimeout(function(){location.href = "about:blank"},500)
+        closer()
+
+
 
         /*Tekan TOMBOL SEND*/
     }
 }
-
 function closer() {
     setTimeout(function(){location.href = "about:blank"},1000)
 
 
 }
+
