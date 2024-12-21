73e1225f689e38c516323576b024fda7fe0de929
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-22 00:50:12 +0700

    Update Peyang4.js

--- Peyang/Peyang4.js
+++ Peyang/Peyang4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Peyang4
 // @namespace    http://tampermonkey.net/
-// @version      3.121
+// @version      3.122
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang4.js
@@ -466,7 +466,7 @@ function gameClosure() {
 var game = gameClosure()
 
 function clicksend() {
-
+game.stop()
     jitter = 1
     /*Tampilkan TOMBOL SEND*/
     if(document.getElementsByClassName("textbox-submit-button")[0] && document.getElementsByClassName("multi-line-floating-textbox")[0].value.length >= 1){
@@ -482,14 +482,18 @@ function clicksend() {
         clickEvent.initEvent ("mousedown", true, true);
         clicksendcoment.dispatchEvent (clickEvent);
         console.log("Comment Terkirim");
-          closer()
+
+            setTimeout(function(){location.href = "about:blank"},500)
+        closer()
+
+
 
         /*Tekan TOMBOL SEND*/
     }
 }
-
 function closer() {
     setTimeout(function(){location.href = "about:blank"},1000)
 
 
 }
+
