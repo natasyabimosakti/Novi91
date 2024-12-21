1f97a69b51ab1ca750688d2bfe4bbf47ec980515
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-22 01:31:20 +0700

    Update Kawul3.js

--- Kawul/Kawul3.js
+++ Kawul/Kawul3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kawul3
 // @namespace    http://tampermonkey.net/
-// @version      3.130
+// @version      3.131
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Kawul/Kawul3.js
@@ -471,9 +471,7 @@ var game = gameClosure()
 function clicksend() {
     game.stop()
     jitter = 1
-    if(document.getElementsByClassName("prevent-scrolling")[0]){
-        return;
-    }
+     
     /*Tampilkan TOMBOL SEND*/
     if(document.getElementsByClassName("textbox-submit-button")[0] && document.getElementsByClassName("multi-line-floating-textbox")[0].value.length >= 1){
         document.getElementsByClassName("textbox multi-line-floating-textbox")[0].dispatchEvent(
