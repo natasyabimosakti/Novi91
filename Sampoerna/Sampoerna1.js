0210515961c3a02fba7c382eccf3467902ffda2f
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-22 00:50:20 +0700

    Update Sampoerna1.js

--- Sampoerna/Sampoerna1.js
+++ Sampoerna/Sampoerna1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sampoerna1
 // @namespace    http://tampermonkey.net/
-// @version      3.129
+// @version      3.130
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna1.js
@@ -466,9 +466,8 @@ function gameClosure() {
     }
 }
 var game = gameClosure()
-
 function clicksend() {
-
+game.stop()
     jitter = 1
     /*Tampilkan TOMBOL SEND*/
     if(document.getElementsByClassName("textbox-submit-button")[0] && document.getElementsByClassName("multi-line-floating-textbox")[0].value.length >= 1){
@@ -484,14 +483,18 @@ function clicksend() {
         clickEvent.initEvent ("mousedown", true, true);
         clicksendcoment.dispatchEvent (clickEvent);
         console.log("Comment Terkirim");
-          closer()
+
+            setTimeout(function(){location.href = "about:blank"},500)
+        closer()
+
+
 
         /*Tekan TOMBOL SEND*/
     }
 }
-
 function closer() {
     setTimeout(function(){location.href = "about:blank"},1000)
 
 
 }
+
