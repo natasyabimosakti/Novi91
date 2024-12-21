24ec26d3670d2ff425870d464222d0825bd11cb6
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-21 19:51:03 +0700

    Update Sampoerna3.js

--- Sampoerna/Sampoerna3.js
+++ Sampoerna/Sampoerna3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sampoerna3
 // @namespace    http://tampermonkey.net/
-// @version      3.127
+// @version      3.128
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna3.js
@@ -80,7 +80,7 @@ var Comment17 = 'Sampoerna3';
 var namagroup18 = 'lajw';
 var Comment18 = 'asek';
 
-var refresh = 30;
+var refresh = 40;
 
 var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","aditia","aldi","ananda","alde","adm","ayesha","aqisya","arga","arifin","aru","agung","alenta","andi","arsyah","mrdepo","acha","annisa","amelia","anisa","fania","ban nee","putri","anisa",
              "boleng","biru","bobby","bastian","boboho","bola","bunga","bonbin",
@@ -221,7 +221,13 @@ var myrefresh = setInterval(function(){
             if (document.readyState === "complete") {
                 for (var coki = 0; coki < waktupost.length; coki++) {
                     if(waktupost[coki].textContent.includes("Aktivitas")) {
-                        waktupost[coki].click()
+                        if(jitter == 1){
+                            return;
+                        }
+                        if(document.getElementsByClassName("prevent-scrolling")[0]){
+                            waktupost[coki].click()
+                        }
+
                     }
                 }
             }
@@ -236,6 +242,9 @@ function gameClosure() {
     function game() {
         console.log('The game is Start')
         if(jitter == 1){
+            return;
+        }
+        if(document.getElementsByClassName("prevent-scrolling")[0]){
             return;
         }
         console.log('The game is running')
@@ -484,7 +493,7 @@ function clicksend() {
 }
 
 function closer() {
-    setTimeout(function(){location.href = "about:blank"},100)
+    setTimeout(function(){location.href = "about:blank"},200)
 
 
 }
