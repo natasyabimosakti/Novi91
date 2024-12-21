7d132fc3f8a85545e665308943dca93a024cb396
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-22 00:59:55 +0700

    Update Peyang4.js

--- Peyang/Peyang4.js
+++ Peyang/Peyang4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Peyang4
 // @namespace    http://tampermonkey.net/
-// @version      3.122
+// @version      3.123
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang4.js
@@ -128,6 +128,9 @@ var Backlist7 = "totomacau";
 var jitter = 0
 var myrefresh = setInterval(function(){
     window.scroll(0,100)
+    if(jitter == 1){
+            return;
+    }
 
     for (let ntv = 0; ntv < document.querySelectorAll('[data-tracking-duration-id').length; ntv++) {
         if (document.querySelectorAll('[data-tracking-duration-id')[ntv]){
@@ -496,4 +499,3 @@ function closer() {
 
 
 }
-
