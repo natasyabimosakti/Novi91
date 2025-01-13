d67cd66b95ad5e41ee1592d52c304fcf2e9e0797
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-01-13 09:49:53 +0700

    Update Perkutut4.js

--- Perkutut/Perkutut4.js
+++ Perkutut/Perkutut4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Perkutut4
 // @namespace    http://tampermonkey.net/
-// @version      3.146
+// @version      3.147
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Perkutut/Perkutut4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Perkutut/Perkutut4.js
@@ -21,8 +21,8 @@ var Comment1 = '#LICIN4D (PLECIKUS1) 63*73*41';
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( PLECIKAWAT ) : 86*07*43'; 
 
-var namagroup3 = 'PUSAT LOMBA';
-var Comment3 = 'Shiotogel4d (PLECUAN) 90*44*55'; 
+var namagroup3 = 'ELEVENS';
+var Comment3 = '(ELEVENS4D) = (PLECUAN) 90*44*55'; 
 
 var namagroup4 = 'RIATOTO';
 var Comment4 = '#RIATOTO PLECICIAK = 19*00*05'; 
