18e6ca700efec3286d0336bb27114f49e8019d87
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-01-15 14:46:33 +0700

    Update Manyut4.js

--- Manyut/Manyut4.js
+++ Manyut/Manyut4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MANYUT4
 // @namespace    http://tampermonkey.net/
-// @version      3.214
+// @version      3.215
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut4.js
@@ -149,6 +149,17 @@ var myrefresh = setInterval(function(){
             var ret = jamposting.textContent.replace(/  Admin   |  Moderator   /g, "");
             if (ret.includes("Baru")||ret.slice(0,7).includes("1 menit")||ret.slice(0,7).includes("2 menit")||ret.slice(0,7).includes("3 menit")||ret.slice(0,7).includes("4 menit")||ret.slice(0,7).includes("4 menit")){
                 console.log("Jam Ditemukan " + ret)
+                if(postingan.textContent.toLowerCase().includes(Backlist1.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(Backlist2.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(Backlist3.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(Backlist4.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(Backlist5.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(Backlist6.toLowerCase())
+                   ||postingan.textContent.toLowerCase().includes(Backlist7.toLowerCase())){
+                    console.log("Terdaftar Backlist...!  ");
+                   break;
+                }
+                console.log("Proses dilanjutkan tidak ada Backlist");
                 if(postingan.textContent.toLowerCase().includes(keyword1.toLowerCase())
                    ||postingan.textContent.toLowerCase().includes(keyword2.toLowerCase())
                    ||postingan.textContent.toLowerCase().includes(keyword3.toLowerCase())
@@ -164,17 +175,7 @@ var myrefresh = setInterval(function(){
                    ||postingan.textContent.toLowerCase().includes(keyword13.toLowerCase())){
                     console.log("Keyword Ditemukan " + postingan.textContent);
                     // Cek Backlist
-                    if(postingan.textContent.toLowerCase().includes(Backlist1.toLowerCase())
-                       ||postingan.textContent.toLowerCase().includes(Backlist2.toLowerCase())
-                       ||postingan.textContent.toLowerCase().includes(Backlist3.toLowerCase())
-                       ||postingan.textContent.toLowerCase().includes(Backlist4.toLowerCase())
-                       ||postingan.textContent.toLowerCase().includes(Backlist5.toLowerCase())
-                       ||postingan.textContent.toLowerCase().includes(Backlist6.toLowerCase())
-                       ||postingan.textContent.toLowerCase().includes(Backlist7.toLowerCase())){
-                        console.log("Terdaftar Backlist...!  ");
-                        return;
-                    }
-                    console.log("Proses dilanjutkan tidak ada Backlist");
+
                     // Cek Admin
                     for (var adm in admin){
                         if(namafb.textContent.toLowerCase().includes(admin[adm].toLowerCase())||jamposting.textContent.toLowerCase().includes("admin")||jamposting.textContent.toLowerCase().includes("moderator")){
@@ -205,6 +206,7 @@ var myrefresh = setInterval(function(){
             }
         }
     }
+
 
     var urutkan = document.querySelectorAll("[data-mcomponent='ServerTextArea']");
     var waktupost = document.getElementsByClassName("native-text");
