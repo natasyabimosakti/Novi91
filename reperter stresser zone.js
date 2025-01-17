041cb45129b0aa4c718518a98a2d79c9da0b8370
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-01-17 12:07:54 +0700

    Update reperter stresser zone.js

--- reperter stresser zone.js
+++ reperter stresser zone.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
-// @version      3.NaN
+// @version      3.01
 // @description  try to take over the world!
 // @author       You
 // @match        http*://*/*
