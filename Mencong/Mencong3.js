7d6396703f9b12200820fa939f49b0a4e6dfaede
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-01-25 15:39:20 +0700

    Update Mencong3.js

--- Mencong/Mencong3.js
+++ Mencong/Mencong3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MENCONG3
 // @namespace    http://tampermonkey.net/
-// @version      3.224
+// @version      3.225
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Mencong/Mencong3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Mencong/Mencong3.js
@@ -224,7 +224,7 @@ var myrefresh = setInterval(function(){
                             }else{
                                 console.log("Admin yang Memosting = " + admin[adm]);
                             }
-                            break;
+                            
                             // Click Comment Box
                             for (var clk = 0; clk < commentbox.length; clk++) {
                                 if(commentbox[clk]){
