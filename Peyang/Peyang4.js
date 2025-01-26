196708c3d8a70be6286765923df452276dc24e88
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-01-26 14:54:20 +0700

    Update Peyang4.js

--- Peyang/Peyang4.js
+++ Peyang/Peyang4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Peyang4
 // @namespace    http://tampermonkey.net/
-// @version      3.137
+// @version      3.138
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang4.js
@@ -16,14 +16,14 @@
 
 
 
-var namagroup1 = 'LICIN';
-var Comment1 = '#LICIN4D (TOMASKU34) 86*66*46'; 
+var namagroup1 = 'GEMERLAP';
+var Comment1 = 'GEMERLAP77 ( TOMASKU34 ) 86*66*46'; 
 
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( TOMRAIDER13 ) : 26*54*34'; 
 
-var namagroup3 = 'ELEVENS';
-var Comment3 = '(ELEVENS4D) = (TOMBOLPENTIL) 09*97*29'; 
+var namagroup3 = 'LAST';
+var Comment3 = '(TOMBOLPENTIL) = 09*97*29 LAST4D'; 
 
 var namagroup4 = 'RIATOTO';
 var Comment4 = '#RIATOTO TOMBOLNUKLIR = 07*55*80'; 
@@ -34,8 +34,8 @@ var Comment5 = '#PEDRO4D (TOMBRENG34*10*63*68)';
 var namagroup6 = 'DIVA4D';
 var Comment6 = '#DIVA4D (TOMBOY23) = 49*23*93'; 
 
-var namagroup7 = 'BENIH';
-var Comment7 = '#BENIHTOTO(TOMBRENG34) : 99*47*02'; 
+var namagroup7 = 'Grup Lomba Tebak';
+var Comment7 = 'ULTRATOGEL88 = (TOMBRENG34) 99*47*02'; 
 
 var namagroup8 = '𝐀𝐋𝐋𝐏𝐀𝐒';
 var Comment8 = 'Tok99Toto ( TOMBIBI24 ) : 07*44*80'; 
@@ -63,6 +63,7 @@ var Comment15 = 'TOMPAKAN23 : 06*90*83 #HOHOTOGEL';
 
 var namagroup16 = 'KECERDASAN';
 var Comment16 = 'CITA4D*TOMBOLPENTIL*05*98*11'; 
+
 
 
 
@@ -106,7 +107,7 @@ var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","aditia","a
              "david","dewa","desi","debby","dewi","dentoto","dika","dealova","diva","damara",
              "erwin","emilia","evelyn",
              "fira","fahresa","fiana","fahmi",
-             "gita",
+             "gita","kang bona","hoky","julianti","libra","claudio",
              "habib","hefi","hoihai","hana","hoki","hokage","esse","nurul","leader",
              "icha","iyatoto","invest","ivanna","inisial","ishaura",
              "jordi","jaguar","jne","jovanka","jessica","je pe","jess","jenifer",
