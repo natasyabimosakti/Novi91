7636d453cab1c5029d3a8e41941a34d26b4ffd78
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-02-16 19:46:34 +0700

    Update Mencong3.js

--- Mencong/Mencong3.js
+++ Mencong/Mencong3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MENCONG3
 // @namespace    http://tampermonkey.net/
-// @version      3.226
+// @version      3.227
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Mencong/Mencong3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Mencong/Mencong3.js
@@ -32,8 +32,8 @@ var Comment4 = 'GAIB4D=NOVITAS3=22*24*21';
 var namagroup5 = 'KEITOGEL';
 var Comment5 = '#keitogel = (NOVIATOS12) = 34*13*79'; 
 
-var namagroup6 = 'JOGJA';
-var Comment6 = '#JOGJATOTO (NOVIRAMBU3)= 00-28'; 
+var namagroup6 = 'Tukang';
+var Comment6 = '( TUKANGTOTO NOVIRAMBU3 : 00*28 )'; 
 
 var namagroup7 = '453P VIP';
 var Comment7 = 'ASEPTOGEL NOVITARU34 04*76*81'; 
@@ -56,8 +56,8 @@ var Comment12 = 'TOGELKUY NOVIKAR23 91*93*23';
 var namagroup13 = 'KOI';
 var Comment13 = '#KOITOTO ( NOVIAN25 ) 74*82'; 
 
-var namagroup14 = 'GROUP PREDIKSI';
-var Comment14 = '# ( NOVIPAL34 ) : 39*27*32'; 
+var namagroup14 = 'GAMELAN';
+var Comment14 = '#GENDANG4D (NOVIPAL34) : 39*27*32'; 
 
 var namagroup15 = 'VESPA';
 var Comment15 = 'VESPATOGEL (NOVIPAL34) 25*16*85'; 
@@ -97,30 +97,29 @@ var id18 = await GM.getValue(18);
 
 var refresh = 40;
 
-var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","adi","aldi","ananda","alde","adm","ayesha","aqisya","anjani","apri","amore","arifin","ayunda","agung","arem","arifa",
-             "boleng","biru","bobby","bastian","bambang","bogard","bannet",
-             "cristina","camb","cassa","che","cinta","celsia","calista","cahyo","cipto","claura","chelsea","calista",
+var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","adi","aldi","ananda","alde","adm","ayesha","aqisya","anjani","apri","amore","arifin","ayunda","agung","arem","arifa","azahra",
+             "boleng","biru","bobby","bastian","bambang","bogard","bannet","botack","bang",
+             "cristina","camb","cassa","che","cinta","celsia","calista","cahyo","cipto","claura","chelsea","calista","chin",
              "david","dewa","desi","debby","dewi","dentoto","dika","delon","dewy","damara",
-             "erwin","elvina","evelyn",
+             "erwin","elvina","evelyn","enzo",
              "fira","fahresa","findlay","fatimah",
-             "gita","genzo",
+             "gita","genzo","gambrong",
              "habib","hefi","hoihai","herfizah","hanny","hanabi","hokage","hoi hai",
              "icha","iyatoto","intan",
              "jordi","jaguar","jne","je pe","jess","junior","jovanka","jasmine",
              "kei","kumbara","kembar","kotna","karina","kopi","kang",
-             "lianda","lusiana","lina","laura","lia","lollo",
+             "lianda","lusiana","lina","laura","lia","lollo","lupin",
              "mahendra","monica","mey","mersya","mad rm","multi","mariana","melati","meleqq","megaways","minion","melly","monicha","manu","maryam","mode",
              "nasution","nyocol","naura","neng","nino","nona","neman","novi","nella","nayla","naomi","nica",
              "oscar","ozawa",
-             "pung","puput","priyan","primus","primus","prediction","pebri",
+             "pung","puput","priyan","primus","primus","prediction","pebri","pasil",
              "ratu","rio","ria","rikodo","rizal","roy","rendy","rahma","ratsa","sinta","rara","ratna","mamad",
-             "sandiego","sanjaya","siska","safar","sinta","surianti","satria","sapto","salsabila","sanchez","sofia","sonia","serena","specialis","seojun","saskia","sifa","seojun",
+             "sandiego","sanjaya","siska","safar","sinta","surianti","satria","sapto","salsabila","sanchez","sofia","sonia","serena","specialis","seojun","saskia","sifa","seojun","sudewo",
              "tink","tiktak","tiara","tatang","tomi",
              "xian",
              "yanty","yoky","yohana","yura","yaya",
              "wulan","wok","wak",
              "zuko"];
-
 
 var keyword1 = "ROOM"
 var keyword2 = "𝗥𝗢𝗢𝗠"
