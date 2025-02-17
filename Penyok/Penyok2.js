2ae9e9be291536290e1da59f5bacb43f6c3e0496
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-02-17 18:31:39 +0700

    Update Penyok2.js

--- Penyok/Penyok2.js
+++ Penyok/Penyok2.js
@@ -15,58 +15,58 @@
 // ==/UserScript==
 
 var namagroup1 = 'SHIOKELINCI';
-var Comment1 = '#shiokelinci4d*SIKIL47*17*95'; 
+var Comment1 = '#shiokelinci4d*NYOLOGMOTO*94*43'; 
 
 var namagroup2 = 'BUKU';
-var Comment2 = 'IYATOTO SELOKAN48 55*90*06'; 
+var Comment2 = 'IYATOTO NYOGOKSILIT 40*92*55'; 
 
 var namagroup3 = 'TIKTAK';
-var Comment3 = 'Tiktaktogel / SENDANG49 / 51 , 35 , 01'; 
+var Comment3 = 'Tiktaktogel / NYOPETTAS / 11 , 45 , 66'; 
 
 var namagroup4 = 'GAIB';
-var Comment4 = 'GAIB4D=SUKET50=36*80*62'; 
+var Comment4 = 'GAIB4D=NYOMOTEMAS=05*48*07'; 
 
 var namagroup5 = 'KEITOGEL';
-var Comment5 = '#keitogel = (SEMPRUL28) = 37*15*50'; 
+var Comment5 = '#keitogel = (NYOTEKTAIMU) = 70*54*57'; 
 
 var namagroup6 = 'Tukang';
-var Comment6 = '( TUKANGTOTO SENDER29 : 14*57 )'; 
+var Comment6 = '( TUKANGTOTO NYOLONGDUET : 56*58 )'; 
 
 var namagroup7 = '453P VIP';
-var Comment7 = 'ASEPTOGEL SEMBUR68 65*56*48'; 
+var Comment7 = 'ASEPTOGEL NYOSINYO898 18*52*36'; 
 
 var namagroup8 = 'MENARA';
-var Comment8 = '#MENARA4D=SEMBAB69= 47*54'; 
+var Comment8 = '#MENARA4D=NYOCOTTERUS= 00*20'; 
 
 var namagroup9 = 'GIL4';
-var Comment9 = 'GILA4D=SUNGKAN79=33*92*38'; 
+var Comment9 = 'GILA4D=NYOMANGEDE1=08*14*33'; 
 
 var namagroup10 = 'JNE';
-var Comment10 = '#JNETOTO(SENENG80)*11*20*38'; 
+var Comment10 = '#JNETOTO(NYODOKBRINJI)*17*84*41'; 
 
 var namagroup11 = 'TOYIB';
-var Comment11 = '#TOYIBSLOT ( SANGGAR81 ) : 58*88*41'; 
+var Comment11 = '#TOYIBSLOT ( NYODOPANGAN99 ) : 88*75*41'; 
 
 var namagroup12 = 'MASTER KUY';
-var Comment12 = 'TOGELKUY SARUNG82 02*09*60'; 
+var Comment12 = 'TOGELKUY NYONYAKU69 01*44*31'; 
 
 var namagroup13 = 'KOI';
-var Comment13 = '#KOITOTO ( SEMPAKK83 ) 67*98'; 
+var Comment13 = '#KOITOTO ( NYOBAKTUKU55 ) 25*85'; 
 
 var namagroup14 = 'GAMELAN';
-var Comment14 = '#GENDANG4D (SINTING84) : 78*46*77'; 
+var Comment14 = '#GENDANG4D (NYOBORKODOK) : 19*59*03'; 
 
 var namagroup15 = 'VESPA';
-var Comment15 = 'VESPATOGEL (SEMBILU85) 71*96*75'; 
+var Comment15 = 'VESPATOGEL (NYOKONGRONDO) 96*83*68'; 
 
 var namagroup16 = 'Nemo';
-var Comment16 = 'NEMO4D (SEKIAN86) : 12*86*68'; 
+var Comment16 = 'NEMO4D (NYOHNYAHNYOH) : 29*74*04'; 
 
 var namagroup17 = 'KIKO';
-var Comment17 = '#KIKOTOTO (SERDADU87) = 66*83';
+var Comment17 = '#KIKOTOTO (NYOPANGBABI) = 65*50';
 
 var namagroup18 = 'Jawatengah';
-var Comment18 = 'group Bejo 1';
+var Comment18 = 'group Penyok 2';
 
 
 
