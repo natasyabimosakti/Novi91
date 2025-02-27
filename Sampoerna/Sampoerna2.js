3e94d4dd1e6bbc38811a8d99a81c2ef71dc4272e
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-02-27 15:26:34 +0700

    Update Sampoerna2.js

--- Sampoerna/Sampoerna2.js
+++ Sampoerna/Sampoerna2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sampoerna2
 // @namespace    http://tampermonkey.net/
-// @version      3.149
+// @version      3.150
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna2.js
@@ -43,7 +43,7 @@ var Comment7 = 'ULTRATOGEL88 = (BOSMAN67) 95*83*23';
 var namagroup8 = '𝐀𝐋𝐋𝐏𝐀𝐒';
 var Comment8 = 'Tok99Toto ( BOSTER73 ) : 93*43*58'; 
 
-var namagroup9 = 'Wellz';
+var namagroup9 = 'TAFSIR MIMPI';
 var Comment9 = 'SIJITOGEL BOSMAN67 32*67*73'; 
 
 var namagroup10 = 'LING';
