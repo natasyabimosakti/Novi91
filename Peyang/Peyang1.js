e737807724e19093410b65d2a357a014c68d1b70
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-02-27 15:24:37 +0700

    Update Peyang1.js

--- Peyang/Peyang1.js
+++ Peyang/Peyang1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Peyang1
 // @namespace    http://tampermonkey.net/
-// @version      3.141
+// @version      3.142
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Peyang/Peyang1.js
@@ -39,7 +39,7 @@ var Comment7 = 'ULTRATOGEL88 = (MASAK741) 72*45*27';
 var namagroup8 = '𝐀𝐋𝐋𝐏𝐀𝐒';
 var Comment8 = 'Tok99Toto ( MACRONI52 ) : 42*76*00'; 
 
-var namagroup9 = 'Wellz';
+var namagroup9 = 'TAFSIR MIMPI';
 var Comment9 = 'SIJITOGEL MEROJAYA51 82*22*20'; 
 
 var namagroup10 = 'LING';
