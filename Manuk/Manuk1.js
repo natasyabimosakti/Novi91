d29a60a111a4df38552422431d412623dc072dd3
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-02-28 11:56:46 +0700

    Update Manuk1.js

--- Manuk/Manuk1.js
+++ Manuk/Manuk1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MANUK 1
 // @namespace    http://tampermonkey.net/
-// @version      3.135
+// @version      3.136
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk1.js
@@ -143,7 +143,7 @@ var Backlist3 = "hasil";
 var Backlist4 = "room lomba freebet";
 var Backlist5 = "prediksi";
 var Backlist6 = "result";
-var Backlist7 = "commen";
+var Backlist7 = "juara lomba";
 
 
 var jitter = 0
