3b785dfc3d6cc6ffbb0d1e1d64dbb843512ddafc
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-02-28 11:52:14 +0700

    Update Bejo1.js

--- Bejo/Bejo1.js
+++ Bejo/Bejo1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Bejo 1
 // @namespace    http://tampermonkey.net/
-// @version      3.10
+// @version      3.11
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bejo/Bejo1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bejo/Bejo1.js
@@ -140,7 +140,7 @@ var Backlist3 = "hasil";
 var Backlist4 = "room lomba freebet";
 var Backlist5 = "prediksi";
 var Backlist6 = "result";
-var Backlist7 = "commen";
+var Backlist7 = "juara lomba";
 
 
 var jitter = 0
