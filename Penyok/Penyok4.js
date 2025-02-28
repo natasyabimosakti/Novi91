37d7dc01bc47739d3b73eca7f35d21cb4550edf9
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-02-28 12:00:24 +0700

    Update Penyok4.js

--- Penyok/Penyok4.js
+++ Penyok/Penyok4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Penyok 4
 // @namespace    http://tampermonkey.net/
-// @version      3.2
+// @version      3.3
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Penyok/Penyok4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Penyok/Penyok4.js
@@ -140,7 +140,7 @@ var Backlist3 = "hasil";
 var Backlist4 = "room lomba freebet";
 var Backlist5 = "prediksi";
 var Backlist6 = "result";
-var Backlist7 = "commen";
+var Backlist7 = "juara lomba";
 
 
 var jitter = 0
