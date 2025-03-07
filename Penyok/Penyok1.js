4dbeefed22f60707f56c7eca8a1f3fe758674c83
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-03-07 16:49:35 +0700

    Update Penyok1.js

--- Penyok/Penyok1.js
+++ Penyok/Penyok1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Penyok 1
 // @namespace    http://tampermonkey.net/
-// @version      3.5
+// @version      3.6
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Penyok/Penyok1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Penyok/Penyok1.js
@@ -93,7 +93,7 @@ var id16 = await GM.getValue(16);
 var id17 = await GM.getValue(17);
 var id18 = await GM.getValue(18);
 
-var refresh = 30;
+var refresh = 40;
 
 var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","adi","aldi","ananda","alde","adm","ayesha","aqisya","anjani","apri","amore","arifin","ayunda","agung","arem","arifa","azahra",
              "boleng","biru","bobby","bastian","bambang","bogard","bannet","botack","bang","aru sundawa","agus tiar","imam",
@@ -146,6 +146,15 @@ var Backlist7 = "juara lomba";
 var jitter = 0
 var Cutter = 0
 var myrefresh = setInterval(function(){
+     if (tm == "" || tm == undefined || tm == null) {
+        GM.setValue("time", hour);
+    }
+    if ( hour > tm + 2 || hour < tm||document.URL.includes("google") == true||hour == undefined||hour == null){
+        for (var kr = 1; kr < 19; kr++) {
+            GM.setValue( kr,0);
+        }
+        GM.setValue("time", hour);
+    }
     window.scroll(0,1000)
 
     for (let ntv = 0; ntv < document.querySelectorAll('[data-tracking-duration-id').length; ntv++) {
@@ -288,7 +297,8 @@ function gameClosure() {
                 if (ceknamagroup.includes(namagroup1) == true||ceknamagroup1.includes(namagroup1) == true||ceknamagroup2.includes(namagroup1) == true||ceknamagroup3.includes(namagroup1) == true||ceknamagroup4.includes(namagroup1) == true) {
                     if (id1 == "1") {
                         jitter = 1
-                        location.href = "about:blank"
+                        setTimeout(function(){location.href = "about:blank"},500)
+                        return;
                     }
                     /*cek nama group dan tulis commntar*/
                     id1 = 1
@@ -302,7 +312,8 @@ function gameClosure() {
                 if (ceknamagroup.includes(namagroup2) == true||ceknamagroup1.includes(namagroup2) == true||ceknamagroup2.includes(namagroup2) == true||ceknamagroup3.includes(namagroup2) == true||ceknamagroup4.includes(namagroup2) == true) {
                     if (id2 == "1") {
                         jitter = 1
-                        location.href = "about:blank"
+                        setTimeout(function(){location.href = "about:blank"},500)
+                        return;
                     }
                     /*cek nama group dan tulis commntar*/
                     id2 = 1
@@ -317,7 +328,8 @@ function gameClosure() {
                 if (ceknamagroup.includes(namagroup3) == true||ceknamagroup1.includes(namagroup3) == true||ceknamagroup2.includes(namagroup3) == true||ceknamagroup3.includes(namagroup3) == true||ceknamagroup4.includes(namagroup3) == true) {
                     if (id3 == "1") {
                         jitter = 1
-                        location.href = "about:blank"
+                        setTimeout(function(){location.href = "about:blank"},500)
+                        return;
                     }
                     /*cek nama group dan tulis commntar*/
                     id3 = 1
@@ -333,7 +345,8 @@ function gameClosure() {
                 if (ceknamagroup.includes(namagroup4) == true||ceknamagroup1.includes(namagroup4) == true||ceknamagroup2.includes(namagroup4) == true||ceknamagroup3.includes(namagroup4) == true||ceknamagroup4.includes(namagroup4) == true) {
                     if (id4 == "1") {
                         jitter = 1
-                        location.href = "about:blank"
+                        setTimeout(function(){location.href = "about:blank"},500)
+                        return;
                     }
                     /*cek nama group dan tulis commntar*/
                     /*cek nama group dan tulis commntar*/
@@ -348,7 +361,8 @@ function gameClosure() {
                 if (ceknamagroup.includes(namagroup5) == true||ceknamagroup1.includes(namagroup5) == true||ceknamagroup2.includes(namagroup5) == true||ceknamagroup3.includes(namagroup5) == true||ceknamagroup4.includes(namagroup5) == true) {
                     if (id5 == "1") {
                         jitter = 1
-                        location.href = "about:blank"
+                        setTimeout(function(){location.href = "about:blank"},500)
+                        return;
                     }
                     /*cek nama group dan tulis commntar*/
                     /*cek nama group dan tulis commntar*/
@@ -363,7 +377,8 @@ function gameClosure() {
                 if (ceknamagroup.includes(namagroup6) == true||ceknamagroup1.includes(namagroup6) == true||ceknamagroup2.includes(namagroup6) == true||ceknamagroup3.includes(namagroup6) == true||ceknamagroup4.includes(namagroup6) == true) {
                     if (id6 == "1") {
                         jitter = 1
-                        location.href = "about:blank"
+                        setTimeout(function(){location.href = "about:blank"},500)
+                        return;
                     }
                     /*cek nama group dan tulis commntar*/
                     /*cek nama group dan tulis commntar*/
@@ -378,7 +393,8 @@ function gameClosure() {
                 if (ceknamagroup.includes(namagroup7) == true||ceknamagroup1.includes(namagroup7) == true||ceknamagroup2.includes(namagroup7) == true||ceknamagroup3.includes(namagroup7) == true||ceknamagroup4.includes(namagroup7) == true) {
                     if (id7 == "1") {
                         jitter = 1
-                        location.href = "about:blank"
+                        setTimeout(function(){location.href = "about:blank"},500)
+                        return;
                     }
                     /*cek nama group dan tulis commntar*/
                     /*cek nama group dan tulis commntar*/
@@ -393,7 +409,8 @@ function gameClosure() {
                 if (ceknamagroup.includes(namagroup8) == true||ceknamagroup1.includes(namagroup8) == true||ceknamagroup2.includes(namagroup8) == true||ceknamagroup3.includes(namagroup8) == true||ceknamagroup4.includes(namagroup8) == true) {
                     if (id8 == "1") {
                         jitter = 1
-                        location.href = "about:blank"
+                        setTimeout(function(){location.href = "about:blank"},500)
+                        return;
                     }
                     /*cek nama group dan tulis commntar*/
                     /*cek nama group dan tulis commntar*/
@@ -408,7 +425,8 @@ function gameClosure() {
                 if (ceknamagroup.includes(namagroup9) == true||ceknamagroup1.includes(namagroup9) == true||ceknamagroup2.includes(namagroup9) == true||ceknamagroup3.includes(namagroup9) == true||ceknamagroup4.includes(namagroup9) == true) {
                     if (id9 == "1") {
                         jitter = 1
-                        location.href = "about:blank"
+                        setTimeout(function(){location.href = "about:blank"},500)
+                        return;
                     }
                     /*cek nama group dan tulis commntar*/
                     /*cek nama group dan tulis commntar*/
@@ -423,7 +441,8 @@ function gameClosure() {
                 if (ceknamagroup.includes(namagroup10) == true||ceknamagroup1.includes(namagroup10) == true||ceknamagroup2.includes(namagroup10) == true||ceknamagroup3.includes(namagroup10) == true||ceknamagroup4.includes(namagroup10) == true) {
                     if (id10 == "1") {
                         jitter = 1
-                        location.href = "about:blank"
+                        setTimeout(function(){location.href = "about:blank"},500)
+                        return;
                     }
                     /*cek nama group dan tulis commntar*/
                     /*cek nama group dan tulis commntar*/
@@ -438,7 +457,8 @@ function gameClosure() {
                 if (ceknamagroup.includes(namagroup11) == true||ceknamagroup1.includes(namagroup11) == true||ceknamagroup2.includes(namagroup11) == true||ceknamagroup3.includes(namagroup11) == true||ceknamagroup4.includes(namagroup11) == true) {
                     if (id11 == "1") {
                         jitter = 1
-                        location.href = "about:blank"
+                        setTimeout(function(){location.href = "about:blank"},500)
+                        return;
                     }
                     /*cek nama group dan tulis commntar*/
                     /*cek nama group dan tulis commntar*/
@@ -453,7 +473,8 @@ function gameClosure() {
                 if (ceknamagroup.includes(namagroup12) == true||ceknamagroup1.includes(namagroup12) == true||ceknamagroup2.includes(namagroup12) == true||ceknamagroup3.includes(namagroup12) == true||ceknamagroup4.includes(namagroup12) == true) {
                     if (id12 == "1") {
                         jitter = 1
-                        location.href = "about:blank"
+                        setTimeout(function(){location.href = "about:blank"},500)
+                        return;
                     }
                     /*cek nama group dan tulis commntar*/
                     /*cek nama group dan tulis commntar*/
@@ -468,7 +489,8 @@ function gameClosure() {
                 if (ceknamagroup.includes(namagroup13) == true||ceknamagroup1.includes(namagroup13) == true||ceknamagroup2.includes(namagroup13) == true||ceknamagroup3.includes(namagroup13) == true||ceknamagroup4.includes(namagroup13) == true) {
                     if (id13 == "1") {
                         jitter = 1
-                        location.href = "about:blank"
+                        setTimeout(function(){location.href = "about:blank"},500)
+                        return;
                     }
                     /*cek nama group dan tulis commntar*/
                     /*cek nama group dan tulis commntar*/
@@ -482,7 +504,8 @@ function gameClosure() {
                 if (ceknamagroup.includes(namagroup14) == true||ceknamagroup1.includes(namagroup14) == true||ceknamagroup2.includes(namagroup14) == true||ceknamagroup3.includes(namagroup14) == true||ceknamagroup4.includes(namagroup14) == true) {
                     if (id14 == "1") {
                         jitter = 1
-                        location.href = "about:blank"
+                        setTimeout(function(){location.href = "about:blank"},500)
+                        return;
                     }
                     /*cek nama group dan tulis commntar*/
                     /*cek nama group dan tulis commntar*/
@@ -498,7 +521,8 @@ function gameClosure() {
                 if (ceknamagroup.includes(namagroup15) == true||ceknamagroup1.includes(namagroup15) == true||ceknamagroup2.includes(namagroup15) == true||ceknamagroup3.includes(namagroup15) == true||ceknamagroup4.includes(namagroup15) == true) {
                     if (id15 == "1") {
                         jitter = 1
-                        location.href = "about:blank"
+                        setTimeout(function(){location.href = "about:blank"},500)
+                        return;
                     }
                     /*cek nama group dan tulis commntar*/
                     /*cek nama group dan tulis commntar*/
@@ -514,7 +538,8 @@ function gameClosure() {
                 if (ceknamagroup.includes(namagroup16) == true||ceknamagroup1.includes(namagroup16) == true||ceknamagroup2.includes(namagroup16) == true||ceknamagroup3.includes(namagroup16) == true||ceknamagroup4.includes(namagroup16) == true) {
                     if (id16 == "1") {
                         jitter = 1
-                        location.href = "about:blank"
+                        setTimeout(function(){location.href = "about:blank"},500)
+                        return;
                     }
                     /*cek nama group dan tulis commntar*/
                     /*cek nama group dan tulis commntar*/
@@ -529,7 +554,8 @@ function gameClosure() {
                 if (ceknamagroup.includes(namagroup17) == true||ceknamagroup1.includes(namagroup17) == true||ceknamagroup2.includes(namagroup17) == true||ceknamagroup3.includes(namagroup17) == true||ceknamagroup4.includes(namagroup17) == true) {
                     if (id17 == "1") {
                         jitter = 1
-                        location.href = "about:blank"
+                        setTimeout(function(){location.href = "about:blank"},500)
+                        return;
                     }
                     /*cek nama group dan tulis commntar*/
                     /*cek nama group dan tulis commntar*/
@@ -544,7 +570,8 @@ function gameClosure() {
                 if (ceknamagroup.includes(namagroup18) == true||ceknamagroup1.includes(namagroup18) == true||ceknamagroup2.includes(namagroup18) == true||ceknamagroup3.includes(namagroup18) == true||ceknamagroup4.includes(namagroup18) == true) {
                     if (id18 == "1") {
                         jitter = 1
-                        location.href = "about:blank"
+                        setTimeout(function(){location.href = "about:blank"},500)
+                        return;
                     }
                     /*cek nama group dan tulis commntar*/
                     /*cek nama group dan tulis commntar*/
