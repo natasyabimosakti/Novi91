8dd34ad0c535a6e379b71b831a0d804c77f01007
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-03-07 17:22:21 +0700

    Update Slamet2.js

--- Slamet/Slamet2.js
+++ Slamet/Slamet2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Slamet 2
 // @namespace    http://tampermonkey.net/
-// @version      3.6
+// @version      3.7
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Slamet/Slamet2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Slamet/Slamet2.js
@@ -199,7 +199,7 @@ var myrefresh = setInterval(function(){
 
                 // Cek Backlist
                 for (var adm in admin){
-                    if(namafb.textContent.toLowerCase().includes(admin[adm].toLowerCase())||jamposting1.textContent.toLowerCase().includes("admin")||jamposting1.textContent.toLowerCase().includes("moderator")||jamposting2.textContent.toLowerCase().includes("admin")||jamposting2.textContent.toLowerCase().includes("moderator")){
+                    if(namafb.textContent.toLowerCase().includes(admin[adm].toLowerCase())||jamposting1.toLowerCase().includes("admin")||jamposting1.toLowerCase().includes("moderator")||jamposting2.toLowerCase().includes("admin")||jamposting2.toLowerCase().includes("moderator")){
                         // Tampilkan Siapa Yang Memposting
                         console.log("Admin yang Memosting Ditemukan= " + admin[adm]);
                         // Click Comment Box
