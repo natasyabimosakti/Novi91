db9b5cc73808303889c1253fb7608f0f640235ca
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-03-14 00:33:52 +0700

    Update Manyut3.js

--- Manyut/Manyut3.js
+++ Manyut/Manyut3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MANYUT3
 // @namespace    http://tampermonkey.net/
-// @version      3.232
+// @version      3.233
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut3.js
@@ -157,7 +157,9 @@ var myrefresh = setInterval(function(){
         }
         GM.setValue("time", hour);
     }
-    window.scroll(0,100)
+    if(document.location.href.includes("group")){
+    window.scroll(0,200)
+    }
     if(Cutter == 1){
         location.href = "about:blank"
     }
