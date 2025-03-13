287384c6cc0a1f6467b22f3a6b6e074ad6acf0a9
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-03-13 17:50:57 +0700

    Update Bosku3.js

--- Bosku/Bosku3.js
+++ Bosku/Bosku3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BOSQUE3
 // @namespace    http://tampermonkey.net/
-// @version      3.152
+// @version      3.153
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku3.js
@@ -596,7 +596,7 @@ function gameClosure() {
     var currentGame;
     return {
         start() {
-            currentGame = setInterval(game, 50)
+            currentGame = setInterval(game, 20)
         },
         stop() {
             clearInterval(currentGame)
