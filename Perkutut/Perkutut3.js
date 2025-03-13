0e39f154f4bdbe5727c9fe878d2923d14283adea
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-03-14 00:37:16 +0700

    Update Perkutut3.js

--- Perkutut/Perkutut3.js
+++ Perkutut/Perkutut3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Perkutut3
 // @namespace    http://tampermonkey.net/
-// @version      3.160
+// @version      3.161
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Perkutut/Perkutut3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Perkutut/Perkutut3.js
@@ -159,7 +159,9 @@ var myrefresh = setInterval(function(){
         }
         GM.setValue("time", hour);
     }
-    window.scroll(0,100)
+    if(document.location.href.includes("group")){
+    window.scroll(0,200)
+    }
     if(Cutter == 1){
         location.href = "about:blank"
     }
