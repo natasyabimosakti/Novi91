8521e4706a72bb7cf056172273f57577b99495dd
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-03-13 17:54:30 +0700

    Update Manyut3.js

--- Manyut/Manyut3.js
+++ Manyut/Manyut3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MANYUT3
 // @namespace    http://tampermonkey.net/
-// @version      3.231
+// @version      3.232
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut3.js
@@ -596,7 +596,7 @@ function gameClosure() {
     var currentGame;
     return {
         start() {
-            currentGame = setInterval(game, 50)
+            currentGame = setInterval(game, 20)
         },
         stop() {
             clearInterval(currentGame)
