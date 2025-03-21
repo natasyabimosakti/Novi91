662d10e34e179dfe89516916cf4745f9b34b62a5
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-03-21 16:42:59 +0700

    Update Slamet2.js

--- Slamet/Slamet2.js
+++ Slamet/Slamet2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Slamet 2
 // @namespace    http://tampermonkey.net/
-// @version      3.12
+// @version      3.13
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Slamet/Slamet2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Slamet/Slamet2.js
@@ -53,8 +53,8 @@ var Comment12 = 'TOGELKUY BNGSAT55 17*84*50';
 var namagroup13 = 'KOI';
 var Comment13 = '#KOITOTO ( BAJINDUL2 ) 88*75'; 
 
-var namagroup14 = 'GAMELAN';
-var Comment14 = '#GENDANG4D (BLUR005) : 19*59*41'; 
+var namagroup14 = 'NAM4D';
+var Comment14 = 'NAM4D ( BLUR005 ) = 19*59*41'; 
 
 var namagroup15 = 'VESPA';
 var Comment15 = 'VESPATOGEL (BUKITKEMBAR) 25*85*67'; 
@@ -98,7 +98,7 @@ var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","adi","aldi
              "boleng","biru","bobby","bastian","bambang","bogard","bannet","botack","bang","aru sundawa","agus tiar","imam",
              "cristina","camb","cassa","che","cinta","celsia","calista","cahyo","cipto","claura","chelsea","calista","chin",
              "david","dewa","desi","debby","dewi","dentoto","dika","delon","dewy","damara",
-             "erwin","elvina","evelyn","enzo",
+             "erwin","elvina","evelyn","enzo","oppe","komandan","melinda","ranger",
              "fira","fahresa","findlay","fatimah",
              "gita","genzo","gambrong",
              "habib","hefi","hoihai","herfizah","hanny","hanabi","hokage","hoi hai",
