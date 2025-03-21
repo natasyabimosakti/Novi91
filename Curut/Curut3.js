950f5ba691c36997ae22b3c26337fa0bf0f539ca
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-03-21 16:31:31 +0700

    Update Curut3.js

--- Curut/Curut3.js
+++ Curut/Curut3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW CURUT3
 // @namespace    http://tampermonkey.net/
-// @version      3.153
+// @version      3.154
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut3.js
@@ -56,8 +56,8 @@ var Comment12 = 'TOGELKUY CIRUN19 73*10*97';
 var namagroup13 = 'KOI';
 var Comment13 = '#KOITOTO ( CUMNI21 ) 52*26'; 
 
-var namagroup14 = 'GAMELAN';
-var Comment14 = '#GENDANG4D (CIMAR19) : 04*76*81'; 
+var namagroup14 = 'NAM4D';
+var Comment14 = 'NAM4D ( CIMAR19 ) = 04*76*81'; 
 
 var namagroup15 = 'VESPA';
 var Comment15 = 'VESPATOGEL (CAHMBUH) 00*28*07'; 
@@ -101,7 +101,7 @@ var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","adi","aldi
              "boleng","biru","bobby","bastian","bambang","bogard","bannet","botack","bang","aru sundawa","agus tiar","imam",
              "cristina","camb","cassa","che","cinta","celsia","calista","cahyo","cipto","claura","chelsea","calista","chin",
              "david","dewa","desi","debby","dewi","dentoto","dika","delon","dewy","damara",
-             "erwin","elvina","evelyn","enzo",
+             "erwin","elvina","evelyn","enzo","oppe","komandan","melinda","ranger",
              "fira","fahresa","findlay","fatimah",
              "gita","genzo","gambrong",
              "habib","hefi","hoihai","herfizah","hanny","hanabi","hokage","hoi hai",
