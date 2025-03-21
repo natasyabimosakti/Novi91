eb490fa848771cf5693745782aead1316725c103
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-03-21 16:33:37 +0700

    Update Manuk2.js

--- Manuk/Manuk2.js
+++ Manuk/Manuk2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MANUK 2
 // @namespace    http://tampermonkey.net/
-// @version      3.145
+// @version      3.146
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk2.js
@@ -56,8 +56,8 @@ var Comment12 = 'TOGELKUY ELANGX3 65*56*48';
 var namagroup13 = 'KOI';
 var Comment13 = '#KOITOTO ( ELANGSH13 ) 14*57'; 
 
-var namagroup14 = 'GAMELAN';
-var Comment14 = '#GENDANG4D (ELANG4739) : 37*15*99'; 
+var namagroup14 = 'NAM4D';
+var Comment14 = 'NAM4D ( ELANG4739 ) = 37*15*99'; 
 
 var namagroup15 = 'VESPA';
 var Comment15 = 'VESPATOGEL (ELANG72) 36*80*62'; 
@@ -101,7 +101,7 @@ var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","adi","aldi
              "boleng","biru","bobby","bastian","bambang","bogard","bannet","botack","bang","aru sundawa","agus tiar","imam",
              "cristina","camb","cassa","che","cinta","celsia","calista","cahyo","cipto","claura","chelsea","calista","chin",
              "david","dewa","desi","debby","dewi","dentoto","dika","delon","dewy","damara",
-             "erwin","elvina","evelyn","enzo",
+             "erwin","elvina","evelyn","enzo","oppe","komandan","melinda","ranger",
              "fira","fahresa","findlay","fatimah",
              "gita","genzo","gambrong",
              "habib","hefi","hoihai","herfizah","hanny","hanabi","hokage","hoi hai",
