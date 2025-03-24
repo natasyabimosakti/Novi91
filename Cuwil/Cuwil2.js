0b04da6ed52e5d4cfb8d79ca1b3fa48649189c4d
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-03-24 16:10:15 +0700

    Update Cuwil2.js

--- Cuwil/Cuwil2.js
+++ Cuwil/Cuwil2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cuwil 2
 // @namespace    http://tampermonkey.net/
-// @version      3.8
+// @version      3.9
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Cuwil/Cuwil2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Cuwil/Cuwil2.js
@@ -47,7 +47,7 @@ var namagroup9 = 'TAFSIR MIMPI';
 var Comment9 = 'SIJITOGEL NYOMANGEDE1 06*09*45'; 
 
 var namagroup10 = 'PASAR KAGET';
-var Comment10 = 'DAGELAN4D(NYODOKBRINJI) : 99*24'; 
+var Comment10 = 'DAGELAN4D(NYODOKBRINJI) : 99*24*88'; 
 
 var namagroup11 = 'MAYAPADA';
 var Comment11 = 'BETT*Mayapada4D(BO)*NYODOPANGAN9*39*15*71'; 
