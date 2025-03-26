7f13d05168c0a4e814356164f345e74e5bacc4ea
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-03-26 14:46:42 +0700

    Update Penyok3.js

--- Penyok/Penyok3.js
+++ Penyok/Penyok3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Penyok 3
 // @namespace    http://tampermonkey.net/
-// @version      3.15
+// @version      3.16
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Penyok/Penyok3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Penyok/Penyok3.js
@@ -30,7 +30,7 @@ var namagroup5 = 'KEITOGEL';
 var Comment5 = '#keitogel = (SUWUNBOSQU) = 80*51*95'; 
 
 var namagroup6 = 'KIOS';
-var Comment6 = 'KIOSTOTO=SUGEHTERUS12=72*39'; 
+var Comment6 = 'KIOSTOTO=SUGEHTERUS12=72*39*99'; 
 
 var namagroup7 = '453P VIP';
 var Comment7 = 'ASEPTOGEL SUKMUMETNDES 77*46*69'; 
