347b6a26f0b7a2ff4baf1221ba4f47e183569822
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-03-26 14:44:06 +0700

    Update Zulf1.js

--- Zulf/Zulf1.js
+++ Zulf/Zulf1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW ZULF1
 // @namespace    http://tampermonkey.net/
-// @version      3.12
+// @version      3.13
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Zulf/Zulf1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Zulf/Zulf1.js
@@ -32,7 +32,7 @@ var namagroup5 = 'KEITOGEL';
 var Comment5 = '#keitogel = (TANMO38) = 01*44*31'; 
 
 var namagroup6 = 'KIOS';
-var Comment6 = 'KIOSTOTO=TANLALI14=88*75'; 
+var Comment6 = 'KIOSTOTO=TANLALI14=88*75*65'; 
 
 var namagroup7 = '453P VIP';
 var Comment7 = 'ASEPTOGEL TANLONTO12 17*84*50'; 
