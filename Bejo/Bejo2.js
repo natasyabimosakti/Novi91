e847917e5a46a18c096132bc3f3316d35cd6d68a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-04-17 05:15:49 +0700

    Update Bejo2.js

--- Bejo/Bejo2.js
+++ Bejo/Bejo2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Bejo 2
 // @namespace    http://tampermonkey.net/
-// @version      3.29
+// @version      3.30
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bejo/Bejo2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bejo/Bejo2.js
