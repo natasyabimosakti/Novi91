c77b9f069c6da6a48b596a79c1ea084ae9a53c78
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-04-17 05:17:00 +0700

    Update Bejo3.js

--- Bejo/Bejo3.js
+++ Bejo/Bejo3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Bejo 3
 // @namespace    http://tampermonkey.net/
-// @version      3.29
+// @version      3.30
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bejo/Bejo3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bejo/Bejo3.js
@@ -184,7 +184,7 @@ var myrefresh = setInterval(function(){
                 var commentbox = document.querySelectorAll('[data-tracking-duration-id')[ntv].getElementsByClassName('native-text')
                 // Cek Jam
 
-                if (postingan.textContent.includes("Baru")||postingan.textContent.split(' men')[0].slice(-2) == 1||postingan.textContent.split(' men')[0].slice(-2) == 2||postingan.textContent.split(' men')[0].slice(-2) == 3||postingan.textContent.split(' men')[0].slice(-2) == 4||postingan.textContent.split(' men')[0].slice(-2) == 5){
+                if (postingan.textContent.includes("Baru")||postingan.textContent.split(' meni')[0].slice(-2) == 1||postingan.textContent.split(' meni')[0].slice(-2) == 2||postingan.textContent.split(' meni')[0].slice(-2) == 3||postingan.textContent.split(' meni')[0].slice(-2) == 4||postingan.textContent.split(' meni')[0].slice(-2) == 5||postingan.textContent.split(' meni')[0].slice(-2) == "‎1"||postingan.textContent.split(' meni')[0].slice(-2) == "‎2"||postingan.textContent.split(' meni')[0].slice(-2) == "‎3"||postingan.textContent.split(' meni')[0].slice(-2) == "‎4"||postingan.textContent.split(' meni')[0].slice(-2) == "‎5"){
                     console.log("Jam Ditemukan " + jamposting1)
                     if(postingan.textContent.toLowerCase().includes(Backlist1.toLowerCase())
                        ||postingan.textContent.toLowerCase().includes(Backlist2.toLowerCase())
