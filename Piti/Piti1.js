3f0120012741e9b00810873f06433ec533377b9a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-09 00:33:16 +0700

    Update Piti1.js

--- Piti/Piti1.js
+++ Piti/Piti1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Piti1
 // @namespace    http://tampermonkey.net/
-// @version      3.30
+// @version      3.31
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti1.js
@@ -110,7 +110,7 @@ var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","aditia","a
              "fira","fahresa","fiana","fahmi","fiona","fania",
              "gita","kang bona","hoky","julianti","libra","garda",
              "habib","hefi","hoihai","hana","hoki","hokage",
-             "icha","iyatoto","invest","ivanna","inisial","ishaura","imam",
+             "icha","iyatoto","invest","ivanna","inisial","ishaura","imam","isticharo",
              "jordi","jaguar","jne","jovanka","jessica","je pe","jess","jenifer","jhone",
              "keitogel","kumbara","kembar","kotna","karina","katharina","kemon","kaka","karla","komandan",
              "lianda","lusiana","lina","laura","lehman","leader","leon","lidya","langit","leader","loetoe",
