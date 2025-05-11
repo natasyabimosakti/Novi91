9331847275a12966c594e8353a3505325da16e82
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-11 20:38:07 +0700

    Update Bosku3.js

--- Bosku/Bosku3.js
+++ Bosku/Bosku3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BOSQUE3
 // @namespace    http://tampermonkey.net/
-// @version      3.165
+// @version      3.166
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku3.js
@@ -119,7 +119,8 @@ var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","adi","aldi
              "xian","vivi",
              "yanty","yoky","yohana","yura","yaya",
              "wulan","wok","wak",
-             "zuko"];
+             "zuko",
+             "Kanaya","Dealova","Amel Lia","Keyza"];
 
 
 var keyword1 = "ROOM"
