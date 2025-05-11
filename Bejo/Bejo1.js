04a94827f77e91c00f3b96d34fd420136d1deb7f
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-11 20:37:29 +0700

    Update Bejo1.js

--- Bejo/Bejo1.js
+++ Bejo/Bejo1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Bejo 1
 // @namespace    http://tampermonkey.net/
-// @version      3.32
+// @version      3.33
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bejo/Bejo1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bejo/Bejo1.js
@@ -116,7 +116,8 @@ var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","adi","aldi
              "xian","vivi",
              "yanty","yoky","yohana","yura","yaya",
              "wulan","wok","wak",
-             "zuko"];
+             "zuko",
+             "Kanaya","Dealova","Amel Lia","Keyza"];
 
 
 var keyword1 = "ROOM"
