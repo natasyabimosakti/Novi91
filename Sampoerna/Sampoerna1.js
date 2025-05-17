56d29ec0e3581d8d5a86f4fffec7183b42aa5e4f
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-17 08:11:39 +0700

    Update Sampoerna1.js

--- Sampoerna/Sampoerna1.js
+++ Sampoerna/Sampoerna1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sampoerna1
 // @namespace    http://tampermonkey.net/
-// @version      3.168
+// @version      3.169
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna1.js
@@ -244,7 +244,7 @@ var myrefresh = setInterval(function(){
                             for (var clk = 0; clk < commentbox.length; clk++) {
                                 if(commentbox[clk]){
                                     console.log("comment box ditemukan")
-                                    if(commentbox[clk].textContent.toLowerCase().includes("jawab")||commentbox[clk].textContent.toLowerCase().includes("tulis")||commentbox[clk].textContent.toLowerCase().includes("komentari")||commentbox[clk].textContent.toLowerCase().includes("postingan")){
+                                    if(commentbox[clk].textContent.toLowerCase().includes("jawab")||commentbox[clk].textContent.toLowerCase().includes("tulis")||commentbox[clk].textContent.toLowerCase().includes("komentari")||commentbox[clk].textContent.toLowerCase().includes("postingan")||commentbox[clk].textContent.toLowerCase().includes("beri")){
                                         clearInterval(myrefresh);
                                         console.log("Click Posting box")
                                         jitter = 0
