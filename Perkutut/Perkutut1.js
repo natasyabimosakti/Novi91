13ccafd507191baa3879168351a81056215d7d9f
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-17 08:09:49 +0700

    Update Perkutut1.js

--- Perkutut/Perkutut1.js
+++ Perkutut/Perkutut1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Perkutut1
 // @namespace    http://tampermonkey.net/
-// @version      3.176
+// @version      3.177
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Perkutut/Perkutut1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Perkutut/Perkutut1.js
@@ -236,7 +236,7 @@ var myrefresh = setInterval(function(){
                             for (var clk = 0; clk < commentbox.length; clk++) {
                                 if(commentbox[clk]){
                                     console.log("comment box ditemukan")
-                                    if(commentbox[clk].textContent.toLowerCase().includes("jawab")||commentbox[clk].textContent.toLowerCase().includes("tulis")||commentbox[clk].textContent.toLowerCase().includes("komentari")||commentbox[clk].textContent.toLowerCase().includes("postingan")){
+                                    if(commentbox[clk].textContent.toLowerCase().includes("jawab")||commentbox[clk].textContent.toLowerCase().includes("tulis")||commentbox[clk].textContent.toLowerCase().includes("komentari")||commentbox[clk].textContent.toLowerCase().includes("postingan")||commentbox[clk].textContent.toLowerCase().includes("beri")){
                                         clearInterval(myrefresh);
                                         console.log("Click Posting box")
                                         jitter = 0
@@ -273,7 +273,7 @@ var myrefresh = setInterval(function(){
         if(document.querySelectorAll("[role='presentation']")[0]){
             if (document.readyState === "complete") {
                 for (var coki = 0; coki < waktupost.length; coki++) {
-                      if(waktupost[coki].textContent === "Aktivitas terbaru") {
+                       if(waktupost[coki].textContent === "Aktivitas terbaru") {
                         if(jitter == 1){
                             return;
                         }
