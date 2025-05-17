633ad8b6078134503b128a26e53b5da46ae26cda
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-18 05:35:15 +0700

    Update Piti2.js

--- Piti/Piti2.js
+++ Piti/Piti2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Piti2
 // @namespace    http://tampermonkey.net/
-// @version      3.32
+// @version      3.33
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti2.js
@@ -120,7 +120,8 @@ var admin = ["Siâo","andre","adiat","andy","ayunda","audi","arxidi","aditia","a
              "sandiego","san","sanjaya","siska","safar","sinta","surianti","satria","sapto","salsabila","sanchez","sofia","sonia","serena","sahara","specialis","sam","sasha","sintia","sifa","satria","sellia","sintya","stevent",
              "tink","tiktak","tiara","tatang","tania","thonex",
              "yanty","yoky","yohana","yii","vero","vaulian",
-             "wulan","wok","widya"];
+             "wulan","wok","widya",
+             "raja","mega","jonh","james","stephen"];
 
 
 
