1583f4c17c4841aaf68ac176dedb681ece6ded59
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-19 08:30:06 +0700

    Update Cuwil4.js

--- Cuwil/Cuwil4.js
+++ Cuwil/Cuwil4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cuwil 4
 // @namespace    http://tampermonkey.net/
-// @version      3.23
+// @version      3.24
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Cuwil/Cuwil4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Cuwil/Cuwil4.js
