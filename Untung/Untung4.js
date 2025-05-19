1a2a89e2eddcefa4f3cae179b3f67e9617a85e90
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-19 08:32:27 +0700

    Update Untung4.js

--- Untung/Untung4.js
+++ Untung/Untung4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Untung 4
 // @namespace    http://tampermonkey.net/
-// @version      3.27
+// @version      3.28
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Untung/Untung4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Untung/Untung4.js
@@ -140,7 +140,7 @@ var keyword13 = "nemo"
 
 var Backlist1 = "pemenang lomba";
 var Backlist2 = "rekap";
-var Backlist3 = "hasil";
+var Backlist3 = "natidulu";
 var Backlist4 = "room lomba freebet";
 var Backlist5 = "prediksi";
 var Backlist6 = "result";
