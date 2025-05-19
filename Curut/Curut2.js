03c136c294e3d11f567d34580a504a2368b7df5a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-19 22:24:46 +0700

    Update Curut2.js

--- Curut/Curut2.js
+++ Curut/Curut2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW CURUT2
 // @namespace    http://tampermonkey.net/
-// @version      3.171
+// @version      3.172
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut2.js
@@ -242,6 +242,10 @@ var refreshPage = setInterval(function(){
                 for (var coki = 0; coki < waktupost.length; coki++) {
                     if(waktupost[coki].textContent === "Aktivitas terbaru") {
                         if(document.getElementsByClassName("prevent-scrolling")[0]){
+                             if (isDound) {
+
+                                 clickAt(1, 1);
+                             }
                             waktupost[coki].click()
                         }
 
