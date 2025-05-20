74c67ec55cff02e44db4bccfa2abd403c3858497
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-21 04:27:39 +0700

    Update Manuk1.js

--- Manuk/Manuk1.js
+++ Manuk/Manuk1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MANUK 1
 // @namespace    http://tampermonkey.net/
-// @version      3.161
+// @version      3.162
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk1.js
@@ -262,7 +262,6 @@ var myrefresh = setInterval(function(){
                                 clickAt(1, 1);
                                 console.log("comment box ditemukan")
                                 clearInterval(myrefresh);
-                                clearInterval(refreshPage);
                                 console.log("Click Posting box")
                                 tombolKirim.click();
                                 game.start()
@@ -275,15 +274,7 @@ var myrefresh = setInterval(function(){
                 }
             }
         }
-    }
-
-
-},1)
-
-
-var refreshPage = setInterval(function(){
-
-    var urutkan = document.querySelectorAll("[data-mcomponent='ServerTextArea']");
+        var urutkan = document.querySelectorAll("[data-mcomponent='ServerTextArea']");
     var waktupost = document.getElementsByClassName("native-text");
     if(!document.querySelectorAll("[role='presentation']")[0]){
         if (document.readyState === "complete") {
@@ -319,8 +310,10 @@ var refreshPage = setInterval(function(){
             }
         }
     }
-}, refresh * 10)
+    }
 
+
+},refresh * 10)
 
 var commentToPost = '';
 var grouptToPost = '';
@@ -381,7 +374,7 @@ function gameClosure() {
     return {
         start() {
             if (currentGame) clearInterval(currentGame);
-            currentGame = setInterval(game, 100);
+            currentGame = setInterval(game, 10);
         },
         stop() {
             if (currentGame) clearInterval(currentGame);
@@ -439,10 +432,9 @@ function scanPosts() {
             GM.setValue("group_" + grouptToPost, true);
             GM.setValue("group_"+grouptToPost+"_expire", Date.now() + EXPIRATION_MS);
             console.log("✅ Komentar DIKIRIM (via dispatch):", commentToPost);
-
             setTimeout(() => {
                 startAutoTask();
-            }, 1000); // Reload ringan setelah kirim
+            }, 2000); // Reload ringan setelah kirim
         });
     } else {
         console.log("❌ Textarea atau tombol kirim tidak ditemukan.");
