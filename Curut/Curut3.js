54e4582ebacc9470a42d048241291ac31f197126
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-24 13:07:22 +0700

    Update Curut3.js

--- Curut/Curut3.js
+++ Curut/Curut3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW CURUT3
 // @namespace    http://tampermonkey.net/
-// @version      3.176
+// @version      3.177
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut3.js
@@ -343,7 +343,7 @@ function scanPosts() {
 
 var intervalId = null;
 function autoTask() {
-    //location.href = "about:blank";
+    location.href = "about:blank";
 }
 
 // Fungsi untuk memulai interval — tidak langsung dipanggil
