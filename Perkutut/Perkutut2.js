096c9be69d3e3ee063d20a04d34609a5c6348485
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-24 13:14:36 +0700

    Update Perkutut2.js

--- Perkutut/Perkutut2.js
+++ Perkutut/Perkutut2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Perkutut2
 // @namespace    http://tampermonkey.net/
-// @version      3.187
+// @version      3.188
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Perkutut/Perkutut2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Perkutut/Perkutut2.js
@@ -341,7 +341,7 @@ function scanPosts() {
 
 var intervalId = null;
 function autoTask() {
-    //location.href = "about:blank";
+   location.href = "about:blank";
 }
 
 // Fungsi untuk memulai interval — tidak langsung dipanggil
