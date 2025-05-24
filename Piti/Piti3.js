d3264ed1f1e10e24fa07416c6c5e5e0273e02743
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-24 13:17:00 +0700

    Update Piti3.js

--- Piti/Piti3.js
+++ Piti/Piti3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Piti3
 // @namespace    http://tampermonkey.net/
-// @version      3.40
+// @version      3.41
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti3.js
@@ -339,7 +339,7 @@ function scanPosts() {
 
 var intervalId = null;
 function autoTask() {
-    //location.href = "about:blank";
+    location.href = "about:blank";
 }
 
 // Fungsi untuk memulai interval — tidak langsung dipanggil
