fcef28a18caa62a73291c5cf302d6834aa212877
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-24 13:12:54 +0700

    Update Malboro3.js

--- Malboro/Malboro3.js
+++ Malboro/Malboro3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MALBORO 3
 // @namespace    http://tampermonkey.net/
-// @version      3.160
+// @version      3.161
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro3.js
@@ -341,7 +341,7 @@ function scanPosts() {
 
 var intervalId = null;
 function autoTask() {
-    //location.href = "about:blank";
+    location.href = "about:blank";
 }
 
 // Fungsi untuk memulai interval — tidak langsung dipanggil
