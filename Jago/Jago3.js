ca325b9aa7a3c958dd7114ad75fbe5d779404f0a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-05-24 13:12:21 +0700

    Update Jago3.js

--- Jago/Jago3.js
+++ Jago/Jago3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JAGO 3
 // @namespace    http://tampermonkey.net/
-// @version      3.8
+// @version      3.9
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Jago/Jago3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Jago/Jago3.js
@@ -343,7 +343,7 @@ function scanPosts() {
 
 var intervalId = null;
 function autoTask() {
-    //location.href = "about:blank";
+    location.href = "about:blank";
 }
 
 // Fungsi untuk memulai interval — tidak langsung dipanggil
