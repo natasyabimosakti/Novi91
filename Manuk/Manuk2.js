1e24feb6725c743155d1a0f13e8ed8ca54e77a33
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-06-02 10:16:03 +0700

    Update Manuk2.js

--- Manuk/Manuk2.js
+++ Manuk/Manuk2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MANUK 2
 // @namespace    http://tampermonkey.net/
-// @version      3.176
+// @version      3.177
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manuk/Manuk2.js
@@ -230,10 +230,15 @@ if(document.location.href.includes("group")){
     });
     myObserver.observe(document.body, { childList: true, subtree: true });
 }
+setTimeout(() => {
+    mulaiRefresh()
+}, 5000);
 var sudahDiPanggil = false
 async function manageGroups() {
-    if(sudahDiPanggil)return;
-    sudahDiPanggil = true
+    if(grouptToPost.length <= 1){
+     return;
+    }
+
     for (const { groupId, defaultValue } of groups) {
         const key = `group_${groupId}`;
         const expireKey = `${key}_expire`;
@@ -252,11 +257,10 @@ async function manageGroups() {
         return;
 
     }else{
+        if(sudahDiPanggil)return;
+        sudahDiPanggil = true
         cekArticle();
         tungguMentionsContainer();
-        setTimeout(() => {
-            mulaiRefresh()
-        }, 5000);
     }
 }
 
