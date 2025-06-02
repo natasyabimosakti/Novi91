d7fa0deaa28d793cda34974ef247e99a10d9486a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-06-02 08:16:11 +0700

    Update Piti3.js

--- Piti/Piti3.js
+++ Piti/Piti3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Piti3
 // @namespace    http://tampermonkey.net/
-// @version      3.52
+// @version      3.53
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti3.js
@@ -169,13 +169,11 @@ function getCommentForGroup() {
     }
     return null;
 }
-let tungguGroupSudahJalan = false;
 
 let sudahDiproses = false;
 
 function tungguGroup() {
-    if (tungguGroupSudahJalan) return;
-    tungguGroupSudahJalan = true;
+
 
     const observer = new MutationObserver((mutations) => {
         for (const mutation of mutations) {
@@ -183,13 +181,16 @@ function tungguGroup() {
                 if (node.nodeType !== 1) continue;
                 const container = node.querySelector?.('.fixed-container');
                 if (container && !sudahDiproses) {
-                    sudahDiproses = true;
+
                     const result = getCommentForGroup();
                     if (result) {
                         commentToPost = result.comment;
                         grouptToPost = result.groupName;
                         console.log("✅ Nama grup : " + result.groupName + " | Comment : " + result.comment);
-                        manageGroups();
+                        if(grouptToPost.length > 2){
+                            sudahDiproses = true;
+                            manageGroups();
+                        }
                         observer.disconnect();
                     }
                 }
