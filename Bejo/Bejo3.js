5dd0fd2e2373b53861f5192cbc07c954313acc3e
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-06-02 08:12:06 +0700

    Update Bejo3.js

--- Bejo/Bejo3.js
+++ Bejo/Bejo3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Bejo 3
 // @namespace    http://tampermonkey.net/
-// @version      3.54
+// @version      3.55
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bejo/Bejo3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bejo/Bejo3.js
@@ -182,13 +182,11 @@ function getCommentForGroup() {
     }
     return null;
 }
-let tungguGroupSudahJalan = false;
 
 let sudahDiproses = false;
 
 function tungguGroup() {
-    if (tungguGroupSudahJalan) return;
-    tungguGroupSudahJalan = true;
+
 
     const observer = new MutationObserver((mutations) => {
         for (const mutation of mutations) {
@@ -196,13 +194,16 @@ function tungguGroup() {
                 if (node.nodeType !== 1) continue;
                 const container = node.querySelector?.('.fixed-container');
                 if (container && !sudahDiproses) {
-                    sudahDiproses = true;
+
                     const result = getCommentForGroup();
                     if (result) {
                         commentToPost = result.comment;
                         grouptToPost = result.groupName;
                         console.log("✅ Nama grup : " + result.groupName + " | Comment : " + result.comment);
-                        manageGroups();
+                        if(grouptToPost.length > 2){
+                            sudahDiproses = true;
+                            manageGroups();
+                        }
                         observer.disconnect();
                     }
                 }
