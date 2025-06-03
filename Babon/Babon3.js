5df5976e0864125a6a85326500f58c16c807bb4c
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-06-04 02:47:24 +0700

    Update Babon3.js

--- Babon/Babon3.js
+++ Babon/Babon3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Babon 3
 // @namespace    http://tampermonkey.net/
-// @version      3.26
+// @version      3.27
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon3.js
@@ -21,8 +21,8 @@
 /*======================================================================Paste Script Tampermonkey di sini===============================================================*/
 
 
-var namagroup1 = 'NONGKRONG';
-var Comment1 = '#AMAVI5D ( MASIHMARIS ) : 50*91*21';
+var namagroup1 = '18NAGA';
+var Comment1 = '#18NAGA (MASIHMARIS) : 50*91*21BET';
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( MANDINI28 ) : 06*90*83';
 var namagroup3 = 'WARUNG';
@@ -33,7 +33,7 @@ var namagroup5 = 'PEDRO';
 var Comment5 = '#PEDRO4D (MUBENGAN52*56*43*31)';
 var namagroup6 = 'DIVA4D';
 var Comment6 = '#DIVA4D (MANCURMUJUR) = 15*69*30';
-var namagroup7 = 'KASTOTO';
+var namagroup7 = 'Moveon88';
 var Comment7 = '#KASTOTO(MULETENAK) = 35*12*78 #AGENTOGELTERPERCAYA';
 var namagroup8 = 'TOK99';
 var Comment8 = 'Tok99Toto ( MELONGOBEGOK ) : 75*77*41';
@@ -47,8 +47,8 @@ var namagroup12 = 'OPUNG';
 var Comment12 = 'OPUNG4D ( MENCLOKCUK ) : 86*66*46';
 var namagroup13 = 'GOHT0G3L';
 var Comment13 = 'GOHTOGEL=MENDUNGKUL=99*47*02';
-var namagroup14 = 'Samson';
-var Comment14 = '#SAMSONTOTO/MRIPATKUSIPIT/65*94';
+var namagroup14 = 'BLITAR';
+var Comment14 = '#BLITAR4D (MRIPATKUSIPIT) = 65*94';
 var namagroup15 = 'Hoho';
 var Comment15 = 'MARISUWEK : 26*54*34 #HOHOTOGEL';
 var namagroup16 = 'GTO';
