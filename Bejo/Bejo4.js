d16da0f2364d56c519d30caace59bfc15a17e806
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-06-08 06:38:46 +0700

    Update Bejo4.js

--- Bejo/Bejo4.js
+++ Bejo/Bejo4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Bejo 4
 // @namespace    http://tampermonkey.net/
-// @version      3.59
+// @version      3.60
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bejo/Bejo4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bejo/Bejo4.js
@@ -18,53 +18,37 @@
 
 
 var namagroup1 = 'SHIOKELINCI';
-var Comment1 = '#shiokelinci4d*WIRANG89*39*27'; 
-
+var Comment1 = '#shiokelinci4d*WIRANG89*39*27';
 var namagroup2 = 'BUKU';
-var Comment2 = 'IYATOTO WAKUL90 74*82*84'; 
-
+var Comment2 = 'IYATOTO WAKUL90 74*82*84';
 var namagroup3 = 'TIKTAK';
-var Comment3 = 'Tiktaktogel / WATAK91 / 91 , 93 , 23'; 
-
+var Comment3 = 'Tiktaktogel / WATAK91 / 91 , 93 , 23';
 var namagroup4 = 'GAIB';
-var Comment4 = 'GAIB4D=WEDI92=08*87*53'; 
-
+var Comment4 = 'GAIB4D=WEDI92=08*87*53';
 var namagroup5 = 'KEITOGEL';
-var Comment5 = '#keitogel = (WAGE93) = 94*89*29'; 
-
-var namagroup6 = 'KIOS';
-var Comment6 = 'KIOSTOTO=WULING94=31*44*59'; 
-
+var Comment5 = '#keitogel = (WAGE93) = 94*89*29';
+var namagroup6 = 'lomba dan event';
+var Comment6 = '# ( WULING94 ) : 31*44*59';
 var namagroup7 = '453P VIP';
-var Comment7 = 'ASEPTOGEL WENANG95 19*03*18'; 
-
+var Comment7 = 'ASEPTOGEL WENANG95 19*03*18';
 var namagroup8 = 'MENARA';
-var Comment8 = '#MENARA4D=WANGSUL96= 25*16'; 
-
+var Comment8 = '#MENARA4D=WANGSUL96= 25*16';
 var namagroup9 = 'GIL4';
-var Comment9 = 'GILA4D=WANI97=64*40*97'; 
-
+var Comment9 = 'GILA4D=WANI97=64*40*97';
 var namagroup10 = 'JNE';
-var Comment10 = '#JNETOTO(WEROH98)*63*30*97'; 
-
+var Comment10 = '#JNETOTO(WEROH98)*63*30*97';
 var namagroup11 = 'TOYIB';
-var Comment11 = '#TOYIBSLOT ( WADAH71 ) : 73*10*45'; 
-
+var Comment11 = '#TOYIBSLOT ( WADAH71 ) : 73*10*45';
 var namagroup12 = 'MASTER KUY';
-var Comment12 = 'TOGELKUY WEDANG72 52*26*70'; 
-
+var Comment12 = 'TOGELKUY WEDANG72 52*26*70';
 var namagroup13 = 'KOI';
-var Comment13 = '#KOITOTO ( WANGSIT73 ) 04*76'; 
-
+var Comment13 = '#KOITOTO ( WANGSIT73 ) 04*76';
 var namagroup14 = 'ANGKER';
-var Comment14 = 'ANGKER4D=WALUYO74=00*28*07'; 
-
+var Comment14 = 'ANGKER4D=WALUYO74=00*28*07';
 var namagroup15 = 'VESPA';
-var Comment15 = 'VESPATOGEL (WAGINEM75) 34*13*79'; 
-
+var Comment15 = 'VESPATOGEL (WAGINEM75) 34*13*79';
 var namagroup16 = 'Nemo';
-var Comment16 = 'NEMO4D (WALI76) : 22*24*21'; 
-
+var Comment16 = 'NEMO4D (WALI76) : 22*24*21';
 var namagroup17 = 'KIKO';
 var Comment17 = '#KIKOTOTO (WEGAH46) = 72*69';
   
