8aae3a59897cb2e1109a9546991c7fff6798f9ca
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-06-07 19:09:05 +0700

    Update Piti1.js

--- Piti/Piti1.js
+++ Piti/Piti1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Piti1
 // @namespace    http://tampermonkey.net/
-// @version      3.58
+// @version      3.59
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti1.js
@@ -29,7 +29,7 @@ var Comment5 = '#PEDRO4D (AMAR26*06*95*79)';
 var namagroup6 = 'DIVA4D';
 var Comment6 = '#DIVA4D (KUMU17) = 17*98*19';
 var namagroup7 = 'Moveon88';
-var Comment7 = '# ( AMIR23) : 34*44*76';
+var Comment7 = '# ( AMIR23 ) : 34*44*76';
 var namagroup8 = 'TOK99';
 var Comment8 = 'Tok99Toto ( AMIR22 ) : 48*18*72';
 var namagroup9 = 'TAFSIR MIMPI';
