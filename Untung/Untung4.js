0d9665981e8836e5b2e13c3f0d6dde13f557b941
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-06-09 00:07:07 +0700

    Update Untung4.js

--- Untung/Untung4.js
+++ Untung/Untung4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Untung 4
 // @namespace    http://tampermonkey.net/
-// @version      3.54
+// @version      3.55
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Untung/Untung4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Untung/Untung4.js
@@ -232,7 +232,7 @@ if(document.location.href.includes("group")){
                     if (tombol.length >= 2) {
                         cekTombolUrutkan = false;
                         tombol.forEach(btn => {
-                            if (countA < 3) {
+                            if (countA < 5) {
                                 if (btn.textContent.includes("Postingan baru")) {
                                     btn.click();
                                     countA++;
