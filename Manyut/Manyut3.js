ccd8ba542256462051d075b54bcda30271c91618
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-06-09 00:01:05 +0700

    Update Manyut3.js

--- Manyut/Manyut3.js
+++ Manyut/Manyut3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MANYUT3
 // @namespace    http://tampermonkey.net/
-// @version      3.268
+// @version      3.269
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut3.js
@@ -49,7 +49,7 @@ var namagroup14 = 'ANGKER';
 var Comment14 = 'ANGKER4D=ERFANGUN15=90*79*09';
 var namagroup15 = 'VESPA';
 var Comment15 = 'VESPATOGEL (ERKON23) 15*98*63';
-var namagroup16 = 'Nemo';
+var namagroup16 = 'NEMO';
 var Comment16 = 'NEMO4D (ERAKOL156) : 37*13*63';
 var namagroup17 = 'KIKO';
 var Comment17 = '#KIKOTOTO (EROSI52) = 02*86';
