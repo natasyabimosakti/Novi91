75decbd9b338a9c4a3f6668c223c8f4dcb3bd4d0
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-06-08 23:56:45 +0700

    Update Bosku1.js

--- Bosku/Bosku1.js
+++ Bosku/Bosku1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BOSQUE1
 // @namespace    http://tampermonkey.net/
-// @version      3.191
+// @version      3.192
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku1.js
@@ -46,7 +46,7 @@ var namagroup14 = 'ANGKER';
 var Comment14 = 'ANGKER4D=XELINA56=33*92*66';
 var namagroup15 = 'VESPA';
 var Comment15 = 'VESPATOGEL (XIONGYU6) 11*20*83';
-var namagroup16 = 'Nemo';
+var namagroup16 = 'NEMO';
 var Comment16 = 'NEMO4D (XENDION) : 58*88*41';
 var namagroup17 = 'KIKO';
 var Comment17 = '#KIKOTOTO (XERLAK67) = 77*05';
