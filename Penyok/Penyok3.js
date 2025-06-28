bfeddd500e2154d28494660c35f588b444226b65
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-06-29 04:43:17 +0700

    Update Penyok3.js

--- Penyok/Penyok3.js
+++ Penyok/Penyok3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Penyok 3
 // @namespace    http://tampermonkey.net/
-// @version      3.48
+// @version      3.49
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Penyok/Penyok3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Penyok/Penyok3.js
@@ -16,25 +16,25 @@
 // @connect      raw.githubusercontent.com
 // ==/UserScript==
 
-var namagroup1 = 'SHIOKELINCI';
+var namagroup1 = 'shio';
 var Comment1 = '#shiokelinci4d*SUSUMUGEDI*47*23';
 var namagroup2 = 'BUKU';
 var Comment2 = 'IYATOTO SUSUKUCILIK 26*53*82';
 var namagroup3 = 'TIKTAK';
 var Comment3 = 'Tiktaktogel / SUKARONDO77 / 22 , 35 , 78';
-var namagroup4 = 'GAIB';
+var namagroup4 = 'G41B';
 var Comment4 = 'GAIB4D=SUKSUKANBAE=60*32*38';
 var namagroup5 = 'KEITOGEL';
 var Comment5 = '#keitogel = (SUWUNBOSQU) = 80*51*95';
-var namagroup6 = 'lomba dan event';
-var Comment6 = '# ( SUGEHTERUS12 ) : 72*39*99';
+var namagroup6 = 'Papuatoto';
+var Comment6 = '#PAPUATOTO=(SUGEHTERUS12)=72-39';
 var namagroup7 = '453P VIP';
 var Comment7 = 'ASEPTOGEL SUKMUMETNDES 77*46*69';
 var namagroup8 = 'MENARA';
 var Comment8 = '#MENARA4D=SUMUKSEKALIG= 61*89';
-var namagroup9 = 'GIL4';
+var namagroup9 = 'G1LA';
 var Comment9 = 'GILA4D=SUMEGEHBAE=34*76*24';
-var namagroup10 = 'JNE';
+var namagroup10 = 'GROUP LOMBA ANGKA';
 var Comment10 = '#JNETOTO(SUNDULUSU)*21*73*63';
 var namagroup11 = 'TOYIB';
 var Comment11 = '#TOYIBSLOT ( SUWENGLAMBEMU ) : 93*62*63';
