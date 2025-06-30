6ee6a576f46f70913cbf0648aa10cb9201a71956
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-06-30 17:47:38 +0700

    Update Bosku2.js

--- Bosku/Bosku2.js
+++ Bosku/Bosku2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BOSQUE2
 // @namespace    http://tampermonkey.net/
-// @version      3.193
+// @version      3.194
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku2.js
 // @author       You
@@ -369,15 +369,24 @@ async function botKoment(mutatin) {
                     clickEvent.initEvent("mousedown", true, true);
                     sendBtn.dispatchEvent(clickEvent);
 
-                    GM.setValue("group_" + grouptToPost, true);
-                    GM.setValue("group_"+grouptToPost+"_expire", Date.now() + EXPIRATION_MS);
+
                     console.log("✅ Komentar DIKIRIM (via dispatch):", commentToPost);
-                    showNotification("Komentar Sudah Terkirim : " + commentToPost);
+
                     isCommenting = true;
 
                     kondisiStop = true
                     observercomment.disconnect();
+                    if(document.querySelector("[role='dialog']")){
+                        if(document.querySelector("[role='dialog']").textContent.includes("Ada Masalah")){
+                            return;
+                        }
+                    }
+
+                    GM.setValue("group_" + grouptToPost, true);
+                    GM.setValue("group_"+grouptToPost+"_expire", Date.now() + EXPIRATION_MS);
+                    showNotification("Komentar Sudah Terkirim : " + commentToPost);
                     startAutoTask();
+
                     break;
                 } else {
                     showNotification("❌ Textarea atau tombol kirim tidak ditemukan");
