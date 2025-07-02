4665adffee55e33a1c30752633748959cf097583
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-02 14:59:31 +0700

    Update Jago2.js

--- Jago/Jago2.js
+++ Jago/Jago2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JAGO 2
 // @namespace    http://tampermonkey.net/
-// @version      3.30
+// @version      3.31
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Jago/Jago2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Jago/Jago2.js
@@ -581,6 +581,7 @@ function cekLogout() {
             if (!window.__sudahKirimLogout) {
                 window.__sudahKirimLogout = true;
                 sendToTelegram("⚠️ Facebook LOGOUT.");
+                startAutoTask()
             }
         }
     } catch (e) {
