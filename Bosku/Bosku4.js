36936d04010cc4d9b080aea18154ee7f894dce57
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-02 14:58:40 +0700

    Update Bosku4.js

--- Bosku/Bosku4.js
+++ Bosku/Bosku4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BOSQUE4
 // @namespace    http://tampermonkey.net/
-// @version      3.197
+// @version      3.198
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku4.js
@@ -579,6 +579,7 @@ function cekLogout() {
             if (!window.__sudahKirimLogout) {
                 window.__sudahKirimLogout = true;
                 sendToTelegram("⚠️ Facebook LOGOUT.");
+                startAutoTask()
             }
         }
     } catch (e) {
