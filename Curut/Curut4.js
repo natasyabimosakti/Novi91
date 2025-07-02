aae3169e7f66cd89ca05c52651f012286a8b0bc5
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-02 15:03:00 +0700

    Update Curut4.js

--- Curut/Curut4.js
+++ Curut/Curut4.js
@@ -578,7 +578,7 @@ function cekLogout() {
             if (!window.__sudahKirimLogout) {
                 window.__sudahKirimLogout = true;
                 sendToTelegram("⚠️ Facebook LOGOUT.");
-                startAutoTask()
+               
             }
         }
     } catch (e) {
