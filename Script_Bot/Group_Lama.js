b887defdc82b1bf0c55c73a3b777e663ed89bfc3
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-02 23:35:33 +0700

    Update Group_Lama.js

--- Script_Bot/Group_Lama.js
+++ Script_Bot/Group_Lama.js
@@ -1,21 +1,3 @@
-// ==UserScript==
-// @name         NEW Bejo 2
-// @namespace    http://tampermonkey.net/
-// @version      3.69
-// @description  try to take over the world!
-// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bejo/Bejo2.js
-// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bejo/Bejo2.js
-// @author       You
-// @match        http*://*/*
-// @run-at       document-end
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        GM.setValue
-// @grant        GM.getValue
-// @grant        window.close
-// @grant        GM_xmlhttpRequest
-// @connect      api.telegram.org
-// @connect      raw.githubusercontent.com
-// ==/UserScript==
 
 var config = window.BOT_CONFIG || {};
 var namagroup1  = config.namagroup1  || ""; const Comment1  = config.Comment1  || "";
