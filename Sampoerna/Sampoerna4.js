575b0494a1b508f49416cf3a70f93db99798f9f2
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-03 17:36:33 +0700

    Update Sampoerna4.js

--- Sampoerna/Sampoerna4.js
+++ Sampoerna/Sampoerna4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sampoerna4
 // @namespace    http://tampermonkey.net/
-// @version      3.212
+// @version      3.213
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Sampoerna/Sampoerna4.js
@@ -613,14 +613,14 @@ async function sendToTelegram(message) {
 async function cekMasalah() {
     try {
         const now = Date.now();
-        const COOLDOWNPostingan = 30 * 60 * 1000; // 5 menit
+        const COOLDOWNPostingan = 60 * 60 * 1000; // 5 menit
         const lastTimepost = await GM.getValue("lastTelegramSame", 0);
 
         if ((now - lastTimepost < COOLDOWNPostingan)) {
-            console.log("⏱️ sudah dikirim sebelumnya");
+             console.log("⏱️ sudah dikirim sse jam yang lalu");
             return;
         }else{
-            GM.setValue("lastTelegramSame", now);
+             GM.setValue("lastTelegramSame", 0);
         }
 
         const elem = document.querySelectorAll("[data-screen-key-action-ids]")[1];
