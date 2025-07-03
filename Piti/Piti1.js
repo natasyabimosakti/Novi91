7224610fe21da8820218224cadf2042c596a3952
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-03 17:25:08 +0700

    Update Piti1.js

--- Piti/Piti1.js
+++ Piti/Piti1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Piti1
 // @namespace    http://tampermonkey.net/
-// @version      3.72
+// @version      3.73
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti1.js
@@ -540,7 +540,7 @@ var TELEGRAM_TOKEN = '7479985104:AAF-ISIxbf18g_mOasLoubBwBKgkfSFzzAw'; // GANTI
 var TELEGRAM_CHAT_ID = '983068551'; // GANTI
 
 let lastMessageSent = ""; // lokal per tab/browser
-
+var sudahkirim = false
 function normalizeText(text) {
     return text
         .trim()
@@ -571,6 +571,7 @@ function levenshtein(a, b) {
 
 // Kirim ke Telegram, dengan deteksi spam berbasis kemiripan
 async function sendToTelegram(message) {
+    if (sudahkirim) return;
     const fullMessage = `📡 [${SCRIPT_NAME}]\n${message}`;
     const normalizedMessage = normalizeText(fullMessage);
 
@@ -595,9 +596,11 @@ async function sendToTelegram(message) {
         method: "GET",
         url: `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage?chat_id=${TELEGRAM_CHAT_ID}&text=${encodeURIComponent(fullMessage)}`,
         onload: function (res) {
+            sudahkirim = true
             console.log("✅ Telegram terkirim:", res.responseText);
             GM.setValue("lastTelegramMessage", fullMessage);
             GM.setValue("lastTelegramTime", now);
+            GM.setValue("lastTelegramSame", now);
         },
         onerror: function (err) {
             console.error("❌ Gagal kirim ke Telegram:", err);
@@ -607,6 +610,17 @@ async function sendToTelegram(message) {
 
 async function cekMasalah() {
     try {
+        const now = Date.now();
+        const COOLDOWNPostingan = 30 * 60 * 1000; // 5 menit
+        const lastTimepost = await GM.getValue("lastTelegramSame", 0);
+
+        if ((now - lastTimepost < COOLDOWNPostingan)) {
+            console.log("⏱️ sudah dikirim sebelumnya");
+            return;
+        }else{
+            GM.setValue("lastTelegramSame", now);
+        }
+
         const elem = document.querySelectorAll("[data-screen-key-action-ids]")[1];
         if (!elem) return;
 
@@ -617,6 +631,7 @@ async function cekMasalah() {
         if (isi.includes("masalah")) {
             const cleanText = dialog.textContent.trim();
             await sendToTelegram(`🛑 Ada "masalah":\n\n${cleanText}`);
+            startAutoTask()
 
         }
     } catch (e) {
