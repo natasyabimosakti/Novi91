fe9b363f6b16158d4a8ecc3743946eb2a323f04b
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-03 18:13:02 +0700

    Update Babon1.js

--- Babon/Babon1.js
+++ Babon/Babon1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Babon 1
 // @namespace    http://tampermonkey.net/
-// @version      3.47
+// @version      3.48
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon1.js
@@ -576,6 +576,7 @@ function levenshtein(a, b) {
 // Kirim ke Telegram, dengan deteksi spam berbasis kemiripan
 async function sendToTelegram(message) {
     if (sudahkirim) return;
+ sudahkirim = true
     const fullMessage = `📡 [${SCRIPT_NAME}]\n${message}`;
     const normalizedMessage = normalizeText(fullMessage);
 
@@ -600,7 +601,7 @@ async function sendToTelegram(message) {
         method: "GET",
         url: `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage?chat_id=${TELEGRAM_CHAT_ID}&text=${encodeURIComponent(fullMessage)}`,
         onload: function (res) {
-            sudahkirim = true
+            
             console.log("✅ Telegram terkirim:", res.responseText);
             GM.setValue("lastTelegramMessage", fullMessage);
             GM.setValue("lastTelegramTime", now);
@@ -614,6 +615,7 @@ async function sendToTelegram(message) {
 
 async function cekMasalah() {
     try {
+      if (sudahkirim) return;
         const now = Date.now();
         const COOLDOWNPostingan = 60 * 60 * 1000; // 5 menit
         const lastTimepost = await GM.getValue("lastTelegramSame", 0);
