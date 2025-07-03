84d923c9bc3700c032164074b73f2763d1ffab16
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-03 17:34:51 +0700

    Update Babon1.js

--- Babon/Babon1.js
+++ Babon/Babon1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Babon 1
 // @namespace    http://tampermonkey.net/
-// @version      3.46
+// @version      3.47
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon1.js
@@ -615,14 +615,14 @@ async function sendToTelegram(message) {
 async function cekMasalah() {
     try {
         const now = Date.now();
-        const COOLDOWNPostingan = 30 * 60 * 1000; // 5 menit
+        const COOLDOWNPostingan = 60 * 60 * 1000; // 5 menit
         const lastTimepost = await GM.getValue("lastTelegramSame", 0);
 
         if ((now - lastTimepost < COOLDOWNPostingan)) {
-            console.log("⏱️ sudah dikirim sebelumnya");
+             console.log("⏱️ sudah dikirim sse jam yang lalu");
             return;
         }else{
-            GM.setValue("lastTelegramSame", now);
+             GM.setValue("lastTelegramSame", 0);
         }
 
         const elem = document.querySelectorAll("[data-screen-key-action-ids]")[1];
