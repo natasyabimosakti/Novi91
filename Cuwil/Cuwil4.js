a31c3d6079b90eadb4a9830ac2871d9d194b00b2
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-03 17:12:04 +0700

    Update Cuwil4.js

--- Cuwil/Cuwil4.js
+++ Cuwil/Cuwil4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cuwil 4
 // @namespace    http://tampermonkey.net/
-// @version      3.62
+// @version      3.63
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Cuwil/Cuwil4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Cuwil/Cuwil4.js
@@ -545,7 +545,7 @@ var TELEGRAM_TOKEN = '7479985104:AAF-ISIxbf18g_mOasLoubBwBKgkfSFzzAw'; // GANTI
 var TELEGRAM_CHAT_ID = '983068551'; // GANTI
 
 let lastMessageSent = ""; // lokal per tab/browser
-
+var sudahkirim = false
 function normalizeText(text) {
     return text
         .trim()
@@ -576,6 +576,7 @@ function levenshtein(a, b) {
 
 // Kirim ke Telegram, dengan deteksi spam berbasis kemiripan
 async function sendToTelegram(message) {
+    if (sudahkirim) return;
     const fullMessage = `📡 [${SCRIPT_NAME}]\n${message}`;
     const normalizedMessage = normalizeText(fullMessage);
 
@@ -600,6 +601,7 @@ async function sendToTelegram(message) {
         method: "GET",
         url: `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage?chat_id=${TELEGRAM_CHAT_ID}&text=${encodeURIComponent(fullMessage)}`,
         onload: function (res) {
+            sudahkirim = true
             console.log("✅ Telegram terkirim:", res.responseText);
             GM.setValue("lastTelegramMessage", fullMessage);
             GM.setValue("lastTelegramTime", now);
@@ -622,6 +624,7 @@ async function cekMasalah() {
         if (isi.includes("masalah")) {
             const cleanText = dialog.textContent.trim();
             await sendToTelegram(`🛑 Ada "masalah":\n\n${cleanText}`);
+            startAutoTask()
 
         }
     } catch (e) {
