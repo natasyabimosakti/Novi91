663679cc02604e1cd752e32bed080b322f25f07e
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-03 17:36:39 +0700

    Update Untung1.js

--- Untung/Untung1.js
+++ Untung/Untung1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Untung 1
 // @namespace    http://tampermonkey.net/
-// @version      3.66
+// @version      3.67
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Untung/Untung1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Untung/Untung1.js
@@ -612,14 +612,14 @@ async function sendToTelegram(message) {
 async function cekMasalah() {
     try {
         const now = Date.now();
-        const COOLDOWNPostingan = 30 * 60 * 1000; // 5 menit
+        const COOLDOWNPostingan = 60 * 60 * 1000; // 5 menit
         const lastTimepost = await GM.getValue("lastTelegramSame", 0);
 
         if ((now - lastTimepost < COOLDOWNPostingan)) {
-            console.log("⏱️ sudah dikirim sebelumnya");
+             console.log("⏱️ sudah dikirim sse jam yang lalu");
             return;
         }else{
-            GM.setValue("lastTelegramSame", now);
+             GM.setValue("lastTelegramSame", 0);
         }
 
         const elem = document.querySelectorAll("[data-screen-key-action-ids]")[1];
