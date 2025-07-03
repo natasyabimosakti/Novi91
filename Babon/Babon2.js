5f901a8f096e313eecb466d626c5a85ea3da62e1
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-03 20:18:05 +0700

    Update Babon2.js

--- Babon/Babon2.js
+++ Babon/Babon2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Babon 2
 // @namespace    http://tampermonkey.net/
-// @version      3.47
+// @version      3.48
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon2.js
@@ -539,7 +539,7 @@ function startAutoTask() {
 
 var SCRIPT_NAME = Comment17
 var TELEGRAM_TOKEN = '7479985104:AAF-ISIxbf18g_mOasLoubBwBKgkfSFzzAw'; // GANTI
-var TELEGRAM_CHAT_ID = '983068551'; // GANTI
+var TELEGRAM_CHAT_ID = '-1002717306025'; // GANTI
 
 let lastMessageSent = ""; // lokal per tab/browser
 var sudahkirim = false
