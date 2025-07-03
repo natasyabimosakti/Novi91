960d4f266e55dd9ffa349d1263e4cd92c977291a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-03 18:08:29 +0700

    Update Bejo4.js

--- Bejo/Bejo4.js
+++ Bejo/Bejo4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Bejo 4
 // @namespace    http://tampermonkey.net/
-// @version      3.71
+// @version      3.72
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bejo/Bejo4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bejo/Bejo4.js
@@ -571,7 +571,7 @@ async function sendToTelegram(message) {
     if (sudahkirim) return;
     const fullMessage = `📡 [${SCRIPT_NAME}]\n${message}`;
     const normalizedMessage = normalizeText(fullMessage);
-
+    sudahkirim = true
     const lastSent = await GM.getValue("lastTelegramMessage", "");
     const normalizedLast = normalizeText(lastSent);
 
@@ -593,7 +593,7 @@ async function sendToTelegram(message) {
         method: "GET",
         url: `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage?chat_id=${TELEGRAM_CHAT_ID}&text=${encodeURIComponent(fullMessage)}`,
         onload: function (res) {
-            sudahkirim = true
+            
             console.log("✅ Telegram terkirim:", res.responseText);
             GM.setValue("lastTelegramMessage", fullMessage);
             GM.setValue("lastTelegramTime", now);
@@ -606,6 +606,7 @@ async function sendToTelegram(message) {
 }
 
 async function cekMasalah() {
+    if (sudahkirim) return;
     try {
         const now = Date.now();
         const COOLDOWNPostingan = 60 * 60 * 1000; // 5 menit
