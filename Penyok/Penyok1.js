f120d84c7ca2fb94898a7ed1c0c903fd11f868e7
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-04 19:44:39 +0700

    Update Penyok1.js

--- Penyok/Penyok1.js
+++ Penyok/Penyok1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Penyok 1
 // @namespace    http://tampermonkey.net/
-// @version      3.59
+// @version      3.60
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Penyok/Penyok1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Penyok/Penyok1.js
@@ -27,7 +27,7 @@ var namagroup4 = 'G41B';
 var Comment4 = 'GAIB4D=OKTOPUS45=25*85*67';
 var namagroup5 = 'KEITOGEL';
 var Comment5 = '#keitogel = (OKBOSKU555) = 01*44*31';
-var namagroup6 = 'Papuatoto';
+var namagroup6 = 'PAPUAT0T0';
 var Comment6 = '#PAPUATOTO=(OKBRASKAYU)=88-75';
 var namagroup7 = '453P VIP';
 var Comment7 = 'ASEPTOGEL OKBENGTANG 17*84*50';
@@ -59,7 +59,7 @@ var Comment18 = 'group Penyok 1';
 
 var refresh = 40;
 var URLADMIN = "https://raw.githubusercontent.com/natasyabimosakti/ADMIN/main/Admin_group_Lama.json"
-var keyword = ["ROOM","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌","HONGKONG","SINGAPUR","nemo","l0mb4","lomb4","l0mba","𝗥𝟬𝟬𝗠","𝗟𝟬𝗠𝗕𝗔"]
+var keyword = ["ROOM","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌","HONGKONG","SINGAPUR","nemo","l0mb4","lomb4","l0mba","𝗥𝟬𝟬𝗠","𝗟𝟬𝗠𝗕𝗔","𝘙𝘖𝘖𝘔"]
 var Backlist =["pemenang lomba","rekap","natidulu","room lomba freebet","prediksi","result","juara lomba","r3k4p","r3kap","rek4p","undang" ]
 var isCommenting = false;
 var EXPIRATION_MS = 8 * 60 * 1000; // 5 minutes
