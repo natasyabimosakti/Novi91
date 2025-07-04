439e8ddae5bb776331b3a4a79d6464a6533bfbb7
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-04 19:38:26 +0700

    Update Bejo3.js

--- Bejo/Bejo3.js
+++ Bejo/Bejo3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Bejo 3
 // @namespace    http://tampermonkey.net/
-// @version      3.73
+// @version      3.74
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bejo/Bejo3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bejo/Bejo3.js
@@ -27,7 +27,7 @@ var namagroup4 = 'G41B';
 var Comment4 = 'GAIB4D=GARUK14=04*76*81';	
 var namagroup5 = 'KEITOGEL';	
 var Comment5 = '#keitogel = (GELEM15) = 52*26*70';	
-var namagroup6 = 'Papuatoto';	
+var namagroup6 = 'PAPUAT0T0';	
 var Comment6 = '#PAPUATOTO=(GEMAR24)=73-10';	
 var namagroup7 = '453P VIP';	
 var Comment7 = 'ASEPTOGEL GALAK17 63*30*72';	
@@ -59,7 +59,7 @@ var Comment18 = 'group Bejo 3';
 
 var refresh = 40;
 var URLADMIN = "https://raw.githubusercontent.com/natasyabimosakti/ADMIN/main/Admin_group_Lama.json"
-var keyword = ["ROOM","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌","HONGKONG","SINGAPUR","nemo","l0mb4","lomb4","l0mba","𝗥𝟬𝟬𝗠","𝗟𝟬𝗠𝗕𝗔"]
+var keyword = ["ROOM","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌","HONGKONG","SINGAPUR","nemo","l0mb4","lomb4","l0mba","𝗥𝟬𝟬𝗠","𝗟𝟬𝗠𝗕𝗔","𝘙𝘖𝘖𝘔"]
 var Backlist =["pemenang lomba","rekap","natidulu","room lomba freebet","prediksi","result","juara lomba","r3k4p","r3kap","rek4p","undang" ]
 var isCommenting = false;
 var EXPIRATION_MS = 8 * 60 * 1000; // 5 minutes
