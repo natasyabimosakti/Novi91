03c11ceadf8dc1fb2a76bf508df3723ce6f13273
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-06 05:49:45 +0700

    Update Piti3.js

--- Piti/Piti3.js
+++ Piti/Piti3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Piti3
 // @namespace    http://tampermonkey.net/
-// @version      3.76
+// @version      3.77
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti3.js
@@ -17,8 +17,8 @@
 // @connect      raw.githubusercontent.com
 // ==/UserScript==
 
-var namagroup1 = '18NAGA';
-var Comment1 = '#18NAGA (CIANAS16) : 80*26*51 BETT';
+var namagroup1 = 'MONTIR';
+var Comment1 = '#MONTIRTOTO (CIANAS16) = 80*26*51';
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( CAKTRAS ) : 90*65*84';
 var namagroup3 = 'WARUNG';
@@ -29,8 +29,8 @@ var namagroup5 = 'PEDRO';
 var Comment5 = '#PEDRO4D (CIMN26*41*04*64)';
 var namagroup6 = 'DIVA4D';
 var Comment6 = '#DIVA4D (CEKODAR13) = 97*07*08';
-var namagroup7 = 'Moveon88';
-var Comment7 = '# ( CAKADUR14 ) : 25*45*83';
+var namagroup7 = 'TREX';
+var Comment7 = '#HATORIBET*CAKADUR14*25*45';
 var namagroup8 = '𝐀𝐋𝐋𝐏𝐀𝐒';
 var Comment8 = 'Tok99Toto ( CAKADUR14 ) : 68*70*87';
 var namagroup9 = 'TAFSIR MIMPI';
@@ -41,8 +41,8 @@ var namagroup11 = 'MAYAPADA';
 var Comment11 = 'BETT*Mayapada4D(BO)*CUMNI212*86*33*22';
 var namagroup12 = 'OPUNG';
 var Comment12 = 'OPUNG4D ( CIRUN19 ) : 99*85*37';
-var namagroup13 = 'GOHT0G3L';
-var Comment13 = 'GOHTOGEL=CUMNI21=47*60*21';
+var namagroup13 = 'UPIN';
+var Comment13 = '#UPINSLOT ( CUMNI21) 47*60*21 BETTING';
 var namagroup14 = 'BLITAR';
 var Comment14 = '#BLITAR4D (CIMAR19) = 66*02';
 var namagroup15 = 'Hoho';
