4dd27d27d72683c7ee8cd8910e06dd9c25ffa268
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-08 10:28:41 +0700

    Update Babon1.js

--- Babon/Babon1.js
+++ Babon/Babon1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Babon 1
 // @namespace    http://tampermonkey.net/
-// @version      3.53
+// @version      3.54
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon1.js
@@ -19,39 +19,6 @@
 
 
 /*======================================================================Paste Script Tampermonkey di sini===============================================================*/
-var namagroup1 = 'MONTIR';
-var Comment1 = '#MONTIRTOTO (SUNTIKMATI) = 59*95';
-var namagroup2 = 'K86';
-var Comment2 = 'K86TOTO ( SETIASAJA ) : 18*28*38';
-var namagroup3 = 'WARUNG';
-var Comment3 = '#(WARUNGTOTO) = (SUKUMLAKU) 13*36*58';
-var namagroup4 = 'RIATOTO';
-var Comment4 = '#RIATOTO SAMBUTAN20 = 42*76*16';
-var namagroup5 = 'PEDRO';
-var Comment5 = '#PEDRO4D (SASISU42*71*48*25)';
-var namagroup6 = 'DIVA4D';
-var Comment6 = '#DIVA4D (SISWARUK) = 92*84*39';
-var namagroup7 = 'TREX';
-var Comment7 = '#HATORIBET*SEMBODOH*72*45';
-var namagroup8 = '??????';
-var Comment8 = 'Tok99Toto ( SINGKONGTHAI ) : 42*76*00';
-var namagroup9 = 'TAFSIR MIMPI';
-var Comment9 = 'SIJITOGEL SUMURMATI 82*22*20';
-var namagroup10 = 'KAGET';
-var Comment10 = 'DAGELAN4D(SUNDALA23) : 70*14*67';
-var namagroup11 = 'MAYAPADA';
-var Comment11 = 'BETT*Mayapada4D(BO)*SAKUKUTARA*32*57*88';
-var namagroup12 = 'OPUNG';
-var Comment12 = 'OPUNG4D ( SIANGANAJA ) : 73*04*81';
-var namagroup13 = 'UPIN';
-var Comment13 = '#UPINSLOT ( SUMBUBULU) 79*74*51 BETTING';
-var namagroup14 = 'BLITAR';
-var Comment14 = '#BLITAR4D (SAMBUNGBAYAM) = 82*22';
-var namagroup15 = 'Hoho';
-var Comment15 = 'SUKAJANTUA : 85*08*60 #HOHOTOGEL';
-var namagroup16 = 'GTO';
-var Comment16 = '(GUDANGTOTO) = (SLEMPANG26) 19*17*33';
-
 
 var namagroup1 = 'MONTIR';
 var Comment1 = '#MONTIRTOTO (SUNTIKMATI) = 59*95';
