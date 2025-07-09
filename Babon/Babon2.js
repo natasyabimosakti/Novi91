4a9ad923986f7d7f799a7a6a98505c9b8ad1438d
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-09 23:47:35 +0700

    Auto-update Babon/Babon2.js dari sel F47

--- Babon/Babon2.js
+++ Babon/Babon2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Babon 2
 // @namespace    http://tampermonkey.net/
-// @version      3.54
+// @version      3.55
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon2.js
@@ -17,7 +17,7 @@
 // @connect      raw.githubusercontent.com
 // ==/UserScript==
 
-var namagroup1 = 'ASU';
+var namagroup1 = 'MONTIR';
 var Comment1 = '#MONTIRTOTO (PEMPEK12) = 73*04';
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( PIALADIGILIR ) : 85*08*60';
