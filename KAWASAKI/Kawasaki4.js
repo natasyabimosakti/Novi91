e77540b83bc84c618bd6aa5f51547ccbebdda81a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-10 05:54:04 +0700

    Auto-update KAWASAKI/Kawasaki4.js dari sel J47

--- KAWASAKI/Kawasaki4.js
+++ KAWASAKI/Kawasaki4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kawasaki 4
 // @namespace    http://tampermonkey.net/
-// @version      3.1
+// @version      3.2
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/KAWASAKI/Kawasaki4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/KAWASAKI/Kawasaki4.js
@@ -17,7 +17,7 @@
 // @connect      raw.githubusercontent.com
 // ==/UserScript==
 
-var namagroup1 = 'MONTIR';
+var namagroup1 = 'COWEK';
 var Comment1 = '#MONTIRTOTO () = ';
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO (  ) : ';
