555d0ac36c7ca5ace2b30b003b27172e8028e380
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-10 04:47:29 +0700

    Auto-update KAWASAKI/Kawasaki2.js dari sel F47

--- KAWASAKI/Kawasaki2.js
+++ KAWASAKI/Kawasaki2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kawasaki 2
 // @namespace    http://tampermonkey.net/
-// @version      3.0
+// @version      3.1
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/KAWASAKI/Kawasaki2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/KAWASAKI/Kawasaki2.js
@@ -18,37 +18,37 @@
 // ==/UserScript==
 
 var namagroup1 = 'MONTIR';
-var Comment1 = '#MONTIRTOTO (SUNTIKMATI) = 59*95';
+var Comment1 = '#MONTIRTOTO () = ';
 var namagroup2 = 'K86';
-var Comment2 = 'K86TOTO ( SETIASAJA ) : 18*28*38';
+var Comment2 = 'K86TOTO (  ) : ';
 var namagroup3 = 'WARUNG';
-var Comment3 = '#(WARUNGTOTO) = (SUKUMLAKU) 13*36*58';
+var Comment3 = '#(WARUNGTOTO) = () ';
 var namagroup4 = 'RIATOTO';
-var Comment4 = '#RIATOTO SAMBUTAN20 = 42*76*16';
+var Comment4 = '#RIATOTO  = ';
 var namagroup5 = 'PEDRO';
-var Comment5 = '#PEDRO4D (SASISU42*71*48*25)';
+var Comment5 = '#PEDRO4D (*)';
 var namagroup6 = 'DIVA4D';
-var Comment6 = '#DIVA4D (SISWARUK) = 92*84*39';
+var Comment6 = '#DIVA4D () = ';
 var namagroup7 = 'TREX';
-var Comment7 = '#HATORIBET*SEMBODOH*72*45';
+var Comment7 = '';
 var namagroup8 = 'ALLPAS';
-var Comment8 = 'Tok99Toto ( SINGKONGTHAI ) : 42*76*00';
+var Comment8 = 'Tok99Toto (  ) : ';
 var namagroup9 = 'TAFSIR MIMPI';
-var Comment9 = 'SIJITOGEL SUMURMATI 82*22*20';
+var Comment9 = '';
 var namagroup10 = 'KAGET';
-var Comment10 = 'DAGELAN4D(SUNDALA23) : 70*14*67';
+var Comment10 = '';
 var namagroup11 = 'MAYAPADA';
-var Comment11 = 'BETT*Mayapada4D(BO)*SAKUKUTARA*32*57*88';
+var Comment11 = '';
 var namagroup12 = 'OPUNG';
-var Comment12 = 'OPUNG4D ( SIANGANAJA ) : 73*04*81';
+var Comment12 = '';
 var namagroup13 = 'UPIN';
-var Comment13 = '#UPINSLOT ( SUMBUBULU) 79*74*51 BETTING';
+var Comment13 = '';
 var namagroup14 = 'BLITAR';
-var Comment14 = '#BLITAR4D (SAMBUNGBAYAM) = 82*22';
+var Comment14 = '';
 var namagroup15 = 'Hoho';
-var Comment15 = 'SUKAJANTUA : 85*08*60 #HOHOTOGEL';
+var Comment15 = '';
 var namagroup16 = 'GTO';
-var Comment16 = '(GUDANGTOTO) = (SLEMPANG26) 19*17*33';
+var Comment16 = '';
 
 //Batas
 
