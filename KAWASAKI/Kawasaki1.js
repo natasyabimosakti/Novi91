70356efafb0de2ead0ff3e76b0b16cd9e3b0d7ca
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-10 04:45:43 +0700

    Update Kawasaki1.js

--- KAWASAKI/Kawasaki1.js
+++ KAWASAKI/Kawasaki1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kawasaki 1
 // @namespace    http://tampermonkey.net/
-// @version      3.58
+// @version      3.0
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/KAWASAKI/Kawasaki1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/KAWASAKI/Kawasaki1.js
