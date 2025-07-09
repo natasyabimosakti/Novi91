bbd54d518414b83bea939d276880889bdf596eed
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-10 05:11:23 +0700

    Update Malboro4.js

--- Malboro/Malboro4.js
+++ Malboro/Malboro4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MALBORO 4
 // @namespace    http://tampermonkey.net/
-// @version      3.191
+// @version      3.192
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro4.js
@@ -54,7 +54,7 @@ var namagroup16 = 'NEMO';
 var Comment16 = 'NEMO4D (DADAKU32) : 04*76*97';
 var namagroup17 = 'KIKO';
 var Comment17 = '#KIKOTOTO (DENGKULMU41) = 81*70';
-
+//Batas
 var namagroup18 = 'Jawatengah';
 var Comment18 = 'Group Malb 4';
 
