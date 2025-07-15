4a568837a351aa9356ac8e8b7a2d9d0ab68295bb
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-15 09:07:44 +0700

    Update Cuwil2.js

--- Cuwil/Cuwil2.js
+++ Cuwil/Cuwil2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cuwil 2
 // @namespace    http://tampermonkey.net/
-// @version      3.81
+// @version      3.82
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Cuwil/Cuwil2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Cuwil/Cuwil2.js
@@ -17,8 +17,8 @@
 // @connect      raw.githubusercontent.com
 // ==/UserScript==
 
-var namagroup1 = 'MONTIR';
-var Comment1 = '#MONTIRTOTO (NYOLOGMOTO) = 73*87';
+var namagroup1 = 'MELATI';
+var Comment1 = '*MELATI188 (NYOLOGMOTO) : 73*87 *BETTING';
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( NYOGOKSILIT ) : 50*41*60';
 var namagroup3 = 'WARUNG';
@@ -35,8 +35,8 @@ var namagroup8 = 'ALLPAS';
 var Comment8 = 'Tok99Toto ( NYOCOTTERUS ) : 61*97*69';
 var namagroup9 = 'TAFSIR MIMPI';
 var Comment9 = 'SIJITOGEL NYOMANGEDE1 06*09*45';
-var namagroup10 = 'KAGET';
-var Comment10 = 'DAGELAN4D(NYODOKBRINJI) : 99*24*88';
+var namagroup10 = 'UDANG';
+var Comment10 = 'UDANGBET ( NYODOKBRINJI ) = 99*24*88';
 var namagroup11 = 'MAYAPADA';
 var Comment11 = 'BETT*Mayapada4D(BO)*NYODOPANGAN9*39*15*71';
 var namagroup12 = 'OPUNG';
@@ -66,7 +66,7 @@ var Comment18 = 'asek';
 var SCRIPT_NAME = Comment17
 var refresh = 40;
 var URLADMIN = "https://raw.githubusercontent.com/natasyabimosakti/ADMIN/refs/heads/main/Admin_group_Baru.json"
-var keyword = ["ROOM","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌","HONGKONG","SINGAPUR","nemo","l0mb4","lomb4","l0mba","𝗥𝟬𝟬𝗠","𝗟𝟬𝗠𝗕𝗔","𝘙𝘖𝘖𝘔","hatori"]
+var keyword = ["ROOM","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌","HONGKONG","SINGAPUR","nemo","l0mb4","lomb4","l0mba","𝗥𝟬𝟬𝗠","𝗟𝟬𝗠𝗕𝗔","𝘙𝘖𝘖𝘔","hatori","klikh4tori001"]
 var Backlist =["pemenang lomba","rekap","natidulu","room lomba freebet","prediksi","result","juara lomba","r3k4p","r3kap","rek4p","undang" ]
 var isCommenting = false;
 var EXPIRATION_MS = 8 * 60 * 1000; // 5 minutes
