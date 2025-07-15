42a25583e4d2e2d88f4e299fc518b0925f662aff
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-15 09:06:01 +0700

    Update Babon2.js

--- Babon/Babon2.js
+++ Babon/Babon2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Babon 2
 // @namespace    http://tampermonkey.net/
-// @version      3.69
+// @version      3.70
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon2.js
@@ -17,8 +17,8 @@
 // @connect      raw.githubusercontent.com
 // ==/UserScript==
 
-var namagroup1 = 'MONTIR';
-var Comment1 = '#MONTIRTOTO (PEMPEK12) = 73*04';
+var namagroup1 = 'MELATI';
+var Comment1 = '*MELATI188 (PEMPEK12) : 73*04 *BETTING';
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( PIALADIGILIR ) : 85*08*60';
 var namagroup3 = 'WARUNG';
@@ -35,8 +35,8 @@ var namagroup8 = 'ALLPAS';
 var Comment8 = 'Tok99Toto ( PINGGULGEDE ) : 37*89*24';
 var namagroup9 = 'TAFSIR MIMPI';
 var Comment9 = 'SIJITOGEL PUNDAKLAH 03*40*64';
-var namagroup10 = 'KAGET';
-var Comment10 = 'DAGELAN4D(PRAWANKOTA) : 71*48*25';
+var namagroup10 = 'UDANG';
+var Comment10 = 'UDANGBET ( PRAWANKOTA ) = 71*48*25';
 var namagroup11 = 'MAYAPADA';
 var Comment11 = 'BETT*Mayapada4D(BO)*PELANGAIR*39*84*92';
 var namagroup12 = 'OPUNG';
@@ -62,7 +62,7 @@ var Comment18 = 'asek';
 var SCRIPT_NAME = Comment17
 var refresh = 40;
 var URLADMIN = "https://raw.githubusercontent.com/natasyabimosakti/ADMIN/refs/heads/main/Admin_group_Baru.json"
-var keyword = ["ROOM","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌","HONGKONG","SINGAPUR","nemo","l0mb4","lomb4","l0mba","𝗥𝟬𝟬𝗠","𝗟𝟬𝗠𝗕𝗔","𝘙𝘖𝘖𝘔","hatori"]
+var keyword = ["ROOM","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌","HONGKONG","SINGAPUR","nemo","l0mb4","lomb4","l0mba","𝗥𝟬𝟬𝗠","𝗟𝟬𝗠𝗕𝗔","𝘙𝘖𝘖𝘔","hatori","klikh4tori001"]
 var Backlist =["pemenang lomba","rekap","natidulu","room lomba freebet","prediksi","result","juara lomba","r3k4p","r3kap","rek4p","undang" ]
 var isCommenting = false;
 var EXPIRATION_MS = 8 * 60 * 1000; // 5 minutes
