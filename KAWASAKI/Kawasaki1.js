73d82ad22494d4af8128f55371f1f68db973b21b
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-19 15:16:54 +0700

    Update Kawasaki1.js

--- KAWASAKI/Kawasaki1.js
+++ KAWASAKI/Kawasaki1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kawasaki 1
 // @namespace    http://tampermonkey.net/
-// @version      3.15
+// @version      3.16
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/KAWASAKI/Kawasaki1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/KAWASAKI/Kawasaki1.js
@@ -57,6 +57,7 @@ var Comment17 = 'Kawasaki 1';
 
 var namagroup18 = 'lajw';
 var Comment18 = 'asek';
+
 
 
 var SCRIPT_NAME = Comment17
@@ -299,7 +300,7 @@ async function manageGroups() {
 let sedangKlikTextbox = false;
 function CekBacklist(postinganBL) {
     for (const DataBacklist of Backlist) {
-        const kata = normalizeToBasicLatin(DataBacklist.toLowerCase())
+        const kata = DataBacklist.toLowerCase()
         if (postinganBL.toLowerCase().includes(kata)) {
             console.log(`❌ Diblok karena mengandung: "${kata}"`);
             return true;
@@ -311,7 +312,7 @@ function CekBacklist(postinganBL) {
 function CekKeyword(postingan) {
     console.log("🔍 CekKeyword untuk:", postingan);
     for (const DataKeyword of keyword) {
-        const kata = normalizeToBasicLatin(DataKeyword.toLowerCase())
+        const kata = DataKeyword.toLowerCase()
         if (postingan.toLowerCase().includes(kata)) {
             console.log(`✅ Keyword ditemukan: "${kata}"`);
             return true;
@@ -735,5 +736,3 @@ function Random(comment) {
 
     return result;
 }
-
-
