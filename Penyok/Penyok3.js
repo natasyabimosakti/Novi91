8b871a5ad4c106a86ad92c8b8b5b74438050224f
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-20 18:30:46 +0700

    Update Penyok3.js

--- Penyok/Penyok3.js
+++ Penyok/Penyok3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Penyok 3
 // @namespace    http://tampermonkey.net/
-// @version      3.69
+// @version      3.70
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Penyok/Penyok3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Penyok/Penyok3.js
@@ -17,24 +17,24 @@
 // @connect      raw.githubusercontent.com
 // ==/UserScript==
 
-var namagroup1 = 'shio';
-var Comment1 = '#shiokelinci4d*SUSUMUGEDI*47*23';
+var namagroup1 = 'ALLPAS';
+var Comment1 = 'Tok99Toto ( SUMUKSEKALIG ) : 47*23*10';
 var namagroup2 = 'BUKU';
 var Comment2 = 'IYATOTO SUSUKUCILIK 26*53*82';
 var namagroup3 = 'TIKTAK';
 var Comment3 = 'Tiktaktogel / SUKARONDO77 / 22 , 35 , 78';
-var namagroup4 = 'G41B';
-var Comment4 = 'GAIB4D=SUKSUKANBAE=60*32*38';
+var namagroup4 = 'TAFSIR MIMPI';
+var Comment4 = 'SIJITOGEL SUMEGEHBAE 60*32*38';
 var namagroup5 = 'KEITOGEL';
 var Comment5 = '#keitogel = (SUWUNBOSQU) = 80*51*95';
 var namagroup6 = 'PAPUAT0T0';
 var Comment6 = '#PAPUATOTO=(SUGEHTERUS12)=72-39';
 var namagroup7 = '453P VIP';
 var Comment7 = 'ASEPTOGEL SUKMUMETNDES 77*46*69';
-var namagroup8 = 'MENARA';
-var Comment8 = '#MENARA4D=SUMUKSEKALIG= 61*89';
-var namagroup9 = 'G1LA';
-var Comment9 = 'GILA4D=SUMEGEHBAE=34*76*24';
+var namagroup8 = 'OPUNG';
+var Comment8 = 'OPUNG4D ( SUSUBERUANG ) : 61*89*97';
+var namagroup9 = 'MAYAPADA';
+var Comment9 = 'BETT*Mayapada4D(BO)*SUWENGLAMBE*34*76*24';
 var namagroup10 = 'GROUP LOMBA ANGKA';
 var Comment10 = '#JNETOTO(SUNDULUSU)*21*73*63';
 var namagroup11 = 'TOYIB';
@@ -43,8 +43,8 @@ var namagroup12 = 'MASTER KUY';
 var Comment12 = 'TOGELKUY SUSUBERUANG 37*13*02';
 var namagroup13 = 'KOI';
 var Comment13 = '#KOITOTO ( SUKAMAMPIR ) 15*98';
-var namagroup14 = 'ANGKER';
-var Comment14 = 'ANGKER4D=SUNANMUHIBIN=90*79*09';
+var namagroup14 = 'Hoho';
+var Comment14 = 'SUNDULLANGIT : 90*79*09 #HOHOTOGEL';
 var namagroup15 = 'VESPA';
 var Comment15 = 'VESPATOGEL (SUNDULKELANG) 16*81*71';
 var namagroup16 = 'NEMO';
