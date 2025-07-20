3a1327a99d1687875d923f3109ad9224c4e315ac
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-20 18:26:45 +0700

    Update Malboro2.js

--- Malboro/Malboro2.js
+++ Malboro/Malboro2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MALBORO 2
 // @namespace    http://tampermonkey.net/
-// @version      3.200
+// @version      3.201
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Malboro/Malboro2.js
@@ -19,24 +19,24 @@
 
 
 
-var namagroup1 = 'shio';
-var Comment1 = '#shiokelinci4d*BOTAX35*67*98';
+var namagroup1 = 'ALLPAS';
+var Comment1 = 'Tok99Toto ( BOSTER73 ) : 67*98*05';
 var namagroup2 = 'BUKU';
 var Comment2 = 'IYATOTO BONCEL58 02*09*60';
 var namagroup3 = 'TIKTAK';
 var Comment3 = 'Tiktaktogel / BIJILO458 / 58 , 88 , 41';
-var namagroup4 = 'G41B';
-var Comment4 = 'GAIB4D=BOLANGBALING=11*20*83';
+var namagroup4 = 'TAFSIR MIMPI';
+var Comment4 = 'SIJITOGEL BOSMAN67 11*20*83';
 var namagroup5 = 'KEITOGEL';
 var Comment5 = '#keitogel = (BOTOLO3) = 33*92*66';
 var namagroup6 = 'PAPUAT0T0';
 var Comment6 = '#PAPUATOTO=(BAKEKLO)=12-86';
 var namagroup7 = '453P VIP';
 var Comment7 = 'ASEPTOGEL BOGEL438 71*96*75';
-var namagroup8 = 'MENARA';
-var Comment8 = '#MENARA4D=BERAK28= 78*46';
-var namagroup9 = 'G1LA';
-var Comment9 = 'GILA4D=BAJUKU34=51*35*01';
+var namagroup8 = 'OPUNG';
+var Comment8 = 'OPUNG4D ( BOSBIS48 ) : 78*46*77';
+var namagroup9 = 'MAYAPADA';
+var Comment9 = 'BETT*Mayapada4D(BO)*BOSBIG53*51*35*01';
 var namagroup10 = 'GROUP LOMBA ANGKA';
 var Comment10 = '#JNETOTO(BANANA12)*55*90*06';
 var namagroup11 = 'TOYIB';
@@ -45,8 +45,8 @@ var namagroup12 = 'MASTER KUY';
 var Comment12 = 'TOGELKUY BANTAISAJA 47*54*49';
 var namagroup13 = 'KOI';
 var Comment13 = '#KOITOTO ( BACOTX ) 65*56';
-var namagroup14 = 'ANGKER';
-var Comment14 = 'ANGKER4D=BERAK28=14*57*42';
+var namagroup14 = 'Hoho';
+var Comment14 = 'BOSNOM63 : 14*57*42 #HOHOTOGEL';
 var namagroup15 = 'VESPA';
 var Comment15 = 'VESPATOGEL (BAKULAN12) 37*15*99';
 var namagroup16 = 'NEMO';
