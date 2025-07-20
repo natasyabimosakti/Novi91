f97a5d80c016832d653d65130d1676ae19299918
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-20 18:32:17 +0700

    Update Piti4.js

--- Piti/Piti4.js
+++ Piti/Piti4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Piti4
 // @namespace    http://tampermonkey.net/
-// @version      3.96
+// @version      3.97
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti4.js
@@ -18,37 +18,37 @@
 // ==/UserScript==
 
 var namagroup1 = 'MELATI';
-var Comment1 = '*MELATI188 (DOKARLARI) : 47*60 *BETTING';
+var Comment1 = '*MELATI188 (DOKARLARI) : 11*83 *BETTING';
 var namagroup2 = 'K86';
-var Comment2 = 'K86TOTO ( DOLANAN ) : 66*02*94';
+var Comment2 = 'K86TOTO ( DOLANAN ) : 52*61*32';
 var namagroup3 = 'WARUNG';
-var Comment3 = '#(WARUNGTOTO) = (DONDONG22) 16*67*30';
+var Comment3 = '#(WARUNGTOTO) = (DONDONG22) 33*30*53';
 var namagroup4 = 'RIATOTO';
-var Comment4 = '#RIATOTO DONGSEL = 27*10*53';
+var Comment4 = '#RIATOTO DONGSEL = 77*43*69';
 var namagroup5 = 'PEDRO';
-var Comment5 = '#PEDRO4D (DUGALTOK*09*24*96)';
+var Comment5 = '#PEDRO4D (DUGALTOK*75*89*64)';
 var namagroup6 = 'DIVA4D';
-var Comment6 = '#DIVA4D (DUDADUWEL) = 23*49*20';
+var Comment6 = '#DIVA4D (DUDADUWEL) = 98*23*82';
 var namagroup7 = 'TREX';
-var Comment7 = '#HATORIBET*DUKUMANIS*86*33*22';
-var namagroup8 = 'ALLPAS';
-var Comment8 = 'Tok99Toto ( DEREK25 ) : 77*85*37';
-var namagroup9 = 'TAFSIR MIMPI';
-var Comment9 = 'SIJITOGEL DIYENG23 41*04*64';
-var namagroup10 = 'UDANG';
-var Comment10 = 'UDANGBET ( DULUANGIH ) = 97*07*08';
-var namagroup11 = 'MAYAPADA';
-var Comment11 = 'BETT*Mayapada4D(BO)*DRUMBUN*25*45*83';
-var namagroup12 = 'OPUNG';
-var Comment12 = 'OPUNG4D ( DELOKDEWE ) : 68*70*87';
-var namagroup13 = 'UPIN';
-var Comment13 = '#UPINSLOT ( DINGDONG24) 80*26*51 BETTING';
-var namagroup14 = 'BLITAR';
-var Comment14 = '#BLITAR4D (DUNGKEK28) = 90*65';
-var namagroup15 = 'Hoho';
-var Comment15 = 'DANCOAN : 58*88*29 #HOHOTOGEL';
-var namagroup16 = 'GTO';
-var Comment16 = '(GUDANGTOTO) = (DIANUKAE) 32*55*63';
+var Comment7 = '#HATORIBET*DUKUMANIS*46*56*71';
+var namagroup8 = 'BLITAR';
+var Comment8 = '#BLITAR4D (DANCOAN) = 72*47';
+var namagroup9 = 'TESTER';
+var Comment9 = '#ASIATOTO = {AOECK62} = 35*06';
+var namagroup10 = 'TESTER';
+var Comment10 = '';
+var namagroup11 = 'TESTER';
+var Comment11 = '';
+var namagroup12 = 'TESTER';
+var Comment12 = '';
+var namagroup13 = 'TESTER';
+var Comment13 = '';
+var namagroup14 = 'TESTER';
+var Comment14 = '';
+var namagroup15 = 'TESTER';
+var Comment15 = '';
+var namagroup16 = 'TESTER';
+var Comment16 = '';
 
 //Batas
 
