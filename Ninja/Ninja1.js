0db35a3dfc97648c440b38e3336a5b548de32405
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-20 18:29:18 +0700

    Update Ninja1.js

--- Ninja/Ninja1.js
+++ Ninja/Ninja1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ninja 1
 // @namespace    http://tampermonkey.net/
-// @version      3.7
+// @version      3.8
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Ninja/Ninja1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Ninja/Ninja1.js
@@ -17,24 +17,24 @@
 // @connect      raw.githubusercontent.com
 // ==/UserScript==
 
-var namagroup1 = 'shio';
-var Comment1 = '#shiokelinci4d*ANIANI11*51*35';
+var namagroup1 = 'ALLPAS';
+var Comment1 = 'Tok99Toto ( DINARCANDY ) : 51*35*01';
 var namagroup2 = 'BUKU';
 var Comment2 = 'IYATOTO ANISAMEI01 36*80*62';
 var namagroup3 = 'TIKTAK';
 var Comment3 = 'Tiktaktogel / ANANAN333 / 37 , 15 , 50';
-var namagroup4 = 'G41B';
-var Comment4 = 'GAIB4D=ANINEI05=14*57*42';
+var namagroup4 = 'TAFSIR MIMPI';
+var Comment4 = 'SIJITOGEL DELAY 14*57*42';
 var namagroup5 = 'KEITOGEL';
 var Comment5 = '#keitogel = (ASIH123) = 65*56*48';
 var namagroup6 = 'PAPUAT0T0';
 var Comment6 = '#PAPUATOTO=(ANEH454)=47-54';
 var namagroup7 = '453P VIP';
 var Comment7 = 'ASEPTOGEL ANJIRRR 17*95*61';
-var namagroup8 = 'POLATRIK';
-var Comment8 = '( POLATRIK  : 55*90 )';
-var namagroup9 = 'G1LA';
-var Comment9 = 'GILA4D=ANJAYY22=71*96*75';
+var namagroup8 = 'OPUNG';
+var Comment8 = 'OPUNG4D ( DOREMI ) : 55*90*06';
+var namagroup9 = 'MAYAPADA';
+var Comment9 = 'BETT*Mayapada4D(BO)*DEWADEWI*71*96*75';
 var namagroup10 = 'GROUP LOMBA ANGKA';
 var Comment10 = '#JNETOTO(ANISASA)*12*86*68';
 var namagroup11 = 'TOYIB';
@@ -43,14 +43,14 @@ var namagroup12 = 'MASTER KUY';
 var Comment12 = 'TOGELKUY ATIATI 11*20*83';
 var namagroup13 = 'KOI';
 var Comment13 = '#KOITOTO ( ALELDUL88 ) 58*88';
-var namagroup14 = 'ANGKER';
-var Comment14 = 'ANGKER4D=ANANA45=02*09*38';
+var namagroup14 = 'Hoho';
+var Comment14 = 'DOMINO : 02*09*38 #HOHOTOGEL';
 var namagroup15 = 'VESPA';
 var Comment15 = 'VESPATOGEL (ANGRYBIRD) 67*98*05';
 var namagroup16 = 'NEMO';
 var Comment16 = 'NEMO4D (ANISAMEME) : 78*46*77';
 var namagroup17 = 'KIKO';
-var Comment17 = '#KIKOTOTO (ARIARI37) = 60*41';	
+var Comment17 = '#KIKOTOTO (ARIARI37) = 60*41';
 //Batas
 var namagroup18 = 'Jawatengah';
 var Comment18 = 'group Ninja 1';
