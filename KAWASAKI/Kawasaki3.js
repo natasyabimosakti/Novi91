de5ab69ebb972370b4a8a69efb985904a5ad9d1f
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-22 08:43:35 +0700

    Update Kawasaki3.js

--- KAWASAKI/Kawasaki3.js
+++ KAWASAKI/Kawasaki3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kawasaki 3
 // @namespace    http://tampermonkey.net/
-// @version      3.17
+// @version      3.18
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/KAWASAKI/Kawasaki3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/KAWASAKI/Kawasaki3.js
@@ -17,8 +17,8 @@
 // @connect      raw.githubusercontent.com
 // ==/UserScript==
 
-var namagroup1 = 'MELATI';
-var Comment1 = '*MELATI188 (ASTER22) : 30*13 *BETTING';
+var namagroup1 = 'GTO';
+var Comment1 = '(GUDANGTOTO) = (ROSELA) 30*13';
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( BUGENVIL ) : 16*79*00';
 var namagroup3 = 'WARUNG';
@@ -33,8 +33,8 @@ var namagroup7 = 'TREX';
 var Comment7 = '#HATORIBET*COCORBEBEK*61*05*52';
 var namagroup8 = 'BLITAR';
 var Comment8 = '#BLITAR4D (EFORBIA) = 63*60';
-var namagroup9 = 'TESTER';
-var Comment9 = '#ASIATOTO = {DEWADEWI} = 80*54';
+var namagroup9 = 'ASIA';
+var Comment9 = '#ASIATOTO = {DEWADEWI2} = 80*54';
 var namagroup10 = 'TESTER';
 var Comment10 = '';
 var namagroup11 = 'TESTER';
