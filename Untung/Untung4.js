591e1ae7744be13646d6fcc7c3b5483ea7ef8223
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-07-22 08:47:11 +0700

    Update Untung4.js

--- Untung/Untung4.js
+++ Untung/Untung4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Untung 4
 // @namespace    http://tampermonkey.net/
-// @version      3.88
+// @version      3.89
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Untung/Untung4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Untung/Untung4.js
@@ -17,8 +17,8 @@
 // @connect      raw.githubusercontent.com
 // ==/UserScript==
 
-var namagroup1 = 'MELATI';
-var Comment1 = '*MELATI188 (WIRANG89) : 67*91 *BETTING';
+var namagroup1 = 'GTO';
+var Comment1 = '(GUDANGTOTO) = (WALI76) 67*91';
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( WAKUL90 ) : 40*50*74';
 var namagroup3 = 'WARUNG';
@@ -33,7 +33,7 @@ var namagroup7 = 'TREX';
 var Comment7 = '#HATORIBET*WENANG95*05*48*88';
 var namagroup8 = 'BLITAR';
 var Comment8 = '#BLITAR4D (WALUYO74) = 65*77';
-var namagroup9 = 'TESTER';
+var namagroup9 = 'ASIA';
 var Comment9 = '#ASIATOTO = {ENGGAN18} = 79*98';
 var namagroup10 = 'TESTER';
 var Comment10 = '';
