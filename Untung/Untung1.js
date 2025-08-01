17dc35b1ddaaf6a5661df32483b5f04107d7d2b9
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-08-02 04:17:09 +0700

    Update Untung1.js

--- Untung/Untung1.js
+++ Untung/Untung1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Untung 1
 // @namespace    http://tampermonkey.net/
-// @version      3.90
+// @version      3.91
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Untung/Untung1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Untung/Untung1.js
@@ -18,7 +18,7 @@
 // ==/UserScript==
 
 var namagroup1 = 'GTO';
-var Comment1 = '(GUDANGTOTO) = (EMAS24) 42*23';
+var Comment1 = '(GUDANGTOTO) = (EMAS24) 42*23*75';
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( ENTOK11 ) : 37*16*20';
 var namagroup3 = 'WARUNG';
