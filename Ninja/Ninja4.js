3af4b79e5012814deba1210576f73f0aece94f8c
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-27 15:59:10 +0700

    Update Ninja4.js

--- Ninja/Ninja4.js
+++ Ninja/Ninja4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ninja 4
 // @namespace    http://tampermonkey.net/
-// @version      3.10
+// @version      3.11
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Ninja/Ninja4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Ninja/Ninja4.js
@@ -27,15 +27,15 @@ var namagroup4 = 'TAFSIR MIMPI';
 var Comment4 = 'SIJITOGEL FULLO9 31*44*59';
 var namagroup5 = 'KEITOGEL';
 var Comment5 = '#keitogel = (SUPERJUNIOR) = 19*03*18';
-var namagroup6 = 'PAPUAT0T0';
-var Comment6 = '#PAPUATOTO (BIGBANG14)=25-16';
+var namagroup6 = 'LIVI';
+var Comment6 = 'BIGBANG14 = 25*16 #LIVITOTO';
 var namagroup7 = '453P VIP';
 var Comment7 = 'ASEPTOGEL DREAMCATCHER 39*27*32';
 var namagroup8 = 'OPUNG';
 var Comment8 = 'OPUNG4D ( EGGROLL ) : 74*82*84';
 var namagroup9 = 'MAYAPADA';
 var Comment9 = 'BETT*Mayapada4D(BO)*SUKRO*34*13*79';
-var namagroup10 = 'GROUP LOMBA ANGKA';
+var namagroup10 = 'KOMUNITAS LOMBA';
 var Comment10 = '#JNETOTO(SNSD01)*22*24*21';
 var namagroup11 = 'TOYIB';
 var Comment11 = '#TOYIBSLOT ( GOLDENCHILD ) : 64*40*69';
