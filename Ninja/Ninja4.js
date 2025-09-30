77f070ed82d2e26c5c5f17d735fef362ca1e80c5
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-30 18:43:31 +0700

    Update Ninja4.js

--- Ninja/Ninja4.js
+++ Ninja/Ninja4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ninja 4
 // @namespace    http://tampermonkey.net/
-// @version      3.11
+// @version      3.12
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Ninja/Ninja4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Ninja/Ninja4.js
@@ -35,7 +35,7 @@ var namagroup8 = 'OPUNG';
 var Comment8 = 'OPUNG4D ( EGGROLL ) : 74*82*84';
 var namagroup9 = 'MAYAPADA';
 var Comment9 = 'BETT*Mayapada4D(BO)*SUKRO*34*13*79';
-var namagroup10 = 'KOMUNITAS LOMBA';
+var namagroup10 = 'JNETOTO';
 var Comment10 = '#JNETOTO(SNSD01)*22*24*21';
 var namagroup11 = 'TOYIB';
 var Comment11 = '#TOYIBSLOT ( GOLDENCHILD ) : 64*40*69';
