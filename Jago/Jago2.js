b73b0fd224f4d6229e69226bf9cd66a92bcc6cda
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-30 18:42:31 +0700

    Update Jago2.js

--- Jago/Jago2.js
+++ Jago/Jago2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JAGO 2
 // @namespace    http://tampermonkey.net/
-// @version      3.55
+// @version      3.56
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Jago/Jago2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Jago/Jago2.js
@@ -37,7 +37,7 @@ var namagroup8 = 'OPUNG';
 var Comment8 = 'OPUNG4D ( PIMAKU28 ) : 02*09*60';
 var namagroup9 = 'MAYAPADA';
 var Comment9 = 'BETT*Mayapada4D(BO)*PELANGAIR*14*57*42';
-var namagroup10 = 'KOMUNITAS LOMBA';
+var namagroup10 = 'JNETOTO';
 var Comment10 = '#JNETOTO(PRAWANKOTA)*37*15*50';
 var namagroup11 = 'TOYIB';
 var Comment11 = '#TOYIBSLOT ( PELANGAIR ) : 36*80*62';
