68891a22709f2fdafaf2a71ef7ce28371255ddcf
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-30 18:45:34 +0700

    Update Slamet4.js

--- Slamet/Slamet4.js
+++ Slamet/Slamet4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Slamet 4
 // @namespace    http://tampermonkey.net/
-// @version      3.74
+// @version      3.75
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Slamet/Slamet4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Slamet/Slamet4.js
@@ -35,7 +35,7 @@ var namagroup8 = 'OPUNG';
 var Comment8 = 'OPUNG4D ( DINASTI88 ) : 15*98*86';
 var namagroup9 = 'MAYAPADA';
 var Comment9 = 'BETT*Mayapada4D(BO)*DUDAMUDA*26*53*82';
-var namagroup10 = 'KOMUNITAS LOMBA';
+var namagroup10 = 'JNETOTO';
 var Comment10 = '#JNETOTO(DJANCUKMIN)*47*23*10';
 var namagroup11 = 'TOYIB';
 var Comment11 = '#TOYIBSLOT ( DUNIAMAYA ) : 61*89*97';
