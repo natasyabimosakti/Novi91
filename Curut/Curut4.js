b2f58605de2433c6253676d87d088bb5f65c77f0
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-30 18:42:21 +0700

    Update Curut4.js

--- Curut/Curut4.js
+++ Curut/Curut4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW CURUT4
 // @namespace    http://tampermonkey.net/
-// @version      3.223
+// @version      3.224
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut4.js
@@ -35,7 +35,7 @@ var namagroup8 = 'OPUNG';
 var Comment8 = 'OPUNG4D ( DELOKDEWE ) : 39*27*32';
 var namagroup9 = 'MAYAPADA';
 var Comment9 = 'BETT*Mayapada4D(BO)*DRUMBUN*39*27*32';
-var namagroup10 = 'KOMUNITAS LOMBA';
+var namagroup10 = 'JNETOTO';
 var Comment10 = '#JNETOTO(DIYENG23)*25*16*85';
 var namagroup11 = 'TOYIB';
 var Comment11 = '#TOYIBSLOT ( DULUANGIH ) : 19*03*43';
