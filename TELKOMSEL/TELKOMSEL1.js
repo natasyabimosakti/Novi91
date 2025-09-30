235e97d08fc962e9aa829bb9fa5e450e9215464a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-30 18:45:40 +0700

    Update TELKOMSEL1.js

--- TELKOMSEL/TELKOMSEL1.js
+++ TELKOMSEL/TELKOMSEL1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TELKOMSEL 1
 // @namespace    http://tampermonkey.net/
-// @version      3.4
+// @version      3.5
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/TELKOMSEL/TELKOMSEL1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/TELKOMSEL/TELKOMSEL1.js
@@ -36,7 +36,7 @@ var namagroup8 = 'OPUNG';
 var Comment8 = 'OPUNG4D ( IKAN55 ) : 51*35*01';
 var namagroup9 = 'MAYAPADA';
 var Comment9 = 'BETT*Mayapada4D(BO)*INKUBATOR*78*46*77';
-var namagroup10 = 'KOMUNITAS LOMBA';
+var namagroup10 = 'JNETOTO';
 var Comment10 = '#JNETOTO(INDONESIA77)*71*96*75';
 var namagroup11 = 'TOYIB';
 var Comment11 = '#TOYIBSLOT ( INKUBATOR ) : 12*86*68';
