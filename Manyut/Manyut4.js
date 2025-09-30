49c85323e8f9aad705c76589860c210b8aefc1fb
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-30 18:43:07 +0700

    Update Manyut4.js

--- Manyut/Manyut4.js
+++ Manyut/Manyut4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW MANYUT4
 // @namespace    http://tampermonkey.net/
-// @version      3.294
+// @version      3.295
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Manyut/Manyut4.js
@@ -37,7 +37,7 @@ var namagroup8 = 'OPUNG';
 var Comment8 = 'OPUNG4D ( ZENDUL5 ) : 37*13*02';
 var namagroup9 = 'MAYAPADA';
 var Comment9 = 'BETT*Mayapada4D(BO)*ZENDIL68*72*39*99';
-var namagroup10 = 'KOMUNITAS LOMBA';
+var namagroup10 = 'JNETOTO';
 var Comment10 = '#JNETOTO(ZENTOLKAWAT)*77*46*69';
 var namagroup11 = 'TOYIB';
 var Comment11 = '#TOYIBSLOT ( ZENDATAWA1 ) : 61*89*97';
