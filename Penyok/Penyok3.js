bce0095d830902f4c892c299f79821fdd064d727
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-30 18:43:47 +0700

    Update Penyok3.js

--- Penyok/Penyok3.js
+++ Penyok/Penyok3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Penyok 3
 // @namespace    http://tampermonkey.net/
-// @version      3.73
+// @version      3.74
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Penyok/Penyok3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Penyok/Penyok3.js
@@ -35,7 +35,7 @@ var namagroup8 = 'OPUNG';
 var Comment8 = 'OPUNG4D ( SUSUBERUANG ) : 61*89*97';
 var namagroup9 = 'MAYAPADA';
 var Comment9 = 'BETT*Mayapada4D(BO)*SUWENGLAMBE*34*76*24';
-var namagroup10 = 'KOMUNITAS LOMBA';
+var namagroup10 = 'JNETOTO';
 var Comment10 = '#JNETOTO(SUNDULUSU)*21*73*63';
 var namagroup11 = 'TOYIB';
 var Comment11 = '#TOYIBSLOT ( SUWENGLAMBEMU ) : 93*62*63';
