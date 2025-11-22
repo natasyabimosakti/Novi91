22f0b6ca089d4449e610c37024d736b18f023a9c
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-11-22 14:48:17 +0700

    Update Babon3.js

--- Babon/Babon3.js
+++ Babon/Babon3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Babon 3
 // @namespace    http://tampermonkey.net/
-// @version      3.79
+// @version      3.80
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon3.js
@@ -29,7 +29,7 @@ var namagroup5 = 'PEDRO';
 var Comment5 = '#PEDRO4D (MUBENGAN52*15*50*68)';
 var namagroup6 = 'DIVA4D';
 var Comment6 = '#DIVA4D (MANCURMUJUR) = 66*04*42';
-var namagroup7 = 'TREX';
+var namagroup7 = 'HAT0R1';
 var Comment7 = '#HATORIBET*MULETENAK*30*47*88';
 var namagroup8 = 'BLITAR';
 var Comment8 = '#BLITAR4D (MRIPATKUSIPI) = 57*82';
