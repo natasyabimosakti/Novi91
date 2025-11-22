5d4ecf5a602a1c2269ebc49eb44d089dc8df8516
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-11-22 14:46:54 +0700

    Update Cuwil3.js

--- Cuwil/Cuwil3.js
+++ Cuwil/Cuwil3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cuwil 3
 // @namespace    http://tampermonkey.net/
-// @version      3.90
+// @version      3.91
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Cuwil/Cuwil3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Cuwil/Cuwil3.js
@@ -29,7 +29,7 @@ var namagroup5 = 'PEDRO';
 var Comment5 = '#PEDRO4D (SUWUNBOSQU*92*27*39)';
 var namagroup6 = 'DIVA4D';
 var Comment6 = '#DIVA4D (SUGEHTERUS12) = 14*42*56';
-var namagroup7 = 'TREX';
+var namagroup7 = 'HAT0R1';
 var Comment7 = '#HATORIBET*SUKMUMETNDES*45*41*52';
 var namagroup8 = 'BLITAR';
 var Comment8 = '#BLITAR4D (SUNANMUHIBIN) = 51*37';
