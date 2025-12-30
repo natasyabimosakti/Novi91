71279dfd99e6df60c4e669f949fbab9b4f329b45
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-12-30 07:31:52 +0700

    Update SIMPATI3.js

--- SIMPATI/SIMPATI3.js
+++ SIMPATI/SIMPATI3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SIMPATI 3
 // @namespace    http://tampermonkey.net/
-// @version      3.6
+// @version      3.7
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/SIMPATI/SIMPATI3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/SIMPATI/SIMPATI3.js
@@ -22,7 +22,7 @@ var Comment1 = '(GUDANGTOTO) = (PANGGANG) 81*24*33';
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( PENJAHAT ) : 46*29*37';
 var namagroup3 = 'KANGMIMPI';
-var Comment3 = '#KANGMIMPI (SUKARONDO77) : *88*03*59';
+var Comment3 = '#KANGMIMPI (PENGGARIS) : *88*03*59';
 var namagroup4 = 'RIATOTO';
 var Comment4 = '#RIATOTO PAYUNG3 = 93*13*34';
 var namagroup5 = 'PEDRO';
