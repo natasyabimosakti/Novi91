f769b85c42cab8430859885eade77b8d8c9bac86
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-12-30 07:11:27 +0700

    Update Babon1.js

--- Babon/Babon1.js
+++ Babon/Babon1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Babon 1
 // @namespace    http://tampermonkey.net/
-// @version      3.81
+// @version      3.82
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon1.js
@@ -21,8 +21,8 @@ var namagroup1 = 'GTO';
 var Comment1 = '(GUDANGTOTO) = (SLEMPANG26) 23*46*75';
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( SETIASAJA ) : 12*97*55';
-var namagroup3 = 'WARUNG';
-var Comment3 = '#(WARUNGTOTO) = (SUKUMLAKU) 56*14';
+var namagroup3 = 'KANGMIMPI';
+var Comment3 = '#KANGMIMPI (SUKUMLAKU) : *56*14*40';
 var namagroup4 = 'RIATOTO';
 var Comment4 = '#RIATOTO SAMBUTAN20 = 98*10*52';
 var namagroup5 = 'PEDRO';
