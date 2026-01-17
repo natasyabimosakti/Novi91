6667143c44ceb7dbd6a50273482f784b013c44d8
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-01-17 17:35:07 +0700

    Update TELKOMSEL1.js

--- TELKOMSEL/TELKOMSEL1.js
+++ TELKOMSEL/TELKOMSEL1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TELKOMSEL 1
 // @namespace    http://tampermonkey.net/
-// @version      3.20
+// @version      3.21
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/TELKOMSEL/TELKOMSEL1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/TELKOMSEL/TELKOMSEL1.js
@@ -610,7 +610,7 @@ async function Mutation_cekArticle() {
         for (const mutation of mutationsList) {
             for (const node of mutation.addedNodes) {
                 if (node.nodeType !== 1) continue;
-
+                if (document.querySelector(".multi-line-floating-textbox")) continue;
                 if (node.matches?.('[data-tracking-duration-id]')) {
                     artikelBaruSet.add(node);
                     if (!parsePost(node)) continue; // ini SKIP hanya artikel ini
@@ -622,18 +622,9 @@ async function Mutation_cekArticle() {
                         return t.includes("jawab") || t.includes("tulis") || t.includes("komentari") || t.includes("postingan") || t.includes("beri");
                     });
                     if (tombolKirim) {
-                        console.log("TextBox komentar ditemukan:", tombolKirim);
-                        async function klikTextboxJikaSiap() {
-                            tombolKirim.click();
-                            const textbox = document.querySelector(".multi-line-floating-textbox");
-                            if (textbox) {
-                                console.log("✅ TextBox komentar Telah DI Klik & Muncul");
-                            } else {
-                                tombolKirim.click();
-
-                            }
-                        }
-                        requestAnimationFrame(klikTextboxJikaSiap);
+                        tombolKirim.click();
+                        console.log("Klik Untuk Komentar")
+                        return;
                     }
                 }
 
@@ -729,9 +720,7 @@ function showNotification(message) {
 }
 async function komentari() {
     let myObservere = new MutationObserver((mutations) => {
-        cekMasalah();
-        cekMasalah2();
-        cekLogout()
+
         for (const mutation of mutations) {
             for (const node of mutation.addedNodes) {
                 if (commentDone) return;
@@ -763,7 +752,7 @@ async function komentari() {
                                 GM.setValue("group_" + grouptToPost, true);
                                 GM.setValue("group_" + grouptToPost + "_expire", Date.now() + EXPIRATION_MS);
                                 console.log("✅ Komentar DIKIRIM (via dispatch):", commentToPost);
-
+                                ObserverCekMasalah()
                                 waitNoDialog();
                                 if (node.nodeType === 1 && node.textContent.toLowerCase().includes('diposting') || node.textContent.toLowerCase().includes('berhasil')) {
                                     setTimeout(() => {
@@ -972,7 +961,16 @@ function MsgError(message) {
     document.body.appendChild(notif);
     ;
 }
+function ObserverCekMasalah() {
+    const observers = new MutationObserver(() => {
+        cekMasalah();
+        cekMasalah2();
 
+        cekLogout()
+    });
+
+    observers.observe(document.body, { childList: true, subtree: true });
+}
 // ===== MAIN FLOW =====
 (async () => {
     try {
