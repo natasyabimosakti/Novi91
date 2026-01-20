d0931c72f4237e438505420636f9ac702581c806
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-01-20 15:54:04 +0700

    Update Bosku2.js

--- Bosku/Bosku2.js
+++ Bosku/Bosku2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW BOSQUE2
 // @namespace    http://tampermonkey.net/
-// @version      3.223
+// @version      3.224
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Bosku/Bosku2.js
 // @author       You
@@ -33,8 +33,8 @@ var namagroup7 = '453P VIP';
 var Comment7 = 'ASEPTOGEL ARNOM63 02*09*60';
 var namagroup8 = 'OPUNG';
 var Comment8 = 'OPUNG4D ( ARSTER73 ) : 58*88*41';
-var namagroup9 = 'WDYUK';
-var Comment9 = '#WDYUK (AREN64) : 37*15*50';
+var namagroup9 = 'HAD1RT0T0';
+var Comment9 = '#HADIRTOTO (AREN64) : 37*15*50';
 var namagroup10 = 'JNETOTO';
 var Comment10 = '#JNETOTO(ARLON89)*36*80*62';
 var namagroup11 = 'TOYIB';
