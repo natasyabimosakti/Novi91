66ab32f23e013aad91972642a4d05902762d81b1
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-01-24 04:10:31 +0700

    Update Jul2.js

--- JUL/Jul2.js
+++ JUL/Jul2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JUL 2
 // @namespace    http://tampermonkey.net/
-// @version      3.13
+// @version      3.14
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/JUL/Jul2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/JUL/Jul2.js
@@ -545,6 +545,7 @@ async function manageGroups() {
     }
 
     const groupKey = `group_${grouptToPost}`;
+    if (groupKey === "group_")return;
     const sudahKomentar = await GM.getValue(groupKey, false);
     if (sudahKomentar) {
         console.log(`Sudah Komentar  ${now}`)
@@ -753,12 +754,6 @@ async function komentari() {
                                 console.log("✅ Komentar DIKIRIM (via dispatch):", commentToPost);
                                 ObserverCekMasalah()
                                 waitNoDialog();
-                                if (node.nodeType === 1 && node.textContent.toLowerCase().includes('diposting') || node.textContent.toLowerCase().includes('berhasil')) {
-                                    setTimeout(() => {
-                                        location.href = "about:blank";
-
-                                    }, 10000);
-                                }
                                 setTimeout(() => {
                                     location.href = "about:blank";
 
@@ -961,11 +956,22 @@ function MsgError(message) {
     ;
 }
 function ObserverCekMasalah() {
-    const observers = new MutationObserver(() => {
-        cekMasalah();
-        cekMasalah2();
+    const observers = new MutationObserver((mutations) => {
+        if (commentDone) return;
 
-        cekLogout()
+        for (const mutation of mutations) {
+            for (const node of mutation.addedNodes) {
+                cekMasalah();
+                cekMasalah2();
+                cekLogout()
+                if (node.nodeType === 1 && node.textContent.toLowerCase().includes('diposting') || node.textContent.toLowerCase().includes('berhasil')) {
+                    setTimeout(() => {
+                        location.href = "about:blank";
+
+                    }, 2000);
+                }
+            }
+        }
     });
 
     observers.observe(document.body, { childList: true, subtree: true });
