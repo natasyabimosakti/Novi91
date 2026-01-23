fcff6c7338c6b60bdb3d5c6aba397fd3669bf3e8
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-01-24 04:16:48 +0700

    Update AutoSwitch.js

--- AutoSwitch.js
+++ AutoSwitch.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto Switch Account
 // @namespace    http://tampermonkey.net/
-// @version      3.97
+// @version      3.98
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/AutoSwitch.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/AutoSwitch.js
