88b5976b0b7b513a18f60e854bcebd14df44921b
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-01-24 14:55:24 +0700

    Update Untung2.js

--- Untung/Untung2.js
+++ Untung/Untung2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW Untung 2
 // @namespace    http://tampermonkey.net/
-// @version      3.109
+// @version      3.110
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Untung/Untung2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Untung/Untung2.js
@@ -545,7 +545,7 @@ async function manageGroups() {
     }
 
     const groupKey = `group_${grouptToPost}`;
-    if (groupKey === "group_")return;
+    if (groupKey === "group_") return;
     const sudahKomentar = await GM.getValue(groupKey, false);
     if (sudahKomentar) {
         console.log(`Sudah Komentar  ${now}`)
@@ -957,14 +957,12 @@ function MsgError(message) {
 }
 function ObserverCekMasalah() {
     const observers = new MutationObserver((mutations) => {
-        if (commentDone) return;
-
         for (const mutation of mutations) {
             for (const node of mutation.addedNodes) {
-                cekMasalah();
-                cekMasalah2();
-                cekLogout()
                 if (node.nodeType === 1 && node.textContent.toLowerCase().includes('diposting') || node.textContent.toLowerCase().includes('berhasil')) {
+                    cekMasalah();
+                    cekMasalah2();
+                    cekLogout()
                     setTimeout(() => {
                         location.href = "about:blank";
 
