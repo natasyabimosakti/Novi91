27ec48667995446a4f97c0b15a74a99a769e104f
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-01-28 19:26:15 +0700

    Update Babon2.js

--- Babon/Babon2.js
+++ Babon/Babon2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Babon 2
 // @namespace    http://tampermonkey.net/
-// @version      3.107
+// @version      3.108
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon2.js
@@ -19,6 +19,7 @@
 
 var namagroup18 = 'Jawatengah';
 var Comment18 = 'babon2';
+
 
 
 
@@ -743,6 +744,7 @@ async function komentari() {
                 sendBtn.dispatchEvent(new MouseEvent("mousedown", opts));
                 sendBtn.dispatchEvent(new MouseEvent("mouseup", opts));
                 sendBtn.click(); // Kadang click() perlu sebagai trigger final
+                window.runBypassTurbo()
                 console.log("🔥 ATOMIC DISPATCH SENT");
 
                 // LOGIKA CEK TERKIRIM (Pindahkan ke luar thread utama agar tidak lag)
