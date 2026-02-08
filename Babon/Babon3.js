25419c699db33140e4b6773c0d59282f14d3bcc7
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-02-08 10:01:52 +0700

    Update Babon3.js

--- Babon/Babon3.js
+++ Babon/Babon3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Babon 3
 // @namespace    http://tampermonkey.net/
-// @version      3.109
+// @version      3.110
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon3.js
