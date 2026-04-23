950e3ff80e9584a8ac444260f9421c197329d44a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-04-23 19:23:33 +0700

    Update Piti3.js

--- Piti/Piti3.js
+++ Piti/Piti3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Piti3
 // @namespace    http://tampermonkey.net/
-// @version      3.137
+// @version      3.138
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti3.js
@@ -307,7 +307,7 @@ function simulateHumanPullToRefresh(distance = 700) {
     const _startX = window.innerWidth / 2;
     const _startY = 150;
     const _steps = 25;
-    const _duration = 50;
+    const _duration = 200;
     const _identifier = Date.now();
 
     // 1. Fungsi pembantu untuk membuat Touch Event
@@ -1250,7 +1250,7 @@ function kirimLaporan(pesan) {
                 }
                 console.log("jalan");
             }
-        }, 500);
+        }, 1000);
 
 
 
