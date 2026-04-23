35f6ef5de8b778a18e1321e86dde99cc0acb0aa0
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-04-23 19:23:40 +0700

    Update SIMPATI3.js

--- SIMPATI/SIMPATI3.js
+++ SIMPATI/SIMPATI3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SIMPATI 3
 // @namespace    http://tampermonkey.net/
-// @version      3.40
+// @version      3.41
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/SIMPATI/SIMPATI3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/SIMPATI/SIMPATI3.js
@@ -307,7 +307,7 @@ function simulateHumanPullToRefresh(distance = 700) {
     const _startX = window.innerWidth / 2;
     const _startY = 150;
     const _steps = 25;
-    const _duration = 50;
+    const _duration = 200;
     const _identifier = Date.now();
 
     // 1. Fungsi pembantu untuk membuat Touch Event
@@ -1250,7 +1250,7 @@ function kirimLaporan(pesan) {
                 }
                 console.log("jalan");
             }
-        }, 500);
+        }, 1000);
 
 
 
