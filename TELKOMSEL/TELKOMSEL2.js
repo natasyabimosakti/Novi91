c94d46d05ce27fed3674663decb9339f19850c9a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-04-23 15:23:37 +0700

    Update TELKOMSEL2.js

--- TELKOMSEL/TELKOMSEL2.js
+++ TELKOMSEL/TELKOMSEL2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TELKOMSEL 2
 // @namespace    http://tampermonkey.net/
-// @version      3.35
+// @version      3.36
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/TELKOMSEL/TELKOMSEL2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/TELKOMSEL/TELKOMSEL2.js
@@ -20,6 +20,7 @@
 
 var namagroup18 = 'Jawatengah';
 var Comment18 = 'telkomsel2';
+
 
 
 
@@ -307,7 +308,7 @@ function simulateHumanPullToRefresh(distance = 700) {
     const _startX = window.innerWidth / 2;
     const _startY = 150;
     const _steps = 25;
-    const _duration = 600;
+    const _duration = 50;
     const _identifier = Date.now();
 
     // 1. Fungsi pembantu untuk membuat Touch Event
@@ -742,7 +743,7 @@ async function Mutation_cekArticle() {
             console.log("📦 koleksi sementara:", artikelBaruSet.size);
 
             // belum memenuhi syarat, jangan stop observer
-            if (artikelBaruSet.size < 2) {
+            if (artikelBaruSet.size < 0) {
                 console.log("⏳ artikel kurang, menunggu...");
                 return; // biarkan observer lanjut
             }
@@ -1226,7 +1227,7 @@ function stopObserver() {
                 }
                 console.log("jalan");
             }
-        }, 1000);
+        }, 500);
 
 
 
