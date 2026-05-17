0e25ceec9b161aace5cd0ed1a2e9c6d40c0003aa
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-05-17 13:40:41 +0700

    Update Piti1.js

--- Piti/Piti1.js
+++ Piti/Piti1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Piti1
 // @namespace    http://tampermonkey.net/
-// @version      3.144
+// @version      3.145
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Piti/Piti1.js
