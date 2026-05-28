b6500c83bb3291daa0af2cedf4ae0a127034815f
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-05-28 10:00:04 +0700

    Update Babon1.js

--- Babon/Babon1.js
+++ Babon/Babon1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Babon 1
 // @namespace    http://tampermonkey.net/
-// @version      3.137
+// @version      3.138
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Babon/Babon1.js
@@ -21,6 +21,7 @@
 
 var namagroup18 = 'Jawatengah';
 var Comment18 = 'babon1';
+
 
 
 
@@ -885,8 +886,8 @@ async function komentari() {
                     return;
                 }
 
-                const textarea2 = node.querySelector(".internal-input")
-                const sendBtn2 = node.querySelector("[aria-label*='Posting komentar' i]");
+                const textarea2 = document.querySelector(".internal-input")
+                const sendBtn2 = document.querySelector("[aria-label*='Posting komentar' i]");
 
                 if (textarea2 && sendBtn2) {
                     commentDone = true;
