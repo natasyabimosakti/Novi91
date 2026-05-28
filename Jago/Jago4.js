95cb08ec91729ddf536e0a8930873b460fc35fd3
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-05-28 12:29:49 +0700

    Update Jago4.js

--- Jago/Jago4.js
+++ Jago/Jago4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JAGO 4
 // @namespace    http://tampermonkey.net/
-// @version      3.102
+// @version      3.103
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Jago/Jago4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Jago/Jago4.js
@@ -21,7 +21,6 @@
 
 var namagroup18 = 'Jawatengah';
 var Comment18 = 'jago4';
-
 
 
 
@@ -150,6 +149,7 @@ function getCommentForGroup() {
     let ceknamagroup2 = "";
     let ceknamagroup3 = "";
     let ceknamagroup4 = "";
+    let ceknamagroup5 = "";
     for (let i = 0; i < groupNames.length; i++) {
         commentMap[groupNames[i]] = normalizeToBasicLatin(CommentList[i]);
     }
@@ -592,7 +592,7 @@ function parsePost(artikels) {
         return false;
     }
     if (!CekKeyword(postingan.toLowerCase())) return false;
-
+    window.focus();
     return true;
 }
 
