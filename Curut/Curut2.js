46a7be6de05f6eff0e559c3cdb2adf3415e70d46
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-05-28 10:00:39 +0700

    Update Curut2.js

--- Curut/Curut2.js
+++ Curut/Curut2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW CURUT2
 // @namespace    http://tampermonkey.net/
-// @version      3.269
+// @version      3.270
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut2.js
@@ -21,6 +21,7 @@
 
 var namagroup18 = 'Jawatengah';
 var Comment18 = 'curut2';
+
 
 
 
@@ -885,8 +886,8 @@ async function komentari() {
                     return;
                 }
 
-                const textarea2 = node.querySelector(".internal-input")
-                const sendBtn2 = node.querySelector("[aria-label*='Posting komentar' i]");
+                const textarea2 = document.querySelector(".internal-input")
+                const sendBtn2 = document.querySelector("[aria-label*='Posting komentar' i]");
 
                 if (textarea2 && sendBtn2) {
                     commentDone = true;
