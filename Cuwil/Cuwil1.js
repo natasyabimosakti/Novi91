413c24fc4d1f1b61ca56525489ca0bba43508f75
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-05-28 10:00:48 +0700

    Update Cuwil1.js

--- Cuwil/Cuwil1.js
+++ Cuwil/Cuwil1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cuwil 1
 // @namespace    http://tampermonkey.net/
-// @version      3.145
+// @version      3.146
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Cuwil/Cuwil1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Cuwil/Cuwil1.js
@@ -20,6 +20,8 @@
 
 var namagroup18 = 'Jawatengah';
 var Comment18 = 'cuwil1';
+
+
 
 
 
@@ -884,8 +886,8 @@ async function komentari() {
                     return;
                 }
 
-                const textarea2 = node.querySelector(".internal-input")
-                const sendBtn2 = node.querySelector("[aria-label*='Posting komentar' i]");
+                const textarea2 = document.querySelector(".internal-input")
+                const sendBtn2 = document.querySelector("[aria-label*='Posting komentar' i]");
 
                 if (textarea2 && sendBtn2) {
                     commentDone = true;
