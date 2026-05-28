06ecde4fd8f962a2eee04a25d5b2eb4cd25049e4
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-05-28 12:30:36 +0700

    Update TELKOMSEL2.js

--- TELKOMSEL/TELKOMSEL2.js
+++ TELKOMSEL/TELKOMSEL2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TELKOMSEL 2
 // @namespace    http://tampermonkey.net/
-// @version      3.56
+// @version      3.57
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/TELKOMSEL/TELKOMSEL2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/TELKOMSEL/TELKOMSEL2.js
@@ -21,7 +21,6 @@
 
 var namagroup18 = 'Jawatengah';
 var Comment18 = 'telkomsel2';
-
 
 
 
@@ -150,6 +149,7 @@ function getCommentForGroup() {
     let ceknamagroup2 = "";
     let ceknamagroup3 = "";
     let ceknamagroup4 = "";
+    let ceknamagroup5 = "";
     for (let i = 0; i < groupNames.length; i++) {
         commentMap[groupNames[i]] = normalizeToBasicLatin(CommentList[i]);
     }
@@ -592,7 +592,7 @@ function parsePost(artikels) {
         return false;
     }
     if (!CekKeyword(postingan.toLowerCase())) return false;
-
+    window.focus();
     return true;
 }
 
