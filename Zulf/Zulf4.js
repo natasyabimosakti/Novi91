7693fda9c6c35f9335aa007c26940ebd521fe667
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-05-28 07:10:29 +0700

    Update Zulf4.js

--- Zulf/Zulf4.js
+++ Zulf/Zulf4.js
@@ -1,3 +1,27 @@
+// ==UserScript==
+// @name         NEW ZULF4
+// @namespace    http://tampermonkey.net/
+// @version      3.119
+// @description  try to take over the world!
+// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Zulf/Zulf4.js
+// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Zulf/Zulf4.js
+// @author       You
+// @match        http*://*/*
+// @run-at       document-end
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        GM.setValue
+// @grant        GM.getValue
+// @grant        window.close
+// @grant        GM_xmlhttpRequest
+// @connect      api.telegram.org
+// @connect      raw.githubusercontent.com
+// @connect      localhost
+// ==/UserScript==
+
+var namagroup18 = 'Jawatengah';
+var Comment18 = 'cuan4';
+
+
 
 // --- ANTI-THROTTLE SPOOFING (Ensures execution in background tabs) ---
 (function () {
