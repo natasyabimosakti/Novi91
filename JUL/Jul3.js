ecbb4ac2476e3c512815fe3cc6d5b1269ae3818d
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-06-05 06:17:10 +0700

    Update version and refactor button click logic

--- JUL/Jul3.js
+++ JUL/Jul3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JUL 3
 // @namespace    http://tampermonkey.net/
-// @version      3.54
+// @version      3.55
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/JUL/Jul3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/JUL/Jul3.js
@@ -975,8 +975,10 @@ async function start() {
         if (isUserPage) {
             simulateHumanPullToRefresh();
         } else {
-            klikTombolByText("URUTKAN");
-            klikTombolByText('\u{f1953}');
+            const ikonTombolTarget = ['\u{f1953}', '\u{f3159}','URUTKAN'];
+            ikonTombolTarget.forEach(ikon => {
+                klikTombolByText(ikon);
+            });
         }
 
     }, 300); // Heartbeat cepat (300ms) dengan proteksi redundansi
