1d5517bdc77746d9e097369cd9b8f37d7bf01efc
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-06-05 06:17:02 +0700

    Update version and modify button click logic

--- Curut/Curut4.js
+++ Curut/Curut4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NEW CURUT4
 // @namespace    http://tampermonkey.net/
-// @version      3.280
+// @version      3.281
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Curut/Curut4.js
@@ -975,8 +975,10 @@ async function start() {
         if (isUserPage) {
             simulateHumanPullToRefresh();
         } else {
-            klikTombolByText("URUTKAN");
-            klikTombolByText('\u{f1953}');
+            const ikonTombolTarget = ['\u{f1953}', '\u{f3159}','URUTKAN'];
+            ikonTombolTarget.forEach(ikon => {
+                klikTombolByText(ikon);
+            });
         }
 
     }, 300); // Heartbeat cepat (300ms) dengan proteksi redundansi
