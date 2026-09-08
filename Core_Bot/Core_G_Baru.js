4afbf89dbdcfa705a4f55904af43b05c9b7fc921
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-09-08 14:11:22 +0700

    Update Core_G_Baru.js

--- Core_Bot/Core_G_Baru.js
+++ Core_Bot/Core_G_Baru.js
@@ -66,14 +66,14 @@ window.initBabonLogic = function (namagroup18, Comment18) {
         return true; // Tab ini MENANG dan berhak komentar!
     }
     // Menentukan URL berdasarkan variabel global pasar (dari @require)
-    var baseURL = `http://127.0.0.1:8080/${Comment19}.json`;
+    var baseURL = `http://127.0.0.1:8080/${Comment18}.json`;
     var URLGROUP = baseURL;
 
     if (typeof pasar !== 'undefined') {
         if (pasar === "SG") {
-            URLGROUP = `http://127.0.0.1:8080/${Comment19}_SG.json`;
+            URLGROUP = `http://127.0.0.1:8080/${Comment18}_SG.json`;
         } else if (pasar === "SD") {
-            URLGROUP = `http://127.0.0.1:8080/${Comment19}_SD.json`;
+            URLGROUP = `http://127.0.0.1:8080/${Comment18}_SD.json`;
         }
     }
     var nama_FB_Global = "Unknown"
@@ -83,7 +83,7 @@ window.initBabonLogic = function (namagroup18, Comment18) {
     var TELEGRAM_TOKEN = '8841941027:-qJTrFa4';
     var TELEGRAM_CHAT_ID = '-1002717306025';
     let adminList = [];
-    var SCRIPT_NAME = Comment19
+    var SCRIPT_NAME = Comment18
     let isAdminListReady = false; // Flag penanda kesiapan data
     var refresh = 300; // Percepat durasi animasi tarik layar agar selesai dalam 200ms
     var refreshNonUser = 300;
@@ -328,7 +328,7 @@ window.initBabonLogic = function (namagroup18, Comment18) {
 
                             if (namagroup18 && Comment18) {
                                 groupNames.push(normalizeToBasicLatin(namagroup18).toLowerCase());
-                                CommentList.push(Comment19);
+                                CommentList.push(Comment18);
                             }
 
                             console.log("✅ Group list berhasil diambil dari " + urlToFetch + ":", groupNames.length);
